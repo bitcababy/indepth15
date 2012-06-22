2c1035b7a13b29400ea291e5e13f8a7252250749
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-22 10:22:33 -0400

    Assignment: Removed Mongoind::Paranoia and Mongoid::Taggable

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,12 +1,7 @@
-class Assignment
 class Assignment < TextDocument
 	include Mongoid::Document
 	include Mongoid::Versioning
-	include Mongoid::Paranoia
-	include Mongoid::Taggable
 
-	field :name
-	field :contents
 	
 	# has_and_belongs_to_many :sections
 	
