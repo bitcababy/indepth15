fc6bd497f14aded989bb8324e41ddbbc4af42228
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-22 12:15:06 -0400

    Removed documents fields from Course, since they're taken care of by "has_one"

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,18 +7,14 @@ class Course
 	field :f, as: :full_name, type: String
 	field :i, as: :in_catalog, type: Boolean, default: true
 	field :a, as: :academic_year, type: Integer
-	field :in, as: :information, default: ->{self.build_course_document}
-	field :d, as: :description, default: ->{self.build_course_document}
-	field :r, as: :resources, default: ->{self.build_course_document}
-	field :p, as: :policies, default: ->{self.build_course_document}
-	field :n, as: :news, default: ->{self.build_course_document}
 	
 	embeds_many :sections
-	belongs_to :information, class_name: 'CourseDocument'
-	belongs_to :description, class_name: 'CourseDocument'
-	belongs_to :resources, class_name: 'CourseDocument'
-	belongs_to :policies, class_name: 'CourseDocument'
-	belongs_to :news, class_name: 'CourseDocument'
+		
+	has_one :information, class_name: 'CourseDocument', autobuild: true
+	has_one :description, class_name: 'CourseDocument', autobuild: true
+	has_one :resources, class_name: 'CourseDocument', autobuild: true
+	has_one :policies, class_name: 'CourseDocument', autobuild: true
+	has_one :news, class_name: 'CourseDocument', autobuild: true
 	
 	validates_uniqueness_of :number
 
