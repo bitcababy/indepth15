f37ec9ab7451fe6802f4c0064e3a34271e3165eb
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-22 10:23:24 -0400

    Assignment: added :name field, validation of presence

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,9 +2,10 @@ class Assignment < TextDocument
 	include Mongoid::Document
 	include Mongoid::Versioning
 
+	field :name, 				type: String
 	
 	# has_and_belongs_to_many :sections
 	
-	validates_presence_of :name, :contents
+	validates_presence_of :name
 	
 end
