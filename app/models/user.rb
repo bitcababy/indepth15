f0ff56bca6b4bf3a5f0f35c5a042cd5e599620c4
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-22 12:20:15 -0400

    Moved unique_name from User to Teacher

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User
 	field :h, as: :honorific, type:String
 	field :fn, as: :first_name, type: String
 	field :ln, as: :last_name, type: String
-	field :un, as: :unique_name, type: String
 	
 end
