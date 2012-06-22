2dc84cd9e5b89a00e5b26c75d459c3ee85011453
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-22 12:19:40 -0400

    Section: changed embedded_in to belongs_to for :course

--- app/models/section.rb
+++ app/models/section.rb
@@ -8,7 +8,7 @@ class Section
 	field :n, as: :number, type: Integer
 	field :r, as: :room, type: String
 	
-	embedded_in 	:course
+	belongs_to 	:course
 	belongs_to 		:teacher
 	has_and_belongs_to_many :assignments
 end
