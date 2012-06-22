6fa72659257218bb79c012ac36195204da1e411b
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-22 12:25:49 -0400

    Added contexts around tests, some basic tests of fields

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
 
 describe Course do
-	it { should validate_uniqueness_of :number }
+	context "validation" do
+		it { should validate_uniqueness_of :number }
+	end
+	
 	it { should have(0).sections }
+	
+	context "associations" do
+		specify { subject.information.should_not be_nil }
+		specify { subject.description.should_not be_nil }
+		specify { subject.resources.should_not be_nil }
+		specify { subject.policies.should_not be_nil }
+		specify { subject.news.should_not be_nil }
+	end
+	
+	
+
 end
