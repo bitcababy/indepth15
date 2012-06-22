052bafea52f78c126f75a45637b84e89de6ead87
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-22 10:28:32 -0400

    Configured action_mailer
    Devise needs it

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,13 +15,16 @@ DepthCharge::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.default_url_options = { host: 'localhost:3000' }
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.perform_deliveries = false
+	# config.action_mailer.default charset: "utf-8"
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-
 
   # Do not compress assets
   config.assets.compress = false
