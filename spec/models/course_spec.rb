3ffcda8f413597112811e8b872beabd95a5fe6bb
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-23 07:27:00 -0400

    Removed association tests from course spec.

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,16 +5,9 @@ describe Course do
 		it { should validate_uniqueness_of :number }
 	end
 	
-	it { should have(0).sections }
 	
 	context "associations" do
-		specify { subject.information.should_not be_nil }
-		specify { subject.description.should_not be_nil }
-		specify { subject.resources.should_not be_nil }
-		specify { subject.policies.should_not be_nil }
-		specify { subject.news.should_not be_nil }
-	end
-	
-	
+		it { should have(0).sections }
+	end	
 
 end
