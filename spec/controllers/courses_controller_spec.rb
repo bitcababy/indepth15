2a6b5e7f4e63d550ce7b94dc71c4e06cb2f11ca5
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-23 11:18:59 -0400

    Added basic tests for the CoursesController

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -2,4 +2,52 @@ require 'spec_helper'
 
 describe CoursesController do
 
+	context "single course" do
+		before :each do
+			@course = Fabricate :course
+		end
+
+	  describe "GET 'home'" do
+	    it "returns http success" do
+	      visit course_home_path(@course)
+	      response.should be_success
+	    end
+	  end
+
+	  describe "GET 'resources_pane'" do
+	    it "returns http success" do
+	      visit course_resources_pane_path(@course)
+	      response.should be_success
+	    end
+	  end
+
+	  describe "GET 'information_pane'" do
+	    it "returns http success" do
+	      visit course_information_pane_path(@course)
+	      response.should be_success
+	    end
+	  end
+
+	  describe "GET 'sections_pane'" do
+	    it "returns http success" do
+	      visit course_sections_pane_path(@course)
+	      response.should be_success
+	    end
+	  end
+
+	  describe "GET 'news_pane'" do
+	    it "returns http success" do
+	      visit course_news_pane_path(@course)
+	      response.should be_success
+	    end
+	  end
+
+	  describe "GET 'policies_pane'" do
+	    it "returns http success" do
+	      visit course_policies_pane_path(@course)
+	      response.should be_success
+	    end
+	  end
+	end
+
 end
