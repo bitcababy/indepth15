e7db2e5d980425dc6b0bde26dc4eb0df7c0ca135
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-23 10:09:00 -0400

    Refactored name fields from User to RegisteredUser

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,3 @@
 class User
-	include Mongoid::Document
-	
-	field :h, as: :honorific, type:String
-	field :fn, as: :first_name, type: String
-	field :ln, as: :last_name, type: String
-	
+	include Mongoid::Document	
 end
