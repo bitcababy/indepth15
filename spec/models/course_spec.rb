39364059bfcb7717edb1651dad3166eeb105e217
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-23 10:14:16 -0400

    Added some tests to course_spec

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
 describe Course do
-	context "validation" do
-		it { should validate_uniqueness_of :number }
+	it { should have_many(:sections) }
+	it { should validate_uniqueness_of(:number).scoped_to(:academic_year) }
+	
+	context "changes" do
+		subject {Fabricate(:course)}
+		it "should have one more section when it adds a section" do
+			expect {
+				subject.sections << Fabricate(:section, course: subject)
+			}.to change {subject.sections.count}.by(1)
+		end
 	end
-	
-	
-	context "associations" do
-		it { should have(0).sections }
-	end	
 
 end
