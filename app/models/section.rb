e1def79abc644d7600f803d7a296d8efdfcfcfad
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-23 10:06:12 -0400

    Section: Added assignments field which contains an array of hashes
    Each hash contains  due_date and an assignment

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,13 +2,12 @@
 
 class Section
 	include Mongoid::Document
-	
+	field :n, as: :number, type: Integer
 	field :b, as: :block, type: String
 	field :d, as: :days, type: Array
-	field :n, as: :number, type: Integer
 	field :r, as: :room, type: String
+	field :as, as: :assignments, type: Array, default: []
 	
-	belongs_to 	:course
-	belongs_to 		:teacher
-	has_and_belongs_to_many :assignments
+	belongs_to :course
+	belongs_to :teacher
 end
