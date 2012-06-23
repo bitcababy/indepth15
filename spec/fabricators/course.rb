5f3d5dd20e37634bc82aa6f30d16c4c18117dd43
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-23 07:26:29 -0400

    Change course fabricator to use strings for the course docs
    Probably temporary

--- spec/fabricators/course.rb
+++ spec/fabricators/course.rb
@@ -5,11 +5,12 @@ Fabricator(:course) do
 	full_name			{ "Course #{Fabricate.sequence}"}
 	in_catalog		true
 	academic_year	2012
-	information
-	description
-	resources
-	policies
-	news
+	description		"This is a description"
+	information		"This is the course information"
+	news					"This is the course news"
+	resources			"These are some resources"
+	policies			"These are the policies"
+	
 end
 
 	
