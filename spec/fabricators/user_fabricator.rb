30e82a1d460826e172186724e502be05540c5133
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-23 10:13:49 -0400

    Added fabricator for :registered_user, fixed inheritance of :teacher.

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,9 +1,12 @@
 Fabricator(:user) do
+end
+
+Fabricator(:registered_user) do
 	honorific				{ %W(Mr. Mrs. Ms. Dr.).sample }
 	first_name			{ %W(John Jane Jake Dan Larry).sample }
 	last_name				{ %W(Black White Orange Red).sample }
 end
 
-Fabricator(:teacher, from: :user) do
+Fabricator(:teacher, from: :registered_user) do
 	unique_name			{ |attrs| "#{attrs[:honorific]} #{Fabricate.sequence} #{attrs[:last_name]}" }
 end
