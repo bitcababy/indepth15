631f007ae41b0f7a5e5028d6beb124a791ad2308
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-23 11:31:18 -0400

    Fixed the page header test

--- features/step_definitions/courses_steps.rb
+++ features/step_definitions/courses_steps.rb
@@ -14,14 +14,16 @@ end
 ###
 ### Thens
 ###
-Then /^I should see this list:?$/ do |table|
+Then /^I should see (?:this|the) list:?$/ do |table|
 	ul = page.find('#courses')
 	names = (ul.all('li').map &:text).map &:strip
 	(table.hashes.map &:values).flatten.should == names
 end
 
-Then /^the page header should contain the course's name$/ do
-  pending # express the regexp above with the code you wish you had
+Then /^the page header should contain the full name of the course$/ do
+	page.should have_selector('#page_header') do |hdr|
+		hdr.should have_content(the_course.full_name)
+	end
 end
 
 Then /^I should see the following tabs:$/ do |table|
