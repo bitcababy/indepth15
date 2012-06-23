e08043f0bcd51570a1217d82b505721265b06328
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-23 10:03:33 -0400

    Assignment: Removed habtm :section

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,10 +1,5 @@
 class Assignment < TextDocument
-	include Mongoid::Document
-	include Mongoid::Versioning
-
 	field :name, 				type: String
-	
-	has_and_belongs_to_many :sections
 	
 	validates_presence_of :name
 	
