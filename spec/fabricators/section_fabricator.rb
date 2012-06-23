89efb60d99192eaa3e0a932a4abaa9cfd56d390b
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-23 10:12:43 -0400

    Added number field, sequenced, to Section fabricator. Set assignments to []

--- spec/fabricators/section_fabricator.rb
+++ spec/fabricators/section_fabricator.rb
@@ -1,9 +1,12 @@
 Fabricator(:section) do
+	number	{ sequence(:section_number) }
 	block		{ ('A'..'E').to_a.sample }
-	days		{ (1..8).sample(5) }
+	days		{ (1..8).to_a.sample(5) }
 	room		{ "Room #{Fabricate.sequence}"}
 	course
 	teacher
-	assignments
+	assignments	[]
 end
+
+Fabricate.sequence(:section_number) {|i| i }
 
