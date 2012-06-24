03d46d294cb487fc2c6cec1fc4502bd82f4cfe8d
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-24 11:26:25 -0400

    Removed assignments field, replaced with embedded section_assignments. Fixed add_assignment to reflect this

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,12 +1,12 @@
 # encoding: UTF-8
 
 class Section
+	include Utils
 	include Mongoid::Document
 	field :n, as: :number, type: Integer
 	field :b, as: :block, type: String
 	field :d, as: :days, type: Array
 	field :r, as: :room, type: String
-	field :as, as: :assignments, type: Array, default: []
 	
 	belongs_to :course
 	belongs_to :teacher
@@ -14,13 +14,14 @@ class Section
 	accepts_nested_attributes_for :section_assignments
 
 	validates_uniqueness_of :number, scope: :course
-
+		
 	def to_s
 		"Section #{self.number}, block: #{self.block}"
 	end
 	
 	def add_assignment(due_date, asst, show=true)
-		self.assignments << [{due_date: due_date, assignment: asst, show: show}]
-	end
+		self.section_assignments.create! due_date: due_date, assignment: asst
+	end			
+	
 
 end
