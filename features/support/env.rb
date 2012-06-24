f66cb0087eb33bf7c6a3fab6bc83c5cb12754211
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-24 11:30:58 -0400

    Added capybara/cucumber. Configured Capybara.

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,12 +5,17 @@
 # files.
 
 require 'cucumber/rails'
+require 'capybara/cucumber'
 
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
 # prefer to use XPath just remove this line and adjust any selectors in your
 # steps to use the XPath syntax.
-Capybara.default_selector = :css
+Capybara.configure do |c|
+	c.default_driver = :rack_test
+	c.javascript_driver = :akephalos
+	c.default_selector = :css
+end
 
 # By default, any exception happening in your Rails application will bubble up
 # to Cucumber so that your scenario will fail. This is a different from how 
