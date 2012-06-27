da7395c3f7247f9eb0e05ae7b8eb31ce3a41135d
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 11:17:14 -0400

    Added scope test for .current

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Teacher do
 	it { should have_many(:sections) }
+	
+	context "scopes" do
+		it "should return current teachers" do
+			3.times {Fabricate(:teacher, current: true)}
+			2.times {Fabricate(:teacher, current: false)}
+			Teacher.count.should == 5
+			Teacher.current.count.should == 3
+		end
+	end
+		
 end
