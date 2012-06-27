2b65326ce77a45bcba9164b336d03897f0d4cbd1
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 11:09:18 -0400

    General steps: various changes
    Added 'Given I've visited a page'  and 'Then I should see ...'

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -1,3 +1,20 @@
+##
+## Givens
+##
+Given /^I've visited a page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+##
+## Whens
+##
+When /^I visit any page$/ do
+  visit root_path
+end
+
+##
+## Then
+##
 Then /^I should see a "([^"]*)" tab$/ do |tab_name|
 	page.should have_selector("div#tabs ul"), "Expected a div with id tabs and a ul"
 	page.find('div#tabs ul').should have_link tab_name#, "Expected a link named #{tab_name}"
@@ -7,6 +24,6 @@ Then /^the page title should be "([^"]*)"$/ do |title|
 	pending "Unfinished test"
 end
 
-When /^I visit any page$/ do
-  visit root_path
+Then /^I should see "(.*?)"$/ do |arg1|
+  page.should have_content(arg1)
 end
