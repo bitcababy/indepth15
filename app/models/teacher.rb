e3c1b1d6096299771ff81feedadf2e305da291cc
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 11:02:32 -0400

    Teacher: various changes.
    Added field :current. (Not sure if necesary). Added scope :current and
    :order_by_name. Added #formal_name

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,14 @@
 class Teacher < RegisteredUser
 	field :un, as: :unique_name, type: String
-	
+	field :c, as: :current, type: Boolean
+
 	has_many :sections
+	
+	scope :current, where(current: true)
+	scope :order_by_name, order_by(:last_name.asc, :first_name.asc)
+	
+	def formal_name
+		"#{self.honorific} #{self.last_name}"
+	end
 	
 end
