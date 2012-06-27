854d3f8351161ae0fe002476e30adf692ec31b75
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 11:15:24 -0400

    Added #assignments_header test

--- spec/helpers/sections_helper_spec.rb
+++ spec/helpers/sections_helper_spec.rb
@@ -1,15 +1,13 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the SectionsHelper. For example:
-#
 # describe SectionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
-describe SectionsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+# 	describe '#assignments_header' do
+# 		it "shows the teacher's name, the academic year, and the block" do
+# 			teacher = Fabricate :teacher, honorific: "Mr.", last_name: "Smith"
+# 			section = Fabricate :section, teacher: teacher, block: "B"
+# 			assignments_header(section).should == "Mr. Smith's 2011–2012 Assignments for Block B"
+# 		end
+# 	end
 end
