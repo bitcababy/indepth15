10c59de19bf407b9f96cbb7d2adff1702ee7d70f
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 11:15:59 -0400

    Course spec: Added #teachers

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,6 +4,11 @@ describe Course do
 	it { should have_many(:sections) }
 	it { should validate_uniqueness_of(:number).scoped_to(:academic_year) }
 	
+	context "A new course" do
+		subject {Fabricate(:course)}
+		it { should have(0).sections }
+	end
+	
 	context "changes" do
 		subject {Fabricate(:course)}
 		it "should have one more section when it adds a section" do
@@ -13,4 +18,14 @@ describe Course do
 		end
 	end
 
+	context '#teachers' do
+		it "should return a list of the teachers teaching the course" do 
+			course = Fabricate(:course)
+			t1 = Fabricate(:teacher)
+			t2 = Fabricate(:teacher)
+			3.times {Fabricate :section, course: course, teacher: t1}
+			2.times {Fabricate :section, course: course, teacher: t2}
+			course.teachers.count.should == 2
+		end
+	end
 end
