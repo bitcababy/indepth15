38e34a38ad6500841fdd582952dee580df8afd8f
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 11:11:18 -0400

    SectionsController spec: Added tests for GET 'assignments'

--- spec/controllers/sections_controller_spec.rb
+++ spec/controllers/sections_controller_spec.rb
@@ -1,12 +1,39 @@
 require 'spec_helper'
 
+def setup_section
+	@the_teacher = Fabricate(:teacher)
+	@the_course = Fabricate(:course)
+	@the_section = Fabricate :section, course: @the_course, teacher: @the_teacher
+
+  3.times { @the_section.add_assignment(Utils.future_due_date + rand(1..5), Fabricate(:assignment)) }
+  2.times { @the_section.add_assignment(Utils.future_due_date - rand(1..5), Fabricate(:assignment)) }
+end
+
 describe SectionsController do
 
   describe "GET 'assignments'" do
+		before do
+			setup_section
+      get :assignments, :id => @the_section.id
+			@the_section.section_assignments.future.asc(:due_date).count.should == 3
+		end
+
     it "returns http success" do
-      get 'assignments'
       response.should be_success
     end
+
+		it "assigns @current_assignment" do
+			assigns(:current_assignment).should_not be_nil
+		end
+
+		it "assigns @upcoming_assignments" do
+			assigns(:upcoming_assignments).should_not be_nil
+		end
+
+		it "assigns @past_assignments" do
+			assigns(:past_assignments).should_not be_nil
+		end
+
   end
 
 end
