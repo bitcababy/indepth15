dd25cb0233734ee3874ffac453dccd0257911eef
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 11:12:48 -0400

    Added fabricator for :teacher

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,12 +1,17 @@
 Fabricator(:user) do
 end
 
-Fabricator(:registered_user) do
+Fabricator(:registered_user, parent: :user) do
 	honorific				{ %W(Mr. Mrs. Ms. Dr.).sample }
 	first_name			{ %W(John Jane Jake Dan Larry).sample }
 	last_name				{ %W(Black White Orange Red).sample }
 end
 
-Fabricator(:teacher, from: :registered_user) do
-	unique_name			{ |attrs| "#{attrs[:honorific]} #{Fabricate.sequence} #{attrs[:last_name]}" }
+Fabricator(:teacher, parent: :registered_user) do
+	honorific				{ %W(Mr. Mrs. Ms. Dr.).sample }
+	first_name			{ %W(John Jane Jake Dan Larry).sample }
+	last_name				{ %W(Black White Orange Red).sample }
+	general_leadin	"This is the general leadin"
+	current_leadin	"This is the current leadin"
+	current					true
 end
