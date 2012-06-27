a0a60c91535ef89d1a46cda4059844d95a5e5a56
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 10:57:46 -0400

    ApplicationHelper: Added #assignment_date_string method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
 	def academic_year_string
 		return "#{Settings.academic_year-1}–#{Settings.academic_year}"
 	end
+	
+	def assignment_date_string(date)
+		date.strftime("%a, %b %-d")
+	end
+	
 end
