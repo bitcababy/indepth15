efeee1ae290246834c2630af2f843c53a970512e
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 10:59:31 -0400

    Course: various changes
    Change field :duration to a Symbol. Change scope :in_catalog to sort by
    number. Added #teachers/

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course
 	include Mongoid::Document
 
 	field :no, as: :number, type: Integer
-	field :d, as: :duration, type: String, default: "Full year"
+	field :d, as: :duration, type: Symbol, default: :full_year
 	field :c, as: :credits, type: BigDecimal, default: 5.0
 	field :f, as: :full_name, type: String
 	field :i, as: :in_catalog, type: Boolean, default: true
@@ -13,11 +13,14 @@ class Course
 	field :n, as: :news, type: String, default: ""
 	field :p, as: :policies, type: String, default: ""
 		
-	scope :in_catalog, where(in_catalog: true)
+	scope :in_catalog, where(in_catalog: true).asc(:number)
 	
 	has_many :sections
-		
 	validates_uniqueness_of :number, scope: :academic_year
+	
+	def teachers
+		(sections.collect {|s| s.teacher}).uniq
+	end
 
 end
 	
