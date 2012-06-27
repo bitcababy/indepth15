3316dd9dbc888ceb991c0ea3b8d4de441fe5d518
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 10:59:48 -0400

    RegisteredUser: Added #formal_name

--- app/models/registered_user.rb
+++ app/models/registered_user.rb
@@ -2,4 +2,8 @@ class RegisteredUser < User
 	field :h, as: :honorific, type:String
 	field :fn, as: :first_name, type: String
 	field :ln, as: :last_name, type: String
+	
+	def formal_name
+		"#{self.honorific} #{self.last_name}"
+	end
 end
