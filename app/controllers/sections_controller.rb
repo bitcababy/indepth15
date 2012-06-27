96b92867aa8c2e9e8b434afe7f1378681501b641
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 10:57:26 -0400

    SectionsController: Added code for #assignments and #find_secton callback

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,4 +1,15 @@
 class SectionsController < ApplicationController
+	before_filter :find_section
+	
   def assignments
+		@upcoming_assignments = @section.section_assignments.future.asc(:due_date)
+		@current_assignment = @upcoming_assignments.shift
+		@past_assignments = @section.section_assignments.future.desc(:due_date).limit(Settings.past_assts_num)
   end
+
+	private
+	def find_section
+		n = params[:id]
+		@section = Section.find(n)
+	end
 end
