a082ef0a00ad4796772ef00c298e30a2f9f57e87
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 11:23:00 -0400

    Added middle_name field to RegisteredUser

--- app/models/registered_user.rb
+++ app/models/registered_user.rb
@@ -1,9 +1,11 @@
 class RegisteredUser < User
 	field :h, as: :honorific, type:String
 	field :fn, as: :first_name, type: String
+	field :mn, as: :middle_name, type: String
 	field :ln, as: :last_name, type: String
 	
 	def formal_name
 		"#{self.honorific} #{self.last_name}"
 	end
+
 end
