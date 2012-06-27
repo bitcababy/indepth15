f7fc4da5276c2bcdd9811e5ea8a6cf63adeefaee
Meredith Lesly <bitcababy+github@gmail.com>
2012-06-27 11:12:05 -0400

    CourseFabricator: Change duration to symbols, added fabrication of course from full_name.

--- spec/fabricators/course_fabricator.rb
+++ spec/fabricators/course_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:course) do
 	number				{ sequence(:course_number) }
-	duration			{ ['Full year', 'Semester'].sample }
+	duration			{ [:full_year, :first_semester, :second_semester].sample }
 	credits				{ [ 5.0, 2.5].sample }
 	full_name			{ "Course #{Fabricate.sequence}"}
 	in_catalog		true
@@ -10,7 +10,10 @@ Fabricator(:course) do
 	news					"This is the course news"
 	resources			"These are some resources"
 	policies			"These are the policies"
-	
 end
 
 Fabricate.sequence(:course_number) {|i| i}
+
+Fabrication::Transform.define(:course, 
+	lambda { |full_name| Fabricate :course, full_name: full_name }
+	)
