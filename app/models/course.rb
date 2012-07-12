8721d295187e64ff49630831fed0399a8501330c
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-12 13:13:11 -0400

    Fixed field defaults, moved BRANCH_MAP out of import section. Removed import section.

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,23 +5,37 @@ class Course
 	
 	NO_YEAR = -1
 	
-	
 	FULL_YEAR = :full_year
 	FIRST_SEMESTER = :first_semester
 	SECOND_SEMESTER = :second_semester
 	FULL_YEAR_HALF_TIME = :halftime
 	DURATIONS = [FULL_YEAR, FIRST_SEMESTER, SECOND_SEMESTER, FULL_YEAR_HALF_TIME]
-	
+	BRANCH_MAP = {
+		321 => 'Geometry',
+		322 => 'Geometry',
+		326 => 'Algebra',
+		331 => 'Algebra',
+		332 => 'Algebra',
+		333 => 'Algebra',
+		341 => 'Precalculus',
+		342 => 'Precalculus',
+		343 => 'Discrete Math',
+		352 => 'Precalculus',
+		361 => 'Calculus',
+		371 => 'Calculus',
+		391 => 'Statistics',
+	}
+
 	field :number, type: Integer
-	field :academic_year, type: Integer, default: NO_YEAR
+	field :academic_year, type: Integer, default: Settings.academic_year
 	field :duration, type: Symbol, default: FULL_YEAR
 	field :credits, type: BigDecimal, default: 5.0
-	field :full_name, type: String
-	field :short_name, type: String
-	field :schedule_name, type: String
+	field :full_name, type: String, default: ""
+	field :short_name, type: String, default: ""
+	field :schedule_name, type: String, default: ""
 	field :in_catalog, type: Boolean, default: true
 	field :occurrences, type: Integer, default: 5
-	field :has_assignments, type: Boolean
+	field :has_assignments, type: Boolean, default: true
 
 	scope :in_catalog, where(in_catalog: true).asc(:number)
 	
@@ -58,58 +72,42 @@ class Course
 		3 => FULL_YEAR_HALF_TIME,
 	}
 
-	class << self
-		BRANCH_MAP = {
-			321 => 'Geometry',
-			322 => 'Geometry',
-			326 => 'Algebra',
-			331 => 'Algebra',
-			332 => 'Algebra',
-			333 => 'Algebra',
-			341 => 'Precalculus',
-			342 => 'Precalculus',
-			343 => 'Discrete Math',
-			352 => 'Precalculus',
-			361 => 'Calculus',
-			371 => 'Calculus',
-			391 => 'Statistics',
-		}
-
-		def massage_hash(hash)
-			hash[:duration] = SEMESTER_MAP[hash[:semester].to_i]
-			# [:semester].each {|k| hash.delete(k)}
-		end
-	
-		#
-		# importing
-		# 
-		def convert_record(hash)
-			hash['duration'] = SEMESTER_MAP[hash['semesters'].to_i]
-			info = hash['info']
-			resources = hash['resources']
-			policies = hash['policies']
-			prog_of_studies_descr = hash['prog_of_studies_descr']
-			%W(semesters info resources policies prog_of_studies_descr orig_id).each {|k| hash.delete(k)}
-			
-			course = self.create! hash
-			course_tag = Tag::Course.find_or_create_by(label: course.full_name)
-			doc = course.create_information_doc content: info
-			doc.tags << course_tag
-			branch_tag = BRANCH_MAP[course.number]
-			course.branch = Tag::Branch.find_or_initialize_by(label: branch_tag) if branch_tag
-			
-			course.create_resources_doc(content: resources)
-			course.create_news_doc
-			course.create_policies_doc content: policies
-			course.create_description_doc content: prog_of_studies_descr
-			course.academic_year = Settings.academic_year
-			course.save!
-			(Settings.academic_year - 1).downto(Settings.start_year) do |y|
-				course.clone_for_year y
-			end
-			course
-		end
-	end
+	# class << self
+	# 	def massage_hash(hash)
+	# 		hash[:duration] = SEMESTER_MAP[hash[:semester].to_i]
+	# 		# [:semester].each {|k| hash.delete(k)}
+	# 	end
+	# 
+	# 	#
+	# 	# importing
+	# 	# 
+	# 	def convert_record(hash)
+	# 		hash['duration'] = SEMESTER_MAP[hash['semesters'].to_i]
+	# 		info = hash['info']
+	# 		resources = hash['resources']
+	# 		policies = hash['policies']
+	# 		prog_of_studies_descr = hash['prog_of_studies_descr']
+	# 		%W(semesters info resources policies prog_of_studies_descr orig_id).each {|k| hash.delete(k)}
+	# 		
+	# 		course = self.create! hash
+	# 		course_tag = Tag::Course.find_or_create_by(label: course.full_name)
+	# 		doc = course.create_information_doc content: info
+	# 		doc.tags << course_tag
+	# 		branch_tag = BRANCH_MAP[course.number]
+	# 		course.branch = Tag::Branch.find_or_initialize_by(label: branch_tag) if branch_tag
+	# 		
+	# 		course.create_resources_doc(content: resources)
+	# 		course.create_news_doc
+	# 		course.create_policies_doc content: policies
+	# 		course.create_description_doc content: prog_of_studies_descr
+	# 		course.academic_year = Settings.academic_year
+	# 		course.save!
+	# 		(Settings.academic_year - 1).downto(Settings.start_year) do |y|
+	# 			course.clone_for_year y
+	# 		end
+	# 		course
+	# 	end
+	# end
 	
 end
 	
