9eb15be8a92c3653712ce53063758d2e19760ec0
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-12 13:18:01 -0400

    Removed test for removed method #tag_obj

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
-class Foo
-	include Mongoid::Document
-  
-	has_and_belongs_to_many :tags, inverse_of: nil
-end
-
 describe Tag do
 	describe '::add' do
 		it "creates a new tag if there isn't one" do
@@ -19,21 +13,6 @@ describe Tag do
 			t2 = Tag.add "test"
 			t1.should === t2
 		end
-	end
-	
-	describe '::tag_obj' do
-		it "adds the object to its tagged list" do
-			obj = Foo.create!
-			tag = Tag.tag_obj("test", obj)
-			tag.tagged.should contain(obj)
-		end
-		it "adds itself to the object's tags list" do
-			obj = Foo.create!
-			tag = Tag.tag_obj("test", obj)
-			obj.tags.should contain(tag)
-		end
-			
-	end
-			
+	end			
 
 end
