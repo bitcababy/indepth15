719a8354490a50da52ba283253f8e641243c6dd9
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-12 13:14:15 -0400

    Remove tag_obj method and association with documents

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,26 +1,14 @@
 class Tag
   include Mongoid::Document
-	field :content, type: String
+	field :content, type: String, default: ""
 	
-	has_and_belongs_to_many :documents
-
-	class << self
-		def add(txt)
-			t = Tag.find_or_create_by(content: txt)
-			return t
-		end
-		
-		def tag_obj(txt, obj)
-			txt = txt.to_s
-			t = add(txt)
-			obj.tags << t
-			obj.save!
-			t.tagged << obj
-			return t
-		end
+	def self.add(txt)
+		t = Tag.find_or_create_by(content: txt)
+		return t
 	end
 		
 	def to_s
 		self.content
-	end	
+	end
+	
 end
