a959857f25d6d7dc9487a760f368f12a4e3528f2
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-12 13:12:19 -0400

    Author: added has_and_belongs_to_many :documents, make author tag after creation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,10 @@
 class Author < User
 	has_one :author_tag, class_name: 'Tag::Author'
+	has_and_belongs_to_many :documents, inverse_of: nil
+
+	after_create :make_tag
+	
+	def make_tag
+		Tag::Author.create!(author: self) unless Tag::Author.where(author: self).exists?
+	end
 end
