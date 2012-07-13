68ce32890f6c691fd1983e33e2fbc5d3576ebe16
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-13 11:12:41 -0400

    Merged in some document types and fixed inheritance

--- spec/fabricators/document_fabricator.rb
+++ spec/fabricators/document_fabricator.rb
@@ -1,11 +1,25 @@
-Fabricator(:document)
-
-Fabricator(:text_document) 
-
-Fabricator(:course_page) do
-	course		nil
+Fabricator :document do
+	tags			[]
 end
 
-Fabricator(:teacher_page) do
-	teacher		nil
+Fabricator('Document::Owned') do
+end
+
+Fabricator('Document::Versioned') do
+	major_version		1
+	minor_version		0
+end
+
+Fabricator :text_document, class_name: 'Document::Text' do
+	content		""
+end
+
+Fabricator :course_page, class_name: 'Document::CoursePage' do
+	content		""
+	kind			:unknown
+end
+
+Fabricator :teacher_page, class_name: 'Document::TeacherPage do
+	content		""
+	kind			:unknown
 end
