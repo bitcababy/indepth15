7bcd91f45aad134f447a49f45e0b94dc2c27f38c
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-20 12:59:10 -0400

    Added #is_are_number_mangler and #link_for_menu_item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,18 @@
 
 module ApplicationHelper
 
+	def is_are_number_mangler(n, word)
+		case
+			when n > 1
+				res = "are "
+			when n == 1 then
+				res = "is "
+			when n == 0 then
+				res = "are "
+		end
+		return res + pluralize(n, word).gsub(/^0/, 'no')
+	end
+
 	def academic_year_string(year)
 		return "#{year-1}—#{year}"
 	end
@@ -10,4 +22,17 @@ module ApplicationHelper
 		return date.strftime("%a, %b %-d")
 	end
 
+	def link_for_menu_item(item)
+ 		case
+		when item.link
+			item.link
+		when item.action && item.controller
+			url_for(controller: item.controller, action: item.action)
+		when item.object
+			url_for(item.object)
+		else
+			raise "Not written yet"
+		end
+	end
+		
 end
