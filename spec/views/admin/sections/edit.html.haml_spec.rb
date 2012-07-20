8b1e696235a562f6cd1644ddb123bb84c7b3e503
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-20 13:15:41 -0400

    Lots of pending tests

--- spec/views/admin/sections/edit.html.haml_spec.rb
+++ spec/views/admin/sections/edit.html.haml_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe "sections/edit.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before :each do
+		pending "Unfinished test"
+		@course = Fabricate(:course)
+		@section = Fabricate.build(:section, course: @course)
+		render
+	end
+
+	it "displays a number picker" do
+		pending "Unfinished test"
+		# puts rendered
+		# pending "Unfinished test"
+		page.should have_field("section[number]")
+		pending "Unfinished test"
+	end
+		
+	it "displays a semester picker"
+	it "display a block picker"
+	it "display an occurrences editor"
+	it "displays a room"
 end
