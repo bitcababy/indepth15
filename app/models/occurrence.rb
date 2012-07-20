0ebe98412e885e18e3ce7d99435203c302ea87a9
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-20 13:01:23 -0400

    Added aliases for fields. Added #to_s.

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -1,13 +1,19 @@
 class Occurrence
   include Mongoid::Document
 
-	field :block_name, type: String
-	field :occurrence, type: Integer
-	field :day_number, type: Integer
-	field :period, type: Integer
-
+	field :no, as: :number, type: Integer
+	field :bn, as: :block, type: String
+	field :dn, as: :day_number, type: Integer
+	field :pe, as: :period, type: Integer
+	
+	scope :for_block, lambda {|b| where(block_name: b)}
+	
 	def self.import_from_hash(hash)
 		Occurrence.create! hash
+	end
+	
+	def to_s
+		"Occurrence: #{number}/#{block}/#{day_number}/#{period}"
 	end
 		
 end
