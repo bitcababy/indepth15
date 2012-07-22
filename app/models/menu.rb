3c92012eba007f8f55b002a9674b1f3c58842e66
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-22 10:10:23 -0400

    Moved creation of the main menus to MenuSet

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,23 +1,17 @@
 class Menu < MenuItem
 	field :mc, as: :menu_class, type: String, default: "sub-menu"
 	
-	def self.top_level(admin = false)
-		if admin then
-			[self.for_home, self.for_courses, self.for_teachers, self.for_admin]
-		else
-			[self.for_home, self.for_courses, self.for_teachers]
-		end
-	end
+	embedded_in :menu_set
 	
 	def self.for_home
-		menu = self.create! label: 'Home', link: 'home'
+		menu = self.create! label: 'Home', link: 'home', item_class: 'home'
 		menu.child_menu_items.create! label: 'Sign in', link: 'sign_in', controller: 'devise/sections', action: :new
 		menu.child_menu_items.create! label: 'About', link: 'about'
 		return menu
 	end
 	
 	def self.for_courses
-		menu = self.create! label: 'Courses', link: 'courses'
+		menu = self.create! label: 'Courses', link: 'courses', item_class: 'courses'
 		Course.in_catalog.where(academic_year: Settings.academic_year).each do |course|
 			submenu = Menu.create! object: course
 			for section in course.sections do
@@ -31,7 +25,7 @@ class Menu < MenuItem
 	end
 	
 	def self.for_teachers
-		menu = self.create! label: 'Teachers', link: 'teachers', controller: :teachers, action: :index
+		menu = self.create! label: 'Teachers', link: 'teachers', controller: :teachers, action: :index, item_class: 'Teachers'
 		Teacher.where(current: true).asc(:last_name).asc(:first_name).each do |teacher|
 			submenu = Menu.create! label: teacher.menu_label
 			submenu.child_menu_items.create! object: teacher
