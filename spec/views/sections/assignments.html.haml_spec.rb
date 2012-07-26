504efd7a08573d7aad2f8ccb9f7d0caf977ee095
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-26 10:07:54 -0400

    Added specs for assignments.html

--- spec/views/sections/assignments.html.haml_spec.rb
+++ spec/views/sections/assignments.html.haml_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe "sections/assignments.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+	include CourseExamplesHelper
+	before :each do
+		course = Fabricate :course, full_name: 'Math 101'
+		teacher = Fabricate :teacher, 
+							honorific: "Mr.", last_name: "Masterson", 
+							generic_msg: "This is my generic message",
+							current_msg: "This is my current message",
+							upcoming_msg: "This is my upcoming message"
+		@section = Fabricate :section, teacher: teacher, course: course
+		add_some_assignments(@section, 3, 2)
+		assign(:section, @section)
+		pending "Need to deal with @current_assignments, etc."
+	end
+
+  it "displays a header for the assignments" do
+	
+		render
+		rendered.should have_selector('div#header') do |div|
+			div.text.should =~ /#{academic_year_string(Settings.academic_year)} Assignments/
+		end
+	end
+	
+	it "displays the teacher's generic message" do
+		render
+		rendered.should contain("This is my generic message")
+	end
+	
+			
 end
