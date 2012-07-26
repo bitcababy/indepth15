39838708ba2e73c921d03bcc0fb99c94c8035f2e
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-26 10:17:38 -0400

    Added academic_year to section fabrication

--- spec/models/section_assignment_spec.rb
+++ spec/models/section_assignment_spec.rb
@@ -5,10 +5,9 @@ describe SectionAssignment do
 	it { should belong_to :assignment }
 	
 	it "should have future and past scopes" do
-		pending "Unfinished test"
 		section = Fabricate(:section)
-		2.times { section.add_assignment Date.today + rand(1..5), Fabricate(:assignment) }
-		3.times { section.add_assignment Date.today - rand(1..5), Fabricate(:assignment) }
+		2.times { section.add_assignment Fabricate(:assignment), Date.today + rand(1..5) }
+		3.times { section.add_assignment Fabricate(:assignment), Date.today - rand(1..5) }
 		section.section_assignments.future.count.should == 2
 		section.section_assignments.past.count.should == 3
 	end
@@ -19,9 +18,9 @@ describe SectionAssignment do
 			Assignment.where(assgt_id: 1234).should exist
 			@teacher = Fabricate(:teacher, login: 'greenx')
 			Teacher.where(login: 'greenx' ).should exist
-			@course = Fabricate(:course, number: 567, academic_year: 2012, full_name: 'Math 101')
+			@course = Fabricate(:course, number: 567, full_name: 'Math 101')
 			Course.where(number: 567).should exist
-			@section = Fabricate(:section, teacher: @teacher, course: @course, block: "C")
+			@section = Fabricate(:section, teacher: @teacher, course: @course, block: "C", academic_year: 2012)
 			@section.should_not be_nil
 			sa = SectionAssignment.import_from_hash({
 				assgt_id: 1234,
