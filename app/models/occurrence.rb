c26f378a0b5f27eceebf5ab1b294b2972291e2eb
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-26 10:10:31 -0400

    SectionAssignment: Changed to only create one if the assignment had been used in that section
    Also modified to find section using the new Section academic_year field

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -9,11 +9,11 @@ class Occurrence
 	scope :for_block, lambda {|b| where(block_name: b)}
 	
 	def self.import_from_hash(hash)
-		Occurrence.create! hash
+		return Occurrence.create! hash
 	end
 	
 	def to_s
-		"Occurrence: #{number}/#{block}/#{day_number}/#{period}"
+		return "Occurrence: #{number}/#{block}/#{day_number}/#{period}"
 	end
 		
 end
--- app/models/section_assignment.rb
+++ app/models/section_assignment.rb
@@ -1,12 +1,13 @@
 class SectionAssignment
 	include Mongoid::Document
+  include Mongoid::Timestamps
 
 	field :due_date, type: Date
-	field :published, type: Boolean
 	field :name, type: String, default: ""
 	
 	belongs_to :section
 	belongs_to :assignment
+	belongs_to :course
 	
 	accepts_nested_attributes_for :section, :assignment
 	
@@ -14,17 +15,18 @@ class SectionAssignment
 	scope :past, -> { lt(due_date: Utils.future_due_date) }
 	
 	def self.import_from_hash(hash)
+		return unless hash[:use_assgt] == 'Y'
 		assgt_id = hash[:assgt_id]
-		hash[:published] = hash[:use_assgt] == 'Y'
 		block = hash[:block]
+		year = hash[:schoolyear]
 
-		course = Course.find_by(number: hash[:course_num], academic_year: hash[:schoolyear])
+		course = Course.find_by(number: hash[:course_num])
 		raise "course is nil" unless course
 		teacher = Teacher.find_by(login: hash[:teacher_id])
 		raise "teacher is nil" unless teacher
-		section = (course.sections.select {|s| s.block == block && s.teacher == teacher}).first
+		section = Section.find_by(course: course, block: block, academic_year: year, teacher: teacher)
 		unless section
-			puts "can't find section for #{hash['schoolyear']}/#{teacher}/#{block}/#{assgt_id}"
+			puts "can't find section for #{year}/#{teacher}/#{block}/#{assgt_id}"
 			return
 		end
 
