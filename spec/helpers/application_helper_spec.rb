afa56134dc576ee866b5c6daeccf1f0c02dc7f27
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-26 10:16:47 -0400

    ApplicationHelper: Removed tests for fossil method

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -16,33 +16,7 @@ describe ApplicationHelper do
 			assignment_date_string(Date.new(2012, 7, 12)).should == "Thu, Jul 12"
 		end
 	end
-	
-	describe '#link_for_menu_item' do
-		context "with explicit link" do
-			it "returns the link" do
-				item = item_with_link link: "http://google.com"
-				link_for_menu_item(item).should == "http://google.com"
-			end
-		end
-
-		context "with action" do
-			it "returns a link using the controller and action" do
-				item = item_with_controller controller: 'courses', action: 'index'
-				link_for_menu_item(item).should == '/courses'
-			end
-		end
 				
-		context "with object" do
-			it "returns a link from the object" do
-				obj = Fabricate(:course)
-				item = Fabricate(:menu_item)
-				item.object = obj
-				link_for_menu_item(item).should == "/courses/#{obj.id}"
-			end
-		end
-
-	end	
-			
 end
 
 	
