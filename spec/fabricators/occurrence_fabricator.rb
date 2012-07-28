03586eeb10af4089dc559a4b3c44fb8a76bf04ac
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-28 13:26:47 -0400

    Fixed occurrence fabricator to avoid block duplication

--- spec/fabricators/occurrence_fabricator.rb
+++ spec/fabricators/occurrence_fabricator.rb
@@ -1,7 +1,7 @@
 Fabricator(:occurrence) do
 	number			{ sequence(:occurrence_numbr) {|i| i} }
-	block				{ (A..H).to_a.sample }
-	day_number	{ (1..8).to_a.sample }
+	block				{ sequence(:block) }
+	day	{ (1..8).to_a.sample }
 	period			{ (1..5).to_a.sample }
 end
 
