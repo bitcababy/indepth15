a05cf6456b94c0c97f544b82fbbde4b50c4abb05
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-28 13:27:59 -0400

    Changed :day_number to :day

--- spec/models/occurrence_spec.rb
+++ spec/models/occurrence_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 describe Occurrence do
 	
+	context "fabricator" do
+		subject { Fabricate :occurrence }
+	end
+	
 	describe 'self.import_from_hash' do
 		it "creates a new occurrence from a hash" do
-			o = Occurrence.import_from_hash({block_name: "B", occurrence: 1, day_number: 2, period: 3})
+			o = Occurrence.import_from_hash({block_name: "B", occurrence: 1, day: 2, period: 3})
 			o.should be_kind_of Occurrence
 		end
 	end
