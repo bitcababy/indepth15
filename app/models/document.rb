d5ecfd5fb1a72c59e0a68948fc74f645802cf7ea
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-28 13:13:34 -0400

    Removed versioning stuff
    Should be done en masse

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,23 +3,12 @@ class Document
 	include Mongoid::Paranoia
   # include Mongoid::Timestamps
 
-	field :mj, as: :major_version, type: Integer, default: 1
-	field :mv, as: :minor_version, type: Integer, default: 0
 	field :dt, as: :document_type, type: Symbol
-	field :ki, as: :kind, type: Symbol
 	field :ma, as: :major_tags, type: Array, default: []
 	field :mi, as: :minor_tags, type: Array, default: []
 	
 	belongs_to :owner, polymorphic: true
 		
-	def bump_minor_version
-		self.minor_version += 1
-	end
-	
-	def bump_major_version
-		self.major_version += 1
-	end
-
 	TYPES = [:assignment, :worksheet, :test, :quiz, :retake, :benchmark, :classwork, :homework]
 
 end
