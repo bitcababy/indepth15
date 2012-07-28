0a37f0d8e6d9c2975ae8e91b28c5892b08c73011
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-28 13:20:39 -0400

    Teacher import now uses HTMLEntities to fix messages

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,5 @@
 class Teacher < Author
 
-	# @@next_id = (Teacher.count > 0 ? Teacher.last.id : 0)
 	field :un, as: :unique_name, type: String
 	field :cu, as: :current, type: Boolean
 	field :dr, as: :default_room, type: String
@@ -11,10 +10,11 @@ class Teacher < Author
 
 	has_many :sections
 	# has_many :teacher_pages
+	
+	index({current: -1})
 
 	scope :current, where(current: true)
 	scope :order_by_name, order_by(:last_name.asc, :first_name.asc)
-	
 	#
 	# importing
 	# 
@@ -22,8 +22,10 @@ class Teacher < Author
 		hash[:email] = hash[:login] + "@mail.weston.org"
 		hash[:password] = (hash[:phrase].split(' ').map &:first).join('') if (hash[:phrase])
 		hash[:current] = hash[:old_current] == 1
-		gm = hash[:generic_msg]
-		um = hash[:upcoming_msg]
+		coder = HTMLEntities.new
+		hash[:generic_msg] = coder.decode(hash[:generic_msg])
+		# puts "#{hash[:generic_msg]}"
+		hash[:upcoming_msg] = coder.decode(hash[:upcoming_msg])
 		
 		[:phrase, :old_current, :teacher_id, :orig_id].each {|k| hash.delete(k)}
 		teacher = self.create! hash
