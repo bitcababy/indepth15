3e9e9b83f95540547d2bcbd04fa535d34b1c225b
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-28 13:06:49 -0400

    Assignment converter now uses a HTMLEntities decoder

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,14 +1,12 @@
 class Assignment < TextDocument
 	field :name, type: String
-	field :number, type: Integer
-
-	belongs_to :course
-	belongs_to :author
-	
-	# has_and_belongs_to_many :section_assignments, inverse_of: nil # Not sure we need to go in this direction
 
 	def self.import_from_hash(hash)
-		return Assignment.create! hash.merge(owner: Teacher.find_by(login: hash[:teacher_id]))
+		coder = HTMLEntities.new
+		hash[:content] = coder.decode(hash[:description])
+		author = Teacher.find_by(login: hash[:teacher_id])
+		[:description, :locked, :number, :teacher_id, :year].each { |k| hash.delete(k) }
+		return Assignment.create! hash.merge(owner: author)
 	end
 
 end
