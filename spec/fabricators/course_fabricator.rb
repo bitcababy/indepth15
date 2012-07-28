1ea9314a270ece018a1f91791d37a2269b324879
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-28 13:26:11 -0400

    Fixed course fabricator

--- spec/fabricators/course_fabricator.rb
+++ spec/fabricators/course_fabricator.rb
@@ -1,23 +1,22 @@
 Fabricator(:course) do
-	number					{ sequence(:course_number) }
+	number					{ sequence(:course_number, 321) {|i| i}}
 	duration				{ [:full_year, :first_semester, :second_semester].sample }
 	credits					{ [ 5.0, 2.5].sample }
-	full_name				{ "Course #{Fabricate.sequence}"}
+	full_name				{ |attrs| "Course #{attrs[:number]}"}
 	in_catalog			true
-	information_doc(fabricator: :text_document)
-	resources_doc(fabricator: :text_document)
-	policies_doc(fabricator: :text_document)
-	news_doc(fabricator: :text_document)
-	description_doc(fabricator: :text_document)
+	information_doc	{ Fabricate :text_document }
+	resources_doc		{ Fabricate :text_document }
+	policies_doc		{ Fabricate :text_document }
+	news_doc				{ Fabricate :text_document }	
+	description_doc	{ Fabricate :text_document }
 end
 
-Fabricate.sequence(:course_number) {|i| i}
+Fabricate.sequence(:course_number, 321)
 
-Fabrication::Transform.define(:course, lambda { |full_name|
-			if Course.where(full_name: full_name).exists? then
-				Course.find_by(full_name: full_name)
+Fabrication::Transform.define(:course, lambda { |number|
+			if Course.where(number: number).exists? then
+				Course.find_by(number: number)
 			else
-			 Fabricate :course, full_name: full_name 
+			 Fabricate :course, number: number 
 			end
 		})
-
