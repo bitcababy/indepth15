975098eac5121161af6b1b6560a5b9dd05955cf6
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-28 13:40:53 -0400

    Removed number as ID

--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -5,9 +5,10 @@ class Occurrence
 	field :bn, as: :block, type: String
 	field :dn, as: :day, type: Integer
 	field :pe, as: :period, type: Integer
-	field :_id, type: Integer, default: ->{ number }
 	
 	scope :for_block, lambda {|b| where(block_name: b)}
+	
+	index {number: 1, block: 1}
 	
 	def self.import_from_hash(hash)
 		return Occurrence.create! hash
