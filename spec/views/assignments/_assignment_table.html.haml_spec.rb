ecbf3b1bbc07b7d8606c5c1ac020a041f90551bd
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-29 15:13:08 -0400

    Removed fossil tests

--- spec/views/assignments/_assignment_table.html.haml_spec.rb
+++ spec/views/assignments/_assignment_table.html.haml_spec.rb
@@ -7,13 +7,9 @@ describe 'assignments/_assignment_table' do
 			section = Fabricate(:section)
 			sas << Fabricate(:section_assignment, section: section, assignment: Fabricate(:assignment), due_date: (Date.today + i))
 		end
-		assign(:caption, "Upcoming message")
-		assign(:table_id, "upcoming")
 		render partial: 'assignments/assignment_table', locals: {assignments: sas}
-		rendered.should have_selector('table', id: 'upcoming') do |table|
-			table.should have_selector('thead tr') do |row|
-				['#', 'Date due', 'Assignment'].each {|hdr| row.should have_selector('th', content: hdr)}
-			end
+		rendered.should have_selector('thead tr') do |row|
+			['#', 'Date due', 'Assignment'].each {|hdr| row.should have_selector('th', content: hdr)}
 		end
 	end
 		
