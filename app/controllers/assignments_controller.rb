0187ed89cf73c57dd1d157dc5d3874cc23260b8d
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-29 14:00:49 -0400

    AssignmentsController: Fixed how I was getting the assignments

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -10,15 +10,14 @@ class AssignmentsController < ApplicationController
 		@section = course.sections.where(academic_year: year, block: block).first
 		raise "section not found: #{course_number}/#{block}" unless @section
 
-		@upcoming_assignments = @section.section_assignments.future.asc(:due_date)
-		@current_assignment = @upcoming_assignments.shift
-		@past_assignments = @section.section_assignments.future.desc(:due_date).limit(Settings.past_assts_num)
+		@upcoming_assignments = @section.section_assignments.upcoming.asc(:due_date)
+		@current_assignment =  @section.section_assignments.current
+		@past_assignments = @section.section_assignments.past.desc(:due_date).limit(Settings.past_assts_num)
+
 		respond_to do |format|
       format.html
     end
 		
 	end
-	
-	
-		
+
 end
