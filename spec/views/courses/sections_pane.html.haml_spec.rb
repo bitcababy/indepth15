1c2d0f114d5df2680f2bf208dd23ca75b96be007
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-29 14:16:13 -0400

    Put in some section_pane tests

--- spec/views/courses/sections_pane.html.haml_spec.rb
+++ spec/views/courses/sections_pane.html.haml_spec.rb
@@ -1,5 +1,29 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
-describe "courses/sections_pane.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "courses/sections_pane" do
+	before :each do
+		@course = Fabricate(:course, full_name: "Fractals", number: 321, duration: :full_year, credits: 5.0)
+		@course.description = "<p>This is some info</p>"
+		4.times { Fabricate(:section, course: @course) }
+		assign(:course, @course)
+		render
+	end
+	
+	it "lists some basic info" do
+		rendered.should have_selector('div#sections_pane') do |div|
+			div.should have_selector('div#info', content: 'Course 321 — Full Year — 5.0 Credits')
+		end
+	end
+	
+	it "display the description" do
+		rendered.should have_selector('div#description') do |div|
+			div.should have_selector('p', content: 'This is some info')
+		end
+	end
+	
+	it "displays a leadin to the section table" do
+		rendered.should have_selector('div#leadin', content: "In the 2011—2012 academic year there are 4 sections of Fractals.")
+	end
 end
