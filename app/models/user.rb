8419c6324ae6375a67b3e65b2868cb59c52046da
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-31 16:29:01 -0400

    User: Added some validations. Added field aliases.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,52 +1,39 @@
 class User
 	include Mongoid::Document
   # include Mongoid::Timestamps
+	HONORIFICS = %W(Mr. Mrs. Ms Dr. Ms.)
 
 	field :ho, as: :honorific, type:String, default: "Mr."
 	field :fn, as: :first_name, type: String, default: ""
 	field :mn, as: :middle_name, type: String, default: ""
 	field :ln, as: :last_name, type: String, default: ""
-
-	def formal_name
-		return "#{self.honorific} #{self.last_name}"
-	end
 	
-	def full_name
-		return "#{self.first_name} #{self.last_name}"
-	end
-
-	def to_s
-		return "#{self.first_name} #{self.last_name}"
-	end
-
  # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :token_authenticatable,
-         :recoverable, :rememberable#, :trackable,
- 				 :timeoutable
-
+         :recoverable, :rememberable, :timeoutable#, :trackable,
+ 				 
   ## Database authenticatable
-  field :email,              :type => String
-	field :login, 						  type: String
-  field :encrypted_password, :type => String, :default => ""
+  field :em, as: :email, :type => String
+	field :lo, as: :login, type: String
+  field :ep, as: :encrypted_password, :type => String, :default => ""
 
-  validates_presence_of :email
   # validates_presence_of :encrypted_password # SMELL: breaks testing
 
   ## Recoverable
-  field :reset_password_token,   :type => String
-  field :reset_password_sent_at, :type => Time
+  field :rt, as: :reset_password_token,   :type => String
+  field :rp, as: :reset_password_sent_at, :type => Time
 
   ## Rememberable
-  field :remember_created_at, :type => Time
+  field :rm, as: :remember_created_at, :type => Time
 
   ## Trackable
-  field :sign_in_count,      :type => Integer, :default => 0
-  field :current_sign_in_at, :type => Time
-  field :last_sign_in_at,    :type => Time
-  field :current_sign_in_ip, :type => String
-  field :last_sign_in_ip,    :type => String
+  field :sic, as: :sign_in_count,      :type => Integer, :default => 0
+  field :csi, as: :current_sign_in_at, :type => Time
+  field :lsi, as: :last_sign_in_at,    :type => Time
+  field :csii, as: :current_sign_in_ip, :type => String
+  field :lsii, as: :last_sign_in_ip,    :type => String
 
   ## Confirmable
   # field :confirmation_token,   :type => String
@@ -61,4 +48,23 @@ class User
 
   ## Token authenticatable
   field :authentication_token, :type => String
+
+	validates :honorific, presence: true#, inclusion: { in: HONORIFICS }
+	validates :first_name, presence: true#, length: { minimum: 1 }#, format: {with: /[A-Z][a-z\-]+}/}
+	validates :last_name, presence: true#, length: { minimum: 2 }
+	validates :email, presence: true#, uniqueness: true
+	validates :login, presence: true#, uniqueness: true, length: { minimum: 5 }
+
+	def formal_name
+		return "#{self.honorific} #{self.last_name}"
+	end
+	
+	def full_name
+		return "#{self.first_name} #{self.last_name}"
+	end
+
+	def to_s
+		return "#{self.first_name} #{self.last_name}"
+	end
+
 end
