e3fe29e892176682bcb86d629cdd5754d1e92b53
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-31 16:26:14 -0400

    First cut at sections controller

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,9 +1,88 @@
 class SectionsController < ApplicationController
-	before_filter :find_section, only: []
+	before_filter :find_user, only: [:show, :edit, :update, :destroy]
+
+ # GET /sections
+  # GET /sections.json
+  def index
+    @sections = Section.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @sections }
+    end
+  end
+
+  # GET /sections/1
+  # GET /sections/1.json
+  def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @section }
+    end
+  end
+
+  # GET /sections/new
+  # GET /sections/new.json
+  def new
+    @section = Section.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @section }
+    end
+  end
+
+  # GET /sections/1/edit
+  def edit
+  end
+
+  # POST /sections
+  # POST /sections.json
+  def create
+		puts params[:section]
+
+    @section = Section.new(params[:section])
+
+    respond_to do |format|
+      if @section.save
+        format.html { redirect_to @section, notice: 'Section was successfully created.' }
+        format.json { render json: @section, status: :created, location: @section }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @section.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /sections/1
+  # PUT /sections/1.json
+  def update
+    respond_to do |format|
+      if @section.update_attributes(params[:section])
+        format.html { redirect_to @section, notice: 'Section was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @section.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /sections/1
+  # DELETE /sections/1.json
+  def destroy
+    @section.destroy
+
+    respond_to do |format|
+      format.html { redirect_to sections_url }
+      format.json { head :no_content }
+    end
+  end
 	
  	private
 	def find_section
 		n = params[:id]
 		@section = Section.find(n)
 	end
+
 end
