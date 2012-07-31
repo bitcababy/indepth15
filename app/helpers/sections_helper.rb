347c0f5c93e7ad3d379137fc76edbef702fe1cc1
Meredith Lesly <bitcababy+github@gmail.com>
2012-07-31 16:26:44 -0400

    SectionsHelper: Added #occurrence_options method

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -4,4 +4,8 @@ module SectionsHelper
 	def assignments_header(section)
 		return "#{section.teacher.formal_name}'s #{academic_year_string(Settings.academic_year)} Assignments for Block #{section.block}"
 	end
+	
+	def occurrence_options(section=nil)
+		(1..Settings.max_occurrences).to_a.collect {|i| [true, i.to_s]}
+	end
 end
