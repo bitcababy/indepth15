3f9890976890ae556913b0680a9856c9f6a802e7
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-06 10:07:01 -0400

    Moved signin/signout code to _login_item

--- spec/views/menus/_home.html.haml_spec.rb
+++ spec/views/menus/_home.html.haml_spec.rb
@@ -1,16 +1,5 @@
 require 'spec_helper'
 
 describe 'menus/_home' do
-	before :each do
-		render partial: 'menus/home'
-	end
-
-	it "has a signin/signout item" do
-		rendered.should have_selector('li a', href: sign_in_path)
-	end
-
-	it "has an about item" do
-		rendered.should have_selector('li a', href: about_path)
-	end
 		
 end
