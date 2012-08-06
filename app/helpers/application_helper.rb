a9b1602454779cae063e2a44475942bfebf078c8
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-06 10:00:56 -0400

    Revert "Moved stuff from devise_helper to application_Helper"
    This reverts commit 6339c47a6a3adfe34a2fbb49ae37cff7f96c5174.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -74,7 +74,6 @@ module ApplicationHelper
 		end
 	end
 	
-	# Devise stuff
 	def devise_mapping
 		Devise.mappings[:user]
 	end
@@ -86,23 +85,6 @@ module ApplicationHelper
 	def resource_class
 		devise_mapping.to
 	end
-	
-	def authenticate_user!(opts={})
-		opts[:scope] = :user
-		warden.authenticate!(opts) if !devise_controller? || opts.delete(:force)
-	end
 
-	def current_user
-		@current_user ||= warden.authenticate(:scope => :user)
-	end
-
-	def user_signed_in?
-		!!current_user
-	end
-
-	def user_session
-		current_user && warden.session(:user)
-	end
-	
-
+			
 end
