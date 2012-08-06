bb7d754a070bf5bd6a092c589516136e2c3bd054
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-06 10:04:06 -0400

    Uncommented devise_error_messages

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -5,37 +5,37 @@ module DeviseHelper
   #
   # This method is intended to stay simple and it is unlikely that we are going to change
   # it to add more behavior or options.
-  # def devise_error_messages!
-  #   return "" if resource.errors.empty?
-  # 
-  #   messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-  #   sentence = I18n.t("errors.messages.not_saved",
-  #                     :count => resource.errors.count,
-  #                     :resource => resource.class.model_name.human.downcase)
-  # 
-  #   html = <<-HTML
-  #   <div id="error_explanation">
-  #     <h2>#{sentence}</h2>
-  #     <ul>#{messages}</ul>
-  #   </div>
-  #   HTML
-  # 
-  #   html.html_safe
-  # end
-
-	def authenticate_user!(opts={})
+  def devise_error_messages!
+     return "" if resource.errors.empty?
+   
+     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+     sentence = I18n.t("errors.messages.not_saved",
+                       :count => resource.errors.count,
+                       :resource => resource.class.model_name.human.downcase)
+   
+     html = <<-HTML
+     <div id="error_explanation">
+       <h2>#{sentence}</h2>
+       <ul>#{messages}</ul>
+     </div>
+     HTML
+   
+     html.html_safe
+   end
+ 
+	def autenticate_user!(opts={})
 		opts[:scope] = :user
 		warden.authenticate!(opts) if !devise_controller? || opts.delete(:force)
 	end
-
+	
 	def user_signed_in?
 		!!current_user
 	end
-
+	
 	def current_user
 		@current_user ||= warden.authenticate(:scope => :user)
 	end
-
+	
 	def user_session
 		current_user && warden.session(:user)
 	end
