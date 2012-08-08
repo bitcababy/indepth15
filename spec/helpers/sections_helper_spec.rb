eef7233c12860c3b4b1ba7a34d1d3fa65a65068d
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-08 18:18:57 -0400

    Restored test for SectionsHelper

--- spec/helpers/sections_helper_spec.rb
+++ spec/helpers/sections_helper_spec.rb
@@ -3,14 +3,15 @@
 require 'spec_helper'
 
 describe SectionsHelper do
-	
+	helper ApplicationHelper
+
 	describe '#occurrence_options'
 
-# 	describe '#assignments_header' do
-# 		it "shows the teacher's name, the academic year, and the block" do
-# 			teacher = Fabricate :teacher, honorific: "Mr.", last_name: "Smith"
-# 			section = Fabricate :section, teacher: teacher, block: "B"
-# 			assignments_header(section).should == "Mr. Smith's 2011–2012 Assignments for Block B"
-# 		end
-# 	end
+	describe '#assignments_header' do
+		it "shows the teacher's name, the academic year, and the block" do
+			teacher = Fabricate :teacher, honorific: "Mr.", last_name: "Smith"
+			section = Fabricate :section, teacher: teacher, block: "B"
+			assignments_header(section).should == "Mr. Smith's 2011—2012 Assignments for Block B"
+		end
+	end
 end
