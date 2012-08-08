408222bc00c83fb6bbc4365ed4d52bfb026765ba
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-08 18:06:32 -0400

    Changed Utils to be more useful as include
    Probably not the right place for these methods, but good enough for now.

--- extras/utils.rb
+++ extras/utils.rb
@@ -1,6 +1,8 @@
+# encoding: UTF-8
 module Utils
+	extend ActiveSupport::Concern
 
-	class << self
+	module ClassMethods
 		def before_cutoff?(time)
 			return !after_cutoff?(time)
 		end
@@ -8,7 +10,7 @@ module Utils
 		def after_cutoff?(time)
 			return time.hour > Settings.cutoff.hour || (time.hour == Settings.cutoff.hour && time.min >= Settings.cutoff.minute)
 		end
-	
+
 		def future_due_date
 			time = Time.now
 			if after_cutoff?(time)
@@ -30,4 +32,33 @@ module Utils
 		end
 
 	end
+
+	def before_cutoff?(time)
+		return !after_cutoff?(time)
+	end
+
+	def after_cutoff?(time)
+		return time.hour > Settings.cutoff.hour || (time.hour == Settings.cutoff.hour && time.min >= Settings.cutoff.minute)
+	end
+
+	def future_due_date
+		time = Time.now
+		if after_cutoff?(time)
+			Date.tomorrow
+		else
+			Date.today
+		end
+	end
+
+	def next_school_day
+		case
+		when day.friday?
+			day + 3
+		when day.saturday?
+			day + 2
+		else
+			day + 1
+		end
+	end
+
 end
