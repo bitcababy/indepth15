21765f75b66dfe7be105aefab00d27ba27ed1df0
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-08 17:51:57 -0400

    Added REST methods to assignments controller

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,4 +1,5 @@
 class AssignmentsController < ApplicationController
+	before_filter :find_assignment, only: [:show, :update, :edit, :delete]
 	# before_filter :authenticate_user!, except: [:page]
 	
 	def page
@@ -18,7 +19,88 @@ class AssignmentsController < ApplicationController
 		respond_to do |format|
       format.html
     end
-		
+	end
+
+	# GET /assignments
+  # GET /assignments.json
+  def index
+    @assignments = Assignment.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @assignments }
+    end
+  end
+
+  # GET /assignments/1
+  # GET /assignments/1.json
+  def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @assignment }
+    end
+  end
+
+  # GET /assignments/new
+  # GET /assignments/new.json
+  def new
+    @assignment = Assignment.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @assignment }
+    end
+  end
+
+  # GET /assignments/1/edit
+  def edit
+  end
+
+  # POST /assignments
+  # POST /assignments.json
+  def create
+    @assignment = Assignment.new(params[:assignment])
+
+    respond_to do |format|
+      if @assignment.save
+        format.html { redirect_to @assignment, notice: 'Assignment was successfully created.' }
+        format.json { render json: @assignment, status: :created, location: @assignment }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @assignment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /assignments/1
+  # PUT /assignments/1.json
+  def update
+    respond_to do |format|
+      if @assignment.update_attributes(params[:assignment])
+        format.html { redirect_to @assignment, notice: 'Assignment was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @assignment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /assignments/1
+  # DELETE /assignments/1.json
+  def destroy
+    @assignment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to assignments_url }
+      format.json { head :no_content }
+    end
+  end
+
+ 	private
+	def find_assignment
+		n = params[:id]
+		@assignment = Assignment.find(n)
 	end
 
 end
