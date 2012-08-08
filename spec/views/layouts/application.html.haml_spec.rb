0c963de79c53466ef3f134d347d7060ce6299f86
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-08 18:18:01 -0400

    Added a bunch of empty spec files

---
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe 'application' do
+	it "provides the layout for the application"
+end
+
---
+++ spec/views/user_session/sign_out.html.haml_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "user_session/sign_out.html.haml" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
