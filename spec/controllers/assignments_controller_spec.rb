ff194b228229c595439a2ec4a81b62aff720ea65
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-09 15:40:54 -0400

    Added preliminary test for AssignmentsController

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -1,2 +1,14 @@
 require 'spec_helper'
 
+describe AssignmentsController do
+	context "guest accessible areas"
+	context "teacher accessible areas"
+	context "admin accessible areas" do
+		login_teacher
+			
+		it "should get index" do
+			get 'index'
+			response.should be_success
+		end
+	end
+end
