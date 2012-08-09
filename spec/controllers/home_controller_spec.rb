d2176b38c4c7d39161cd8f129ab9913625a3bfad
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-09 15:41:45 -0400

    Removed fossil code from HomeController spec.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,11 +2,4 @@ require 'spec_helper'
 
 describe HomeController do
 	
-  describe "GET 'about'" do
-    it "returns http success" do
-      get 'about'
-      response.should be_success
-    end
-  end
-
 end
