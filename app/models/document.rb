0416c3ce70062916c9a32f0717a6f01553f3cd41
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-09 15:35:54 -0400

    Changed field :document_type to :type

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,12 +3,13 @@ class Document
 	include Mongoid::Paranoia
   # include Mongoid::Timestamps
 
-	field :dt, as: :document_type, type: Symbol
+	field :dt, as: :type, type: Symbol, default: :unknown
 	field :ma, as: :major_tags, type: Array, default: []
 	field :mi, as: :minor_tags, type: Array, default: []
 	
 	belongs_to :owner, polymorphic: true
-		
+	
+	ASSIGNMENT_TYPE = :assignment
 	TYPES = [:assignment, :worksheet, :test, :quiz, :retake, :benchmark, :classwork, :homework]
 
 end
