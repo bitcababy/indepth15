443ac092b7f4f607995ffd14db2d6d46cadd15ba
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-09 15:35:33 -0400

    Added Devise methods to UserSessionHelper

--- app/helpers/user_session_helper.rb
+++ app/helpers/user_session_helper.rb
@@ -1,2 +1,34 @@
+require 'warden'
+
 module UserSessionHelper
+ 
+	def resource_class
+		User
+	end
+
+	def resource_name
+		:user
+	end
+
+	def resource
+		@resource || User.new
+	end
+
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+	end
+	
+	def resource_name
+		devise_mapping.name
+	end
+	
+	def resource_class
+		devise_mapping.to
+	end
+
+	def authenticate_user!(opts={})
+		opts[:scope] = :user
+		warden.authenticate!(opts) if !devise_controller? || opts.delete(:force)
+	end
+
 end
