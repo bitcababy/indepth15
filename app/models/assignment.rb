65a14ba25c27c0ae80a42e59d03a066240349ce2
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-09 15:34:13 -0400

    Assignment: set @type to :assignment

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -4,6 +4,7 @@ class Assignment < TextDocument
 	def self.import_from_hash(hash)
 		coder = HTMLEntities.new
 		hash[:content] = coder.decode(hash[:description])
+		hash[:type] = :assignment
 		author = Teacher.find_by(login: hash[:teacher_id])
 		raise "Couldn't find teacher #{hash[:teacher_id]}" unless author
 		[:description, :locked, :number, :teacher_id, :year].each { |k| hash.delete(k) }
