afc83ca50339f25943f8bdc44fa106ede3370d88
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-09 15:26:57 -0400

    AssignmentsController: Got rid of #page method, which was moved to Sections

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,26 +1,7 @@
 class AssignmentsController < ApplicationController
 	before_filter :find_assignment, only: [:show, :update, :edit, :delete]
-	# before_filter :authenticate_user!, except: [:page]
+	before_filter :authenticate_user!
 	
-	def page
-		course_number = params['course_number'].to_i
-		block = params['block']
-		year = params['year'] || Settings.academic_year
-	
-		course = Course.find_by(number: course_number)
-		rails "Course not found: #{course_number}" unless course
-		@section = course.sections.where(academic_year: year, block: block).first
-		raise "section not found: #{course_number}/#{block}" unless @section
-
-		@upcoming_assignments = @section.section_assignments.upcoming.asc(:due_date)
-		@current_assignment =  @section.section_assignments.current
-		@past_assignments = @section.section_assignments.past.desc(:due_date).limit(Settings.past_assts_num)
-
-		respond_to do |format|
-      format.html
-    end
-	end
-
 	# GET /assignments
   # GET /assignments.json
   def index
