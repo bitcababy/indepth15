0d9c29a0539e69e5b59949d531c7fbe7a397685b
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-10 18:24:44 -0400

    Removed unnecessary assigns from sections#assignments

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,12 +2,9 @@ class SectionsController < ApplicationController
 
 	def assignments
 		@section = Section.find(params['id'])
-		@upcoming_assignments = @section.section_assignments.upcoming.asc(:due_date)
-		@current_assignment =  @section.section_assignments.current
-		@past_assignments = @section.section_assignments.past.desc(:due_date).limit(Settings.past_assts_num)
 		
     respond_to do |format|
-      format.html # index.html.haml
+      format.html # assignments.html.haml
     end
 	end
 
