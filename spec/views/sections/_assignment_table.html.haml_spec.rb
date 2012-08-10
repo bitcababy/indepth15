f79789c8895930267e54892a4396b2cc03c93548
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-10 18:32:04 -0400

    Changed _assignment_table spec to match actual

--- spec/views/sections/_assignment_table.html.haml_spec.rb
+++ spec/views/sections/_assignment_table.html.haml_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'sections/_assignment_table' do
 	it "display a table with the provided assignments" do
-		render partial: 'sections/assignment_table', locals: {assignments: []}
+		render partial: 'sections/assignment_table', locals: {sas: []}
 		rendered.should have_selector('thead tr') do |row|
 			['#', 'Date due', 'Assignment'].each {|hdr| row.should have_selector('th', content: hdr)}
 		end
