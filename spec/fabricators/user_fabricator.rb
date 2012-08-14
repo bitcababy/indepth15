e81ee37d5b3dcdd931d3e30c033bc68f79c4798b
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-14 13:52:20 -0400

    Fixed user fabricator to (hopefully) create unique logins

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -5,43 +5,31 @@ Fabricator :user do
 	authentication_token		"user"
 	login										nil
 	email										nil
-	# role										{Fabricate :role, name: :registered_user}
+	suffix									{ sequence }
+	# role									{Fabricate :role, name: :registered_user}
 	after_build							{ |obj|
-		obj.login ||= (obj.last_name + obj.first_name.first).downcase
+		obj.login ||= (obj.last_name + obj.first_name.first).downcase + obj.suffix.to_s
 		obj.email ||= obj.login + "@example.com"
 		}
-	
 end
+
+# Fabricate.sequence :first_name, {|i| }
 
 Fabricator :author, from: :user, class_name: :author do
 	# role										{Fabricate :role, name: :author}
 	authentication_token		'author'
 end
 
-Fabrication::Transform.define(:author, lambda{|full_name|
-	name = full_name.split(" ")
-	if Author.where(first_name: name[0], last_name: name[1]).exists? then
-		Author.find_by(first_name: name[0], last_name: name[1])
-	else
-		Fabricate(:author, first_name: name[0], last_name: name[1])
-	end
-})
-
 Fabricator :guest, from: :user  do
 	# role										{Fabricate :role, name: :guest}
 end
 
-Fabricator :teacher do
-	# role										{Fabricate :role, name: :teacher}
-	honorific								{ %W(Mr. Mrs. Ms Dr.).sample }
-	first_name							{ %W(John Jane Jake Dan Larry).sample }
-	last_name								{ %W(Black White Orange Red).sample }
-	after_build						{ |obj|
-		obj.login ||= (obj.last_name + obj.first_name.first).downcase
-		obj.email ||= obj.login + "@example.com"
-		}
+Fabricator :teacher, from: :user, class_name: :teacher do
 	current									true
 	authentication_token		'teacher'
+	general_msg							"This is the general leadin"
+	current_msg							"This is the current leadin"
+	upcoming_msg						"This is the current leadin"
 end
 
 Fabrication::Transform.define(:teacher, lambda{|full_name|
@@ -53,24 +41,39 @@ Fabrication::Transform.define(:teacher, lambda{|full_name|
 	end
 })
 
-# Fabrication::Transform.define :general_msg, lambda{|txt| Fabricate :teacher_page, content: txt}
-# Fabrication::Transform.define :current_msg, lambda{|txt| Fabricate :teacher_page, content: txt}
-	
-Fabricator :test_teacher, from: :teacher do
-	honorific					"Mr."
-	first_name				"John"
-	last_name					"Doe"
-	email							"teacher@example.com"
-	authentication_token	'test_teacher'
-	general_leadin		"This is the general leadin"
-	current_leadin		"This is the current leadin"
+Fabricator :test_teacher, from: :teacher, class_name: :teacher do
+	email										"teacher@example.com"
+	login										'test_teacher'
+	authentication_token		'test_teacher'
 end
 
-Fabricator :test_admin, from: :author  do
+Fabricator :test_admin, from: :user, class_name: :admin  do
 	honorific					{ %W(Mr. Mrs. Ms. Dr.).sample }
 	first_name				{ %W(John Jane Jake Dan Larry).sample }
 	last_name					{ %W(Black White Orange Red).sample }
 	current						true
-	authentication_token	'admin'
+	login							'test_admin'
+	authentication_token	'test_admin'
 end
+
+Fabrication::Transform.define(:teacher, lambda { |formal_name|
+		h,l = formal_name.split(' ')
+		cond = {honorific: h.strip, last_name: l.strip}
+		if Teacher.where(cond).exists?
+			Teacher.find_by(cond)
+		else
+			Fabricate(:teacher, honorific: h, last_name: l)
+		end
+	})
+
+	Fabrication::Transform.define(:author, lambda{|full_name|
+		name = full_name.split(" ")
+		if Author.where(first_name: name[0], last_name: name[1]).exists? then
+			Author.find_by(first_name: name[0], last_name: name[1])
+		else
+			Fabricate(:author, first_name: name[0], last_name: name[1])
+		end
+	})
+
+
 
