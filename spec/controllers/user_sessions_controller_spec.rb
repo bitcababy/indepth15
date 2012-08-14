3080ad8a078c8b1710a2efec3460a395c369982c
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-14 10:18:28 -0400

    Added specs for a user signing in and out

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -2,5 +2,19 @@ require 'spec_helper'
 
 describe UserSessionsController do
 
+	# This tries to sign in a user
+	describe "UPDATE 'create'" do
+		it "signs in a user if the login and password are correct"
+		it "does something if either the login or password are incorrect"
+	end
+	
+	describe "GET 'new" do
+		it "presents a sign-in dialog"
+	end
+	
+	describe "DELETE 'destroy'" do
+		it "signs out the current user"
+	end
+	
 
 end
