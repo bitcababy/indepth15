6991596fd2e9e5b7478f4f8e208c2897e433c30b
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-15 13:40:47 -0400

    Cleared fossil methods out of CoursesController

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,53 +1,50 @@
 require 'spec_helper'
 
 describe CoursesController do
-	context "guest accessible" do
-		before :each do
-			@course = Fabricate :course
-		end
-
-	  describe "GET 'home'" do
-	    it "returns http success" do
-	      get 'home', id: @course.to_param
-	      response.should be_success
-	    end
-	  end
-
-	  describe "GET 'resources_pane'" do
-	    it "returns http success" do
-				get 'resources_pane', id: @course.to_param
-	      response.should be_success
-	    end
-	  end
-
-	  describe "GET 'information_pane'" do
-	    it "returns http success" do
-				get 'information_pane', id: @course.to_param
-	      response.should be_success
-	    end
-	  end
-
-	  describe "GET 'sections_pane'" do
-	    it "returns http success" do
-				get 'sections_pane', id: @course.to_param
-	      response.should be_success
-	    end
-	  end
-
-	  describe "GET 'news_pane'" do
-	    it "returns http success" do
-				get 'news_pane', id: @course.to_param
-	      response.should be_success
-	    end
-	  end
-
-	  describe "GET 'policies_pane'" do
-	    it "returns http success" do
-				get 'policies_pane', id: @course.to_param
-	      response.should be_success
-	    end
-	  end
-
+	before :each do
+		@course = Fabricate :course
 	end
+
+  describe "GET 'show'" do
+    it "returns http success" do
+      get 'show', id: @course.to_param
+      response.should be_success
+    end
+  end
+
+  describe "GET 'resources_pane'" do
+    it "returns http success" do
+			get 'resources_pane', id: @course.to_param
+      response.should be_success
+    end
+  end
+
+  describe "GET 'information_pane'" do
+    it "returns http success" do
+			get 'information_pane', id: @course.to_param
+      response.should be_success
+    end
+  end
+
+  describe "GET 'sections_pane'" do
+    it "returns http success" do
+			get 'sections_pane', id: @course.to_param
+      response.should be_success
+    end
+  end
+
+  describe "GET 'news_pane'" do
+    it "returns http success" do
+			get 'news_pane', id: @course.to_param
+      response.should be_success
+    end
+  end
+
+  describe "GET 'policies_pane'" do
+    it "returns http success" do
+			get 'policies_pane', id: @course.to_param
+      response.should be_success
+    end
+  end
 
 end
