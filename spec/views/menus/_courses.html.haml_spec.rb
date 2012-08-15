88415c7f23e293cc6c26a967375a377509fa75bd
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-15 13:42:08 -0400

    Added names for course to help testing in menus/_courses

--- spec/views/menus/_courses.html.haml_spec.rb
+++ spec/views/menus/_courses.html.haml_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe 'menus/_courses' do
-	before :each do
-		courses = []
-		courses << Fabricate(:course)
-		courses << Fabricate(:course)
-		render partial: 'menus/courses', locals: {courses: courses}
-	end
-
 	it "renders a list for each course" do
+		courses = []
+		courses << Fabricate(:course, full_name: "Fractals")
+		courses << Fabricate(:course, full_name: "Fubar")
+		render partial: 'menus/courses', locals: {courses: courses}
 		rendered.should have_selector('li.courses') do |li|
 			li.should have_selector('a', href: '#', content: 'Courses')
-			li.should have_selector('ul.course-list')
+			li.should have_selector('ul.course-list') do |ul|
+				ul.should have_selector('a', content: "Fractals")
+				ul.should have_selector('a', content: "Fubar")
+			end
 		end
 	end
 end
