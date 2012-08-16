1f54383b8a8cf29d5add0fc330d8c868fb566e10
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-16 16:08:57 -0400

    Fixed section requests spec

--- spec/requests/sections_spec.rb
+++ spec/requests/sections_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
-describe "Sections" do
-	pending "Unfinished test"
+describe 'Sections' do
+	it "shows an assignments page" do
+		section = Fabricate(:section)
+		get assignments_page_path(section)
+		response.should render_template(:assignments)
+	end
 end
