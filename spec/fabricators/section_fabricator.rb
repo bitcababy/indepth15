7a6abb9d2c0c33c5487ee14cf60256bd354270c9
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-16 16:07:34 -0400

    Changed section transform to go by :in_block, rather than :block
    It was interfering with specifying the block

--- spec/fabricators/section_fabricator.rb
+++ spec/fabricators/section_fabricator.rb
@@ -1,16 +1,22 @@
 Fabricator(:section) do
-	block					{ sequence(:block) {|i| Settings.blocks[i%Settings.blocks.length] } }
-	teacher				{ Fabricate :teacher }
 	academic_year	{ Settings.academic_year }
 	semester			{ Section::SEMESTERS.sample }
+	block					{ Settings.blocks.sample }
+	teacher
+	course
+	after_build	{ |obj|
+		# obj.teacher ||= Fabricate(:teacher)
+		# obj.course ||= Fabricate(:course)
+		}
 end
 
 Fabricate.sequence(:block) {|i| Settings.blocks[i%Settings.blocks.length] }
 
-Fabrication::Transform.define(:block, lambda {|b| 
+Fabrication::Transform.define(:in_block, lambda {|b| 
 	if Section.where(block: b).exists?
 		Section.find_by(block: b)
 	else
 		Fabricate(:section, block: b)
 	end
 	})
+	
