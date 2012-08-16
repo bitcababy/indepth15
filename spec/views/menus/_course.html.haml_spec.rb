4a0c30153816c18f7cc6d138d25d586caed06909
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-16 16:09:42 -0400

    Kinda fixed menus/_course

--- spec/views/menus/_course.html.haml_spec.rb
+++ spec/views/menus/_course.html.haml_spec.rb
@@ -2,14 +2,17 @@ require 'spec_helper'
 
 describe 'menus/_course' do
 	it "displays a course and its sections" do
-		sections = mock do
-		end
-		course = mock do
+		sections = []
+		course = mock('course') do
 			stubs(:full_name).returns "Fractals 101"
 			stubs(:current_sections).returns sections
+			stubs(:number).returns 321
 		end
 		render partial: 'menus/course', locals: {course: course}
-		rendered.should have_selector('li.course', content: 'Fractals 101')
+		rendered.should have_selector('li.course', content: 'Fractals 101') do |li|
+			pending "This needs a real model."
+			# li.should have_selector('a', href: '/courses/321')
+		end
 	end
 
 end
