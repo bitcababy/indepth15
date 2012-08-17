e913949b548b3e275bea9ea32bbf1275f15195b2
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-17 11:56:46 -0400

    Added app/assets/stylesheets/shared to load_paths

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DepthCharge::Application.initialize!
+Sass.load_paths << Rails.root.join('app/assets/stylesheets', 'shared')
