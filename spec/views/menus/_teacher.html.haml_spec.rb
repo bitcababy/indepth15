4595fa266820d24cba81e3f44b4b2d51e723fd58
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-20 10:36:27 -0400

    Cleaned up _teacher.html spec

--- spec/views/menus/_teacher.html.haml_spec.rb
+++ spec/views/menus/_teacher.html.haml_spec.rb
@@ -4,9 +4,7 @@ describe 'menus/_teacher' do
 	it "should display a link to the teachers home page" do
 		teacher = Fabricate(:teacher)
 		render partial: 'menus/teacher', locals: {teacher: teacher}
-		rendered.should have_selector('li.home-page') do |li|
-			li.should have_selector('a', href: home_teacher_path(teacher))
-		end
+		rendered.should have_selector('a', href: teacher_path(teacher))
 	end
 			
 end
