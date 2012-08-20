350a9f28aad686c0cbab5e9497bc47ee235d5bbb
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-20 10:28:47 -0400

    Don't add branches to courses if there aren't any

--- app/models/course_observer.rb
+++ app/models/course_observer.rb
@@ -2,16 +2,12 @@ class CourseObserver < Mongoid::Observer
 	
 	def after_create(obj)
 		branches = Course::BRANCH_MAP[obj.number]
-		branches = [branches] unless branches.kind_of? Array
+		if branches
+			branches = [branches] unless branches.kind_of? Array
 
-		for branch in branches do
-			obj.branches.find_or_create_by(name: branch)
-		end
-		branches = Course::BRANCH_MAP[obj.number]
-		branches = [branches] unless branches.kind_of? Array
-
-		for branch in obj.branches do
-			obj.major_tags.concat branch.major_tags
+			for branch in branches do
+				obj.branches.find_or_create_by(name: branch)
+			end
 		end
 			
 		obj.create_information
