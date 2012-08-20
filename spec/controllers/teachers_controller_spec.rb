255ab3245c77c3142034bc98be4e1a85102503b8
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-20 10:34:12 -0400

    Fixed TeachersController spec to test 'show', not 'home'

--- spec/controllers/teachers_controller_spec.rb
+++ spec/controllers/teachers_controller_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe TeachersController do
 
-	describe "GET home" do
+	describe "GET show" do
 		it "returns http success" do
 			teacher = Fabricate :teacher
-			get :home, id: teacher.to_param
+			get :show, id: teacher.to_param
 			response.should be_success
 		end
 	end
