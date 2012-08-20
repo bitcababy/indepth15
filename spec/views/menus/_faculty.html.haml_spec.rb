08e13b3910cf4c332b8f406b56ab18e27c64847d
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-20 10:36:14 -0400

    Fixed _faculty.html spec ids

--- spec/views/menus/_faculty.html.haml_spec.rb
+++ spec/views/menus/_faculty.html.haml_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe 'menus/_faculty' do
 	it "displays a faculty list" do
-		render partial: 'menus/faculty'
+		render partial: 'menus/faculty', locals: {teachers: []}
 		rendered.should have_selector('li.faculty') do |li|
 			li.should have_selector('a', href: '#', content: 'Faculty')
-			li.should have_selector('ul#faculty-list')
+			li.should have_selector('ul#faculty_menu')
 		end
 	end
 end
