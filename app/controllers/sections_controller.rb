6f15c83b4a04a2af2fbd5cbe377fe39c5f19b4c6
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-26 09:49:23 -0400

    Added code to return json for assignment requests

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,20 +1,44 @@
 class SectionsController < ApplicationController
+	before_filter :find_section
 
 	def show
-		@section = Section.find(params['id'])
-		
     respond_to do |format|
       format.html {render :layout => !request.xhr?}
     end
 	end
 	
 	def assignments
-		@section = Section.find(params['id'])
-
     respond_to do |format|
       format.html {render :layout => !request.xhr?}
     end
 	end
-		
+	
+	def past_assignments
+		respond_to do |format|
+			format.html {render :layout => !request.xhr?}
+			format.json { render json: @section.past_assignments }
+    end
+	end
+
+	def current_assignment
+		respond_to do |format|
+      format.html {render :layout => !request.xhr?}
+			format.json { render json: @section.current_assignments }
+    end
+	end
+
+	def upcoming_assignments
+		limit = params['limit'] || 2
+		respond_to do |format|
+      format.html {render :layout => !request.xhr?}
+			format.json { render json: @section.upcoming_assignments }
+    end
+	end
+
+	protected
+	def find_section
+		@section = Section.find(params['id'])
+	end
+	
 
 end
