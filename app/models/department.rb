c17e87ba07b5daa5500e6d821aa77c47893b90c8
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-26 09:50:24 -0400

    Added Department associations and fields

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,15 @@
+# encoding: UTF-8
+
 class Department
   include Mongoid::Document
   include Mongoid::Timestamps if Rails.env == 'production'
+
+	field :n, as: :name, type: String
+
+	belongs_to :why_doc, class_name: 'TextDocument'
+	belongs_to :info_doc, class_name: 'TextDocument'
+	belongs_to :news_doc, class_name: 'TextDocument'
+	belongs_to :resources_doc, class_name: 'TextDocument'
+	belongs_to :puzzle_doc, class_name: 'TextDocument'
+	
 end
