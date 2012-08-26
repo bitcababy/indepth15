da4edfd3fe3d649a06b70cf23bc32a1fa51f34bb
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-26 12:27:15 -0400

    SectionsController: using globals for @past, @currrent, and @upcoming
    Not sure this is the right thing.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -8,12 +8,16 @@ class SectionsController < ApplicationController
 	end
 	
 	def assignments
+		@past = self.past_assignments
+		@current = self.current_assignment
+		@upcoming = self.upcoming_assignments
     respond_to do |format|
       format.html {render :layout => !request.xhr?}
     end
 	end
 	
 	def past_assignments
+		@assignments = self.past_assignments
 		respond_to do |format|
 			format.html {render :layout => !request.xhr?}
 			format.json { render json: @section.past_assignments }
@@ -21,6 +25,7 @@ class SectionsController < ApplicationController
 	end
 
 	def current_assignment
+		@assignments = self.current_assignment
 		respond_to do |format|
       format.html {render :layout => !request.xhr?}
 			format.json { render json: @section.current_assignments }
@@ -28,7 +33,7 @@ class SectionsController < ApplicationController
 	end
 
 	def upcoming_assignments
-		limit = params['limit'] || 2
+		@assignments = self.upcoming_assignments
 		respond_to do |format|
       format.html {render :layout => !request.xhr?}
 			format.json { render json: @section.upcoming_assignments }
@@ -40,5 +45,17 @@ class SectionsController < ApplicationController
 		@section = Section.find(params['id'])
 	end
 	
+	def past_assignments
+		return @section.past_assignments.desc(:due_date)
+	end
+	
+	def current_assignment
+		return @section.current_assignment
+	end
+	
+	def upcoming_assignments(limit=2)
+		limit ||= params['limit'] || 2
+		@section.upcoming_assignments.asc(:due_date).limit(limit)
+	end
 
 end
