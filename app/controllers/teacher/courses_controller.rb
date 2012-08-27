d3075725e82631074427c1dad533b080f70b95c2
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-27 14:50:44 -0400

    Got rid of the teacher controllers

--- app/controllers/teacher/courses_controller.rb
+++
@@ -1,8 +0,0 @@
-class Teacher::CoursesController < ApplicationController
-	before_filter :authenticate_user!
-	
-	def edit
-		
-	end
-	
-end
--- spec/controllers/teacher/courses_controller_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Teacher::CoursesController do
-	login_teacher
-	it_behaves_like "any teacher controller"
-
-end
