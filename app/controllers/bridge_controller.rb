9090d9762bf5521b72b0af4442d0f237d0840c95
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-28 14:03:39 -0400

    BridgeController: changed keys to symbols

--- app/controllers/bridge_controller.rb
+++ app/controllers/bridge_controller.rb
@@ -1,11 +1,21 @@
 class BridgeController < ApplicationController
+
 	def incoming
-		hash = params['wmdhs']
-		if v = hash["assignments"] then
+		hash = fix_hash params['wmdhs']
+		if v = hash[:assignments] then
 			Assignment.handle_incoming(v)
 		end
-		if v = hash["section_assignments"] then
+		if v = hash[:courses] then
+			Course.handle_incoming(fix_has())
+		end
+		if v = hash[:section_assignments] then
 			SectionAssignment.handle_incoming(v)
 		end
+	end
+	
+	def fix_hash(hash)
+		new_hash = Hash.new
+		hash.each {|k,v| new_hash[k.to_sym] = v} 
+		return new_hash
 	end
 end
