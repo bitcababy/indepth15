414be517b9082dd0a937386f240e1ce499c396c6
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-28 14:04:50 -0400

    Cleaned up import and convert code

--- app/models/section_assignment.rb
+++ app/models/section_assignment.rb
@@ -14,7 +14,6 @@ class SectionAssignment
 	belongs_to :assignment, index: true, inverse_of: nil
 	belongs_to :course, index: true
 
-	
 	scope :after,	->(date) { gt(due_date: date) }
 	scope :past, -> { lt(due_date: future_due_date) }
 	scope :future, -> { gte(due_date: future_due_date) }
@@ -29,63 +28,59 @@ class SectionAssignment
 		end
 	end
 	
-	def self.handle_incoming(hashes)
-		hashes = [hashes] unless hashes.kind_of? Array
-		for hash in hashes do
-			assgt_id = hash['assgt_id']
-			year = hash['year']
-			course_num = hash['course_num'].to_i
-			block = hash['block']
-			name = hash['name']
-			due_date = Date.parse(hash['due_date'])
-			teacher_id = hash['teacher_id']
-			course = Course.find_by(number: course_num)
-			raise ArgumentError, "course is nil for #{course_num}" unless course
-			teacher = Teacher.find_by(login: teacher_id)
-			raise ArgumentError, "teacher is nil for #{hash['teacher_id']}" unless teacher
-			section = Section.find_by(course: course, block: block, academic_year: year, teacher: teacher)
-			raise ArgumentError, "can't find section for #{year}/#{teacher}/#{block}" unless section
-			assignment = Assignment.find_by(assgt_id: assgt_id)
-			raise ArgumentError,  "can't find assignment for #{year}/#{teacher}/#{block}/#{assgt_id}" unless assignment
+	def self.get_sa(hash)
+		assgt_id = hash.delete(:assgt_id)
+		year = hash[:year]
+		course_num = hash.delete(:course_num).to_i
+		block = hash[:block]
+		name = hash[:name]
+		teacher_id = hash.delete(:teacher_id)
+		course = Course.find_by(number: course_num)
+		raise "Course #{course_num} not found" unless course
+		teacher = Teacher.find_by(login: teacher_id)
+		raise "Course #{teacher_id} not found" unless teacher
+		section = Section.find_by(course: course, block: block, academic_year: year, teacher: teacher)
+		raise "Section #{course}/#{block}/#{teacher_id} not found" unless teacher
+		assignment = Assignment.find_by(assgt_id: assgt_id)
+		hash[:course] = course
+		return section,assignment
+	end
+	
+	def self.handle_incoming(hash)
+		hash = Hash[hash]
+		section,assignment = self.get_sa(hash)
 
-			if section.section_assignments.where(assignment: assignment, name: name).exists?
-				sa = section.section_assignments.find_by(assignment: assignment, name: name)
-				sa.due_date = due_date if sa.due_date != due_date
-				sa.name = hash['name'] unless sa.name = hash['name']
-				use = (hash['use_assgt'] == 'Y')
-				sa.use = use unless sa.use == use
-				sa.save!
-			else
-				sa = section.section_assignments.create! due_date: due_date, name: name, assignment: assignment, use: use
-				return sa
-			end
+		name = hash[:name]
+		hash[:use] = (hash.delete(:use_assgt) == 'Y')
+		[:ada, :aa].each {|k| hash.delete(k)}
+
+		puts section.section_assignments.first
+
+		# puts section.section_assignments.where(assignment: assignment, name: name).exists?
+		if section.section_assignments.where(assignment: assignment, name: name).exists?
+			puts hash
+			sa = section.section_assignments.find_by(assignment: assignment, name: name)
+			sa.update_attributes(hash)
+			# sa.due_date = due_date if sa.due_date != due_date
+			# sa.name = name unless sa.name = name
+			# sa.use = use unless sa.use == use
+			sa.save!
+		else
+			puts section.section_assignments.count
+			[:ada, :aa].each {|k| hash.delete(k)}
+			section.section_assignments.create! hash
+			puts section.section_assignments.count
 		end
 	end
 	
-	
 	def self.import_from_hash(hash)
-		assgt_id = hash[:assgt_id]
-		block = hash[:block]
-		year = hash[:year]
+		section,assignment = self.get_sa(hash)
 
-		course = Course.find_by(number: hash[:course_num])
-		raise ArgumentError, "course is nil for #{hash[:course_num]}" unless course
-		teacher = Teacher.find_by(login: hash[:teacher_id])
-		raise ArgumentError, "teacher is nil for #{hash[:teacher_id]}" unless teacher
-		section = Section.find_by(course: course, block: block, academic_year: year, teacher: teacher)
-		raise ArgumentError, "can't find section for #{year}/#{teacher}/#{block}" unless section
-		assignment = Assignment.find_by(assgt_id: assgt_id)
-		raise ArgumentError,  "can't find assignment for #{year}/#{teacher}/#{block}/#{assgt_id}" unless assignment
+		hash[:use] = (hash.delete(:use_assgt) == 'Y')
 
-		[:teacher_id, :assgt_id, :dept_id, :course_num, :ada, :aa].each {|k| hash.delete(k)}
-		hash[:course] = course
+		[:ada, :aa].each {|k| hash.delete(k)}
 		hash[:assignment] = assignment
-		sa = section.section_assignments.create! hash
-
-		sa.assignment = assignment
-		sa.course = course
-		sa.save!
-		return sa
+		section.section_assignments.create! hash
 	end
 
 
