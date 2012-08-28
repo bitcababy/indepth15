5afa4f0760e8412d45b23d66e3975ceb9e5153e2
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-28 14:04:34 -0400

    Cleaned up handle_incoming code

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,27 +1,29 @@
 class Assignment < TextDocument
 	field :assgt_id, type: Integer
+	
+	def self.fix_content(txt)
+		coder = HTMLEntities.new
+		coder.decode(txt)
+	end
 
 	def self.import_from_hash(hash)
-		coder = HTMLEntities.new
-		hash[:content] = coder.decode(hash[:content])
+		hash[:content] = self.fix_content(hash[:content])
 		author = Teacher.find_by(login: hash[:teacher_id])
 		raise "Couldn't find teacher #{hash[:teacher_id]}" unless author
 		[:teacher_id].each { |k| hash.delete(k) }
 		return Assignment.create! hash.merge(owner: author)
 	end
 	
-	def self.handle_incoming(hashes)
-		hashes = [hashes] unless hashes.kind_of? Array
-		for hash in hashes do
-			assgt_id = hash['assgt_id']
-			if self.where(assgt_id: assgt_id).exists?
-				asst = self.find_by(assgt_id: assgt_id)
-				asst.content = hash["content"]
-				asst.save!
-			else
-				author = Teacher.find_by(login: hash["teacher_id"])
-				Assignment.create! content: hash["content"], author: author, assgt_id: assgt_id
-			end
+	def self.handle_incoming(hash)
+		hash[:content] = self.fix_content(hash[:content])
+		assgt_id = hash[:assgt_id]
+		if self.where(assgt_id: assgt_id).exists?
+			asst = self.find_by(assgt_id: assgt_id)
+			asst.content = hash[:content]
+			asst.save!
+		else
+			hash[:owner] = Teacher.find_by(login: hash.delete(:teacher_id))
+			Assignment.create! hash
 		end
 	end
 			
