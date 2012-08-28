5b745f57627eda92e9b7da9dfedc69db8dc1a654
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-28 14:40:03 -0400

    Got rid of info_doc

--- app/models/department.rb
+++ app/models/department.rb
@@ -8,7 +8,6 @@ class Department
 
 	belongs_to :why_doc, class_name: 'TextDocument'
 	belongs_to :how_doc, class_name: 'TextDocument'
-	belongs_to :info_doc, class_name: 'TextDocument'
 	belongs_to :news_doc, class_name: 'TextDocument'
 	belongs_to :resources_doc, class_name: 'TextDocument'
 	belongs_to :puzzle_doc, class_name: 'TextDocument'
