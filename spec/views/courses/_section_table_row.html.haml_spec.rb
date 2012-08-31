b908735c3e306680730da8886af4fcfc6fe0d412
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-31 17:10:51 -0400

    _section_table_row_spec: removed days column

--- spec/views/courses/_section_table_row.html.haml_spec.rb
+++ spec/views/courses/_section_table_row.html.haml_spec.rb
@@ -5,12 +5,11 @@ describe 'courses/_section_table_row' do
 	
 	it "displays a row for a section" do
 		teacher = mock_teacher(formal_name: "Mr. Ed")
-		section = mock_section(block: "B", days_for_section: [1,2,3,4,5], room: "501", teacher: teacher)
+		section = mock_section(block: "B", room: "501", teacher: teacher)
 
 		render partial: 'courses/section_table_row', locals: {section: section}
 		rendered.should have_selector('tr') do |row|
 			row.should have_selector('td.block', content: "B")
-			row.should have_selector('td.days', content: "12345")
 			row.should have_selector('td.teacher', content: "Mr. Ed")
 			row.should have_selector('td.room', content: "5")
 		end
--- spec/views/courses/_sections_table.html.haml_spec.rb
+++ spec/views/courses/_sections_table.html.haml_spec.rb
@@ -10,7 +10,7 @@ describe 'courses/_sections_table' do
 		assign(:course, course)
 		render partial: 'courses/sections_table'
 		rendered.should have_selector('table#sections_table thead tr') do |row|
-			%W(block days teacher room).each do |hdr|
+			%W(block teacher room).each do |hdr|
 				row.should have_selector('th', class: hdr)
 			end
 		end
