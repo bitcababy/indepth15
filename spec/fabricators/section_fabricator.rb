e935a2c329e30c30d6155679f5f60963530c052d
Meredith Lesly <bitcababy+github@gmail.com>
2012-08-31 17:06:55 -0400

    Changed section fabricator to assign blocks in sequence, to avoid duplicates

--- spec/fabricators/section_fabricator.rb
+++ spec/fabricators/section_fabricator.rb
@@ -1,16 +1,10 @@
 Fabricator(:section) do
 	academic_year	{ Settings.academic_year }
 	semester			{ Section::SEMESTERS.sample }
-	block					{ Settings.blocks.sample }
-	teacher				{|attrs| attrs['teachers'] ? attrs['teachers'] : Fabricate(:teacher )}
-	course				{|attrs| attrs['course'] ? attrs['course'] : Fabricate(:course )}
-	after_build	{ |obj|
-		# obj.teacher ||= Fabricate(:teacher)
-		# obj.course ||= Fabricate(:course)
-		}
+	block					{ |attrs| attrs[:block] || sequence(:block) {|i| Settings.blocks[i%Settings.blocks.length]} }
+	teacher				{ |attrs| attrs[:teacher] || Fabricate(:teacher )}
+	course				{ |attrs| attrs[:course] || Fabricate(:course )}
 end
-
-Fabricate.sequence(:block) {|i| Settings.blocks[i%Settings.blocks.length] }
 
 Fabrication::Transform.define(:in_block, lambda {|b| 
 	if Section.where(block: b).exists?
