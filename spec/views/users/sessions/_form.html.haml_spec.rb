7d07a3b3f1fe44721787561643f09ee8aec5a925
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-01 16:34:39 -0400

    Added spec for sign-in form

---
+++ spec/views/users/sessions/_form.html.haml_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe '/users/sessions/new' do
+	it "renders a sign-in form" do
+		render
+		rendered.should have_selector('form', action: '/users/sign_in') do |f|
+			f.should have_selector('input', name: 'user[login]')
+			f.should have_selector('input', name: 'user[password]')
+			f.should have_selector('input', name: 'user[remember_me]')
+			f.should have_selector('div.form-actions') do |fa|
+				fa.should have_selector('input', name: 'commit', type: 'submit')
+			end
+		end
+	end
+			
+end
