05dd8fa24ade15e7c4729c83fabe5239ded0b324
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-02 13:55:58 -0400

    New AssignmentsController that includes #create

---
+++ app/controllers/assignments_controller.rb
@@ -0,0 +1,29 @@
+class AssignmentsController < ApplicationController
+ 	# POST Assignments
+  # POST assignments.json
+  protect_from_forgery except: [:create, :update]
+
+  def create
+		teacher_id = params['assignment'].delete('teacher_id')
+		teacher = Teacher.find_by(login: teacher_id)
+		if teacher
+			@assignment = Assignment.new(params['assignment'])
+		else
+			@assignment = nil
+		end
+
+    respond_to do |format|
+      if @assignment.save
+				teacher.assignments << @assignment
+        format.html { redirect_to assignment_url(@assignment), notice: 'Assignment was successfully created.' }
+				format.js
+        format.json { render json: @assignment, status: :created, location: @assignment }
+      else
+        format.html { render action: "new" }
+ 				format.js
+       	format.json { render json: @assignment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+end
