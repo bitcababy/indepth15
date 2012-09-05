ed72ddf86eeaf7af7dacfad037a96268c2b36028
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-05 10:42:00 -0400

    Fixed import_from_hash to handle both old and new assignments

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,51 +1,48 @@
 class Assignment < TextDocument
 	field :assgt_id, type: Integer
 	
-	def self.fix_content(txt)
-		coder = HTMLEntities.new
-		coder.decode(txt)
-	end
-
-	def self.import_from_hash(hash)
-		hash[:content] = self.fix_content(hash[:content])
-		author = Teacher.find_by(login: hash[:teacher_id])
-		raise "Couldn't find teacher #{hash[:teacher_id]}" unless author
-		[:teacher_id].each { |k| hash.delete(k) }
-		return Assignment.create! hash.merge(owner: author)
-	end
-	
-	def self.handle_incoming(hash)
-		hash[:content] = self.fix_content(hash[:content])
-		assgt_id = hash[:assgt_id]
-		if self.where(assgt_id: assgt_id).exists?
-			asst = self.find_by(assgt_id: assgt_id)
-			asst.content = hash[:content]
-			asst.save!
-		else
-			hash[:owner] = Teacher.find_by(login: hash.delete(:teacher_id))
-			Assignment.create! hash
-		end
-	end
-			
 	scope :dupes, ->(a) { where(:assgt_id.gt => a.assgt_id, :content => a.content) }
-	
 
 	index( {assgt_id: 1} )
 	scope :with_assgt_id, ->(i) {where(assgt_it: i)}
 
-	def self.cleanup
-		dupes = {}
-		skip = []
-		Assignment.asc(:assgt_id).each do |asst|
-			next if skip.include?(asst.assgt_id)
-			next if Assignment.dupes(asst).count == 0
-			ids = []
-			Assignment.dupes(asst).each {|a| ids << a.assgt_id}
-			print "."
-			skip += ids
-			dupes[asst.assgt_id ] = ids
+	class << self
+		def fix_content(txt)
+			coder = HTMLEntities.new
+			coder.decode(txt)
 		end
-		puts dupes
+
+		def import_from_hash(hash)
+			assgt_id = hash[:assgt_id]
+			hash[:content] = fix_content(hash[:content])
+			crit = self.with_assgt_id(assgt_id)
+			if crit.exists?
+				asst = crit.first
+				asst.content = hash[:content]
+				return asst.save!
+			else
+				author = Teacher.find_by(login: hash[:teacher_id])
+				raise "Couldn't find teacher #{hash[:teacher_id]}" unless author
+				[:teacher_id].each { |k| hash.delete(k) }
+				return Assignment.create! hash.merge(owner: author)
+			end
+		end
+			
+		# def cleanup
+		# 	dupes = {}
+		# 	skip = []
+		# 	Assignment.asc(:assgt_id).each do |asst|
+		# 		next if skip.include?(asst.assgt_id)
+		# 		next if Assignment.dupes(asst).count == 0
+		# 		ids = []
+		# 		Assignment.dupes(asst).each {|a| ids << a.assgt_id}
+		# 		print "."
+		# 		skip += ids
+		# 		dupes[asst.assgt_id ] = ids
+		# 	end
+		# 	puts dupes
+		# end
+		
 	end
 			
 end
