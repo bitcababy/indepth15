1ad68172c3cad4925c9d11c7ead542421e94998c
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-09 13:52:19 -0400

    BridgeController now creates XML.
    Not returning it yet

--- app/controllers/bridge_controller.rb
+++ app/controllers/bridge_controller.rb
@@ -1,8 +1,15 @@
 class BridgeController < ApplicationController
 
 	def import
-		Bridge.create_or_update_assignments
-		Bridge.create_or_update_sas
+		res = <<EOT
+<?xml version="1.0" encoding="utf-8"?>
+
+<sent>
+EOT
+		
+		res += Bridge.create_or_update_assignments
+		res += Bridge.create_or_update_sas
+		res += </sent>
 		head :success
 	end
 
