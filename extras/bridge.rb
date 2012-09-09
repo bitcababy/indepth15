94c81f0704f1b3c0c6b79334fbd6ef3678271928
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-09 13:51:48 -0400

    Removed unnecessary MySQL calls from Bridge

--- extras/bridge.rb
+++ extras/bridge.rb
@@ -8,36 +8,30 @@ module Bridge
 			return Mysql.connect "db1.xoala.net", 'whswriter', 'hyperbole', ((Rails.env == 'production')  ? 'whsmd' : 'whsmd_testing'), 3306, "/var/run/mysqld/mysqld.sock"
 		end
 		
-		def create_or_update_assignment(assgt_id)
+		def create_or_update_assignment(hash)
 			res = ""
-			if conn = connector
-				begin 
-					conn.query("SELECT assgt_id, teacher_id, content FROM assignments WHERE assgt_id=#{assgt_id}").each_hash do |hash|
-						if Assignment.where(assgt_id: assgt_id).exists?
-							asst = Assignment.find_by(assgt_id: assgt_id)
-							asst.content = hash['content']
-							asst.save!
-						else
-							Assignment.create! hash
-						end
-						conn.query("UPDATE assgts_status SET sent=1 WHERE assgt_id=#{assgt_id}")
-						res += <<EOT
-	<assgt_id>#{assgt_id}</assgt_id>
-EOT
-					end
-				ensure
-					conn.close
+			assgt_id = hash['assgt_id']
+			if Assignment.where(assgt_id: assgt_id).exists?
+				asst = Assignment.find_by(assgt_id: assgt_id)
+				if asst.content != hash['content'] 
+					asst.content = hash['content']
+					asst.save!
 				end
-				return res
+			else
+				Assignment.create! hash
 			end
+			res + <<EOT
+<assgt_id>#{assgt_id}</assgt_id>
+EOT
 		end
 				
 		def create_or_update_assignments
 			res = ""
 			if conn = connector
 				begin
-					conn.query("SELECT assgt_id FROM assgts_status WHERE sent=0").each do |rec|
-						res += create_or_update_assignment(rec[0].to_i)
+					conn.query("SELECT assignments.assgt_id, assignments.teacher_id, assignments.content FROM assgts_status, assignments WHERE assgts_status.sent=0 AND assgts_status.assgt_id=assignments.assgt_id").each_hash do |hash|
+						res += create_or_update_assignment(hash)
+						conn.query("UPDATE assgts_status SET sent=1 WHERE assgt_id=#{hash['assgt_id']}")
 					end
 				ensure
 					conn.close
@@ -49,40 +43,33 @@ EOT
 		
 		def create_or_update_sa(hash)
 			res = ""
-			if conn = connector
-				begin
-					section = Section.find_by course_id: hash['course_num'].to_i, teacher_id: hash['teacher_id'], block: hash['block']
-					raise "Missing section #{hash}" unless section
-					adid = hash['id']
-					conn.query("SELECT due_date, assgt_id, use_assgt FROM section_assignments WHERE id=#{adid}").each_hash do |hash2|
-						hash['use'] = hash.delete('use_assgt') == 'Y'
-						raise "assignment #{hash['assgt_id']} doesn't exist" unless Assignment.where(assgt_id: hash['assgt_id'].to_i).exists?
+			hash['academic_year'] = hash['academic_year'].to_i
+			hash['course_id'] = hash['course_id'].to_i
+			section = Section.find_by course_id: hash['course_id'], teacher_id: hash['teacher_id'], block: hash['block'], academic_year: hash['academic_year']
+			raise "Missing section #{hash}" unless section
+			raise "assignment #{hash['assgt_id']} doesn't exist" unless Assignment.where(assgt_id: hash['assgt_id'].to_i).exists?
 
-						if section.section_assignments.where(assgt_id: hash2['assgt_id']).exists?
-							sa = section.section_assignments.find_by(assgt_id: hash2['assgt_id'])
-							sa.update_attributes hash2
-							sa.save!
-						else
-							section.add_assignment(hash2['name'], hash2['assgt_id'], hash2['use'])
-						end
-						conn.query("UPDATE assgt_dates_status SET sent=1 WHERE id=#{adid}")
-						res += <<EOT
-	<assgt_date>#{adid}</assgt_date>
-EOT
-					end
-				ensure
-					conn.close
-				end
+			if section.section_assignments.where(assgt_id: hash['assgt_id']).exists?
+				sa = section.section_assignments.find_by(assgt_id: hash['assgt_id'])
+				sa.due_date = hash['due_date']
+				sa.due_date = hash.delete('use_assgt') == 'Y'
+				sa.save!
+			else
+				section.add_assignment(hash['name'], hash['assgt_id'], hash['use'])
 			end
-			return res
+			return res + <<EOT
+	<assgt_date>#{hash['id']}</assgt_date>
+EOT
 		end
 
 		def create_or_update_sas
 			res = ""
 			if conn = connector
 				begin
-					conn.query("SELECT assgt_dates.id, assgt_id,course_num,teacher_id,block FROM assgt_dates,assgt_dates_status WHERE assgt_dates.id=assgt_dates_status.id").each_hash do |hash|
+					threads = []
+					conn.query("SELECT assgt_dates.id, assgt_id, course_num AS course_id,teacher_id,block,date_due AS due_date,use_assgt,schoolyear AS academic_year FROM assgt_dates,assgt_dates_status WHERE assgt_dates.id=assgt_dates_status.id AND assgt_dates_status.sent=0").each_hash do |hash|
 						res += create_or_update_sa(hash)
+						conn.query("UPDATE assgt_dates_status SET sent=1 WHERE id=#{hash['id']}")
 					end
 				ensure
 					conn.close
