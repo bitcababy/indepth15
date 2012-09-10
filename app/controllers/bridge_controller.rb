dfd8b942acafa02f0447eee46d86be377c7fc8d6
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-10 10:39:35 -0400

    Changed bridge_controller to use a delayed job for importing

--- app/controllers/bridge_controller.rb
+++ app/controllers/bridge_controller.rb
@@ -1,15 +1,8 @@
 class BridgeController < ApplicationController
 
 	def import
-		res = <<EOT
-<?xml version="1.0" encoding="utf-8"?>
-
-<sent>
-EOT
-		
-		res += Bridge.create_or_update_assignments
-		res += Bridge.create_or_update_sas
-		res += "</sent>"
+		importer = ImportJob.new
+		importer.import_new_stuff
 		head :success
 	end
 
