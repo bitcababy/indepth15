ac237c92a5fa4492734b7bc0ac5f050ad9d95ada
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-11 14:42:52 -0400

    Fixed past and future sa specs

--- spec/fabricators/section_assignment_fabricator.rb
+++ spec/fabricators/section_assignment_fabricator.rb
@@ -1,13 +1,14 @@
 Fabricator(:section_assignment) do
 	due_date			{ Date.today }
+	section
 	assignment
 end
 
 Fabricator(:past_section_assignment, from: :section_assignment) do
-	due_date			{ Date.today - rand(1..5) }
+	due_date			{ Date.today - sequence(2) }
 end
 
 Fabricator(:future_section_assignment, from: :section_assignment) do
-	due_date			{ Date.today + rand(1..5) }
+	due_date			{ Date.today + sequence(2)  }
 end
 	
