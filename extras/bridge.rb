ecb5aa511e11c900f2a94775572371bee4fff437
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-11 14:42:19 -0400

    Cleaned bridge up a whole bunch and wrote some specs

--- extras/bridge.rb
+++ extras/bridge.rb
@@ -8,35 +8,70 @@ module Bridge
 			return Mysql.connect "db1.xoala.net", 'whswriter', 'hyperbole', ((Rails.env == 'production')  ? 'whsmd' : 'whsmd_testing'), 3306, "/var/run/mysqld/mysqld.sock"
 		end
 		
+		def create_assignment(hash)
+			assgt_id = hash['assgt_id'].to_i
+			asst = Assignment.new assgt_id: assgt_id, content: hash['content']
+			if asst.valid?
+				return asst.save!
+			else
+				return false
+			end
+		end
+		
+		def update_assignment(hash)
+			assgt_id = hash['assgt_id'].to_i
+			return false unless (crit = Assignment.where(assgt_id: assgt_id)).exists?
+			asst = crit.first
+			asst.content = hash['content']
+			return asst.save!
+		end
+		
 		def create_or_update_assignment(hash)
 			assgt_id = hash['assgt_id'].to_i
 			if Assignment.where(assgt_id: assgt_id).exists?
-				asst = Assignment.find_by(assgt_id: assgt_id)
-				asst.content = hash['content']
-				asst.save!
+				update_assignment(hash)
 			else
-				Assignment.create! assgt_id: assgt_id, content: hash['content']
+				create_assignment(hash)
 			end
 		end
-				
-		def create_or_update_sa(hash)
-			assgt_id = hash['assgt_id'].to_i
-			use = hash['use_assgt'] == 'Y'
-			ay = hash['academic_year'].to_i
-			course_id = hash['course_id'].to_i
-			due_date = Date.parse(hash['due_date'])
 
-			section = Section.find_by course_id: course_id, teacher_id: hash['teacher_id'], block: hash['block'], academic_year: ay
-			raise "Missing section #{hash}" unless section
-			raise "assignment #{assgt_id} doesn't exist" unless Assignment.where(assgt_id: assgt_id).exists?
-			
-			if sa = section.section_assignments.detect {|s| s.assignment.assgt_id == assgt_id}
-				sa.due_date = due_date
-				sa.use = use
-				sa.name = hash['name']
-				section.save!
+		def translate_sa_hash(hash)
+			hash['assgt_id'] = hash['assgt_id'].to_i
+			hash['use'] = hash['use_assgt'] == 'Y'
+			hash['academic_year'] = hash['academic_year'].to_i
+			hash['course_id'] = hash['course_id'].to_i
+			hash['due_date'] = Date.parse(hash['due_date'])
+		end
+	
+		def create_sa(section, hash)
+			sa = section.section_assignments.new name: hash['name'],  due_date: hash['due_date'], 
+					assignment: Assignment.find_by(assgt_id: hash['assgt_id']), 
+					use: hash['use']
+			if sa.valid?
+				return sa.save!
 			else
-				section.section_assignments.create! name: hash['name'], due_date: due_date, assignment: Assignment.find_by(assgt_id: assgt_id), use: use
+				return false
+			end
+		end
+		
+		def update_sa(sa, hash)
+			sa.due_date = hash['due_date']
+			sa.use = hash['use'] == 'Y'
+			sa.name = hash['name']
+			return sa.save!
+		end
+		
+		def create_or_update_sa(hash)
+			translate_sa_hash(hash)
+			assgt_id = hash['assgt_id']
+			return false unless (crit = Section.where course_id: hash['course_id'], teacher_id: hash['teacher_id'], block: hash['block'], academic_year: hash['academic_year']).exists?
+			section = crit.first
+			return false unless Assignment.where(assgt_id: assgt_id).exists?
+			sa = section.section_assignments.detect {|s| s.assignment.assgt_id == assgt_id}
+			if sa
+				update_sa(sa, hash)
+			else
+				create_sa(section, hash)
 			end
 		end
 
@@ -44,28 +79,26 @@ module Bridge
 			if conn = connector
 				begin
 					conn.query("SELECT assignments.assgt_id, assignments.teacher_id, assignments.content FROM assgts_status, assignments WHERE assgts_status.sent=0 AND assgts_status.deleted=0 AND assgts_status.assgt_id=assignments.assgt_id").each_hash do |hash|
-						create_or_update_assignment(hash)
-						conn.query("UPDATE assgts_status SET sent=1 WHERE assgt_id=#{hash['assgt_id']}")
+						conn.query("UPDATE assgts_status SET sent=1 WHERE assgt_id=#{hash['assgt_id']}") if create_or_update_assignment(hash) 
 					end
-				rescue
-					logger.warn "Couldn't connect to database #{SERVER}"
 				ensure
 					conn.close if conn
 				end
 			end
 
-			# return res
 		end
 		
 		def create_or_update_sas
-			# res = ""
 			if conn = connector
 				begin
-					conn.query("SELECT section_assignments.id, assgt_id, name, course_num AS course_id,teacher_id,block,due_date,academic_year,use_assgt FROM section_assignments,assgt_dates_status WHERE assgt_dates_status.sent=0 AND AND assgts_status.deleted=0 section_assignments.id=assgt_dates_status.id").each_hash do |hash|
-						create_or_update_sa(hash)
-						conn.query("UPDATE assgt_dates_status SET sent=1 WHERE id=#{hash['id']}")
+					conn.query("SELECT section_assignments.id, assgt_id, name, course_num 
+								AS course_id,teacher_id,block,due_date,year AS academic_year,use_assgt 
+								FROM section_assignments,assgt_dates_status 
+								WHERE assgt_dates_status.sent=0 AND assgt_dates_status.deleted=0 
+								AND section_assignments.id=assgt_dates_status.id").each_hash do |hash|
+							conn.query("UPDATE assgt_dates_status SET sent=1 WHERE id=#{hash['id']}") if create_or_update_sa(hash)
 					end
-				rescue
+				# rescue
 				ensure
 					conn.close if conn
 				end
---
+++ spec/extras/bridge_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Bridge do
+
+	describe 'Bridge.create_assignment' do
+		it "creates an assignment from a hash and returns true" do
+			Bridge.create_assignment(assgt_id: 123, content: "Foo bar").should be_true
+		end
+		
+		it "returns false if the creation fails" do
+			Bridge.create_assignment(assgt_id: 123, content: "Foo bar").should be_true
+			Bridge.create_assignment(assgt_id: 123, content: "Foo bar").should be_false
+		end		
+	end
+	
+	describe "Bridge.update_assignment" do
+		it "returns false if the assignment doesn't exist" do
+			Bridge.update_assignment(assgt_id: 123, content: "Foo bar").should be_false
+		end
+		
+		it "returns true if the assignment is updated" do
+			Bridge.create_assignment(assgt_id: 123, content: "Foo bar")
+			Bridge.update_assignment(assgt_id: 123, content: "quux").should be_true
+		end
+	end
+			
+	describe "Bridge.create_sa"
+		
+	
+end
+
