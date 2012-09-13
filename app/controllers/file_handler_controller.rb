ab0f93f915b006a52a4538544014f2cebe338a29
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-13 10:08:48 -0400

    Got rid of some fossils

--- app/controllers/file_handler_controller.rb
+++
@@ -1,9 +0,0 @@
-class FileHandlerController < ApplicationController
-	REDIRECT_URL = 'http://oldfiles.westonmath.org'
-
-  def pass_on
-		redirect_to "#{REDIRECT_URL}#{request.path}"
-  end
-end
-
-# http://www.westonmath.org/teachers/monzj/alg2h/Summary%20of%20Topics%20for%20Prospective%20Algebra%20II%20Honors%20Students.pdf
