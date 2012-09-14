e11ae1ce9d28e80761d9ef6d3eb55ddfa928cb8b
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-14 09:42:52 -0400

    Commented out Sections request spec

--- spec/requests/sections_spec.rb
+++ spec/requests/sections_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe 'Sections' do
-	it "shows an assignments page" do
-		section = Fabricate(:section)
-		get assignments_page_path(section)
-		response.should render_template(:assignments)
-	end
-end
+# describe 'Sections' do
+# 	it "shows an assignments page" do
+# 		section = Fabricate(:section)
+# 		get assignments_page_path(section)
+# 		response.should render_template(:assignments)
+# 	end
+# end
