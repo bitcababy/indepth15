baff40652a5fa9d729af27f0435605aaff8aa298
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-14 09:34:02 -0400

    Cosmetic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-	
+	protect_from_forgery
 	# For ajax requests
 	# skip_before_filter :verify_authenticity_token, :only => [:name_of_your_action] 
-
+	
+	##
+	## Devise methods
+	##
 	helper_method :user_signed_in?
 
 	def user_signed_in?
@@ -21,51 +23,51 @@ class ApplicationController < ActionController::Base
 	end
 
 	# if user is logged in, return current_user, else return guest_user
-	 def current_or_guest_user
-    if current_user
-      if session[:guest_user_id]
-        logging_in
-        guest_user.destroy
-        session[:guest_user_id] = nil
-      end
-      current_user
-    else
-      guest_user
-    end
-  end
+	def current_or_guest_user
+		if current_user
+			if session[:guest_user_id]
+				logging_in
+				guest_user.destroy
+				session[:guest_user_id] = nil
+			end
+			current_user
+		else
+			guest_user
+		end
+	end
 
-  # find guest_user object associated with the current session,
-  # creating one as needed
-  def guest_user
-    User.find(session[:guest_user_id].nil? ? session[:guest_user_id] = create_guest_user.id : session[:guest_user_id])
-  end
+	# find guest_user object associated with the current session,
+	# creating one as needed
+	def guest_user
+		User.find(session[:guest_user_id].nil? ? session[:guest_user_id] = create_guest_user.id : session[:guest_user_id])
+	end
 
-  private
+	private
 
-  # called (once) when the user logs in, insert any code your application needs
-  # to hand off from guest_user to current_user.
-  def logging_in
-    # For example:
-    # guest_comments = guest_user.comments.all
-    # guest_comments.each do |comment|
-      # comment.user_id = current_user.id
-      # comment.save
-    # end
-  end
+	# called (once) when the user logs in, insert any code your application needs
+	# to hand off from guest_user to current_user.
+	def logging_in
+		# For example:
+		# guest_comments = guest_user.comments.all
+		# guest_comments.each do |comment|
+			# comment.user_id = current_user.id
+			# comment.save
+		# end
+	end
 
-  def create_guest_user
-    u = User.new
-    u.save(:validate => false)
-    u
-  end
+	def create_guest_user
+		u = User.new
+		u.save(:validate => false)
+		u
+	end
 
 	# def devise_mapping
-	# 	@devise_mapping ||= Devise.mappings[:user]
+	#		@devise_mapping ||= Devise.mappings[:user]
 	# end
 	
 	protected
 		def ckeditor_filebrowser_scope(options = {})
-      super({ :assetable_id => current_user.id, :assetable_type => 'User' }.merge(options))
-    end
+			super({ :assetable_id => current_user.id, :assetable_type => 'User' }.merge(options))
+		end
 
 end
