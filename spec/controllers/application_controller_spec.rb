3443ef5868adb59cea876b9da985632baa00e58a
Meredith Lesly <bitcababy+github@gmail.com>
2012-09-17 11:20:23 -0400

    Added (unfinished) tests for #render_404 and #render_500

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,9 +3,28 @@ require 'spec_helper'
 describe ApplicationController do
 	describe '#current_user'
 	describe '#user_signed_in?'
-	describe '#user_sessions'
+	describe '#user_session'
 	describe '#current_or_guest_user'
 	describe '#guest_user'
-	describe '#create_gues_user'
+	describe '#create_guest_user'
+	
+	
+	describe 'render_404' do
+		it "handles ActionController::RoutingError" do
+			pending "Unfinished test"
+			raise ActionController::RoutingError, 'foo'
+			assigns[:not_found_path].should == 'foo'
+		end
+
+		it "sets @not_found_path to the error message" do
+			pending "Unfinished test"
+			assigns[:not_found_path].should_not be_nil
+		end
+		pending "Unfinished test"
+	end
+	
+	describe 'render_500' do
+		it "handles fatal errors"
+	end
 	
 end
