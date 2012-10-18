d2ca0651dd82e4e4c13b60e6cb31d009ce880b86
Meredith Lesly <bitcababy+github@gmail.com>
2012-10-18 13:34:53 -0400

    Wrote import_from_hash for Department

--- app/models/department.rb
+++ app/models/department.rb
@@ -12,4 +12,16 @@ class Department
 	belongs_to :resources_doc, class_name: 'TextDocument', polymorphic: true
 	belongs_to :puzzle_doc, class_name: 'TextDocument', polymorphic: true
 	
+	def self.import_from_hash(hash)
+		dept = Department.new
+		dept.name = hash[:name]
+		dept.create_why_doc content: hash[:why]
+		dept.create_how_doc content: hash[:how_to_use]
+		dept.create_news_doc content: hash[:news]
+		dept.create_resources_doc content: hash[:resources]
+		dept.create_puzzle_doc content: hash[:puzzle]
+		dept.save!
+	end
+		
+	
 end
