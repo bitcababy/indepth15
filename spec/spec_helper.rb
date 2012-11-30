82497384ffc3fa585eeca093ed2b0f0bfbeac233
Meredith Lesly <meredith@xoala.com>
2012-11-30 10:54:29 -0500

    Redid spec_helper to use spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,76 +1,140 @@
-if( ENV['COVERAGE'] == 'on' )
-  require 'simplecov'
-  require 'simplecov-rcov'
-  class SimpleCov::Formatter::MergedFormatter
-    def format(result)
-       SimpleCov::Formatter::HTMLFormatter.new.format(result)
-       SimpleCov::Formatter::RcovFormatter.new.format(result)
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  if( ENV['COVERAGE'] == 'on' )
+    require 'simplecov'
+    require 'simplecov-rcov'
+    class SimpleCov::Formatter::MergedFormatter
+      def format(result)
+         SimpleCov::Formatter::HTMLFormatter.new.format(result)
+         SimpleCov::Formatter::RcovFormatter.new.format(result)
+      end
+    end
+    SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
+    SimpleCov.start 'rails' do
+    	add_filter '/spec/'
+    	add_filter '/features/'
+      add_filter '/config/'
+      add_filter '/uploaders/'
+      add_filter '/app/models/ckeditor/'
     end
   end
-  SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
-  SimpleCov.start 'rails' do
-  	add_filter '/spec/'
-  	add_filter '/features/'
-    add_filter '/config/'
-    add_filter '/uploaders/'
-    add_filter '/app/models/ckeditor/'
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'email_spec'
+  require 'rspec/autorun'
+  require 'capybara/rspec'
+  require 'devise/test_helpers'
+
+  module Devise
+    module Models
+      module DatabaseAuthenticatable
+        protected
+
+        def password_digest(password)
+          password
+        end
+      end
+    end
   end
+
+  Devise.setup do |config|
+    config.stretches = 0
+  end
+
+  include Utils
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  Dir[Rails.root.join("app/lib/extras/**/*.rb")].each {|f| require f}
+
+  require 'database_cleaner'
+  DatabaseCleaner.strategy = :truncation
+
+
+  RSpec.configure do |config|
+    # ## Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+
+    # If true, the base class of anonymous controllers will be inferred
+    # automatically. This will be the default behavior in future versions of
+    # rspec-rails.
+    config.infer_base_class_for_anonymous_controllers = false
+  	config.include Mongoid::Matchers
+  	config.include Devise::TestHelpers, :type => :controller
+    config.include(EmailSpec::Helpers)
+    config.include(EmailSpec::Matchers)
+
+  	# Clear out 
+  	config.before(:each) do
+  		DatabaseCleaner.clean
+  		Mongoid::IdentityMap.clear
+  	end
+	
+  	config.after(:each) do
+  		DatabaseCleaner.clean
+  	end
+	
+  	# Devise
+  	config.include Devise::TestHelpers, :type => :controller
+  	config.include Devise::TestHelpers, :type => :view
+  	config.include Devise::TestHelpers, :type => :helper
+  	config.extend ControllerMacros, :type => :controller
+
+  end
+  require 'mocha/setup'
+
+
 end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
+
+# --- Instructions ---
+# Sort the contents of this file into a Spork.prefork and a Spork.each_run
+# block.
+#
+# The Spork.prefork block is run only once when the spork server is started.
+# You typically want to place most of your (slow) initializer code in here, in
+# particular, require'ing any 3rd-party gems that you don't normally modify
+# during development.
+#
+# The Spork.each_run block is run each time you run your specs.  In case you
+# need to load files that tend to change during development, require them here.
+# With Rails, your application modules are loaded automatically, so sometimes
+# this block can remain empty.
+#
+# Note: You can modify files loaded *from* the Spork.each_run block without
+# restarting the spork server.  However, this file itself will not be reloaded,
+# so if you change any of the code inside the each_run block, you still need to
+# restart the server.  In general, if you have non-trivial code in this file,
+# it's advisable to move it into a separate file so you can easily edit it
+# without restarting spork.  (For example, with RSpec, you could move
+# non-trivial code into a file spec/support/my_helper.rb, making sure that the
+# spec/support/* files are require'd from inside the each_run block.)
+#
+# Any code that is left outside the two blocks will be run during preforking
+# *and* during each_run -- that's probably not what you want.
+#
+# These instructions should self-destruct in 10 seconds.  If they don't, feel
+# free to delete them.
+
+
+
+
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'email_spec'
-require 'rspec/autorun'
-require 'capybara/rspec'
-require 'devise/test_helpers'
-
-include Utils
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-Dir[Rails.root.join("app/lib/extras/**/*.rb")].each {|f| require f}
-
-require 'database_cleaner'
-DatabaseCleaner.strategy = :truncation
-
-
-RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
-	config.include Mongoid::Matchers
-	config.include Devise::TestHelpers, :type => :controller
-  config.include(EmailSpec::Helpers)
-  config.include(EmailSpec::Matchers)
-
-	# Clear out 
-	config.before(:each) do
-		DatabaseCleaner.clean
-		Mongoid::IdentityMap.clear
-	end
-	
-	config.after(:each) do
-		DatabaseCleaner.clean
-	end
-	
-	# Devise
-	config.include Devise::TestHelpers, :type => :controller
-	config.include Devise::TestHelpers, :type => :view
-	config.include Devise::TestHelpers, :type => :helper
-	config.extend ControllerMacros, :type => :controller
-
-end
-require 'mocha/setup'
-
