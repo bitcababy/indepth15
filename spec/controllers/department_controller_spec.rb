db8398451734b7a50726dcb892a2aed044571f7f
Meredith <bitcababy@gmail.com>
2012-12-11 10:19:36 -0500

    Created a spec for DepartmentController#home_page

--- spec/controllers/department_controller_spec.rb
+++ spec/controllers/department_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
-describe HomeController do
+describe DepartmentController do
+  describe 'GET :home_page' do
+    it "sets up a bunch of objects to be displayed" do
+      docs = []
+      5.times do 
+        docs << mock('doc') do
+          stubs(:content).returns "Some content"
+        end
+      end
+      dept = mock('dept') do
+        stubs(:how_doc).returns docs[0]
+        stubs(:why_doc).returns docs[1]
+        stubs(:resources_doc).returns docs[2]
+        stubs(:news_doc).returns docs[3]
+        stubs(:puzzle_doc).returns docs[4]
+      end
+      Department.stubs(:first).returns dept
+      get :home_page
+      assigns(:panes).should == docs
+    end
+  end  
 	
 end
