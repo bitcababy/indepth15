ef79d9707bfa642ef5e8c0408922d7d246f1c741
Meredith <bitcababy@gmail.com>
2012-12-18 10:32:51 -0500

    Added support files for HistoryTracker

---
+++ config/initializers/mongoid-history.rb
@@ -0,0 +1,6 @@
+# config/initializers/mongoid-history.rb
+# initializer for mongoid-history
+# assuming HistoryTracker is your tracker class
+Mongoid::History.tracker_class_name = :history_tracker
+# This is for devise/authlogic
+Mongoid::History.current_user_method = :current_user
