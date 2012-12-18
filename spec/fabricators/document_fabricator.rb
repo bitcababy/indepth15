de5d774320185b67d19b8345b8afcdc0ddea719d
Meredith <bitcababy@gmail.com>
2012-12-18 10:27:20 -0500

    Removed some unused code from document fabricator

--- spec/fabricators/document_fabricator.rb
+++ spec/fabricators/document_fabricator.rb
@@ -4,26 +4,14 @@ Fabricator :document do
 end
 
 Fabricator :text_document do
+  title     nil
 	content		""
 end
 
 ## Assignments
 
 Fabricator :assignment, from: :text_document, class_name: :assignment  do
-	name								{ sequence(:assignment_name) }
 	content							""
 	section_assignments	[]
 	assgt_id						{ sequence(1)}
-end
-
-Fabricator :upload, from: :document
-
-Fabricate.sequence(:assignment_name) {|i| i.to_s }
-
-Fabricator(:future_assignment, from: :assignment) do
-	after_build	{ |asst| Fabricate.build(:future_section_assignment, assignment: asst) }
-end
-
-Fabricator(:past_assignment, from: :assignment) do
-	after_build	{ |asst| Fabricate.build(:past_section_assignment, assignment: asst) }
 end
