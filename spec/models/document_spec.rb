d57af40432469c4a783df24113f496e6058701ad
Meredith <bitcababy@gmail.com>
2012-12-18 10:29:39 -0500

    Removed fossil versioning code from Document

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -2,25 +2,4 @@ require 'spec_helper'
 
 describe Document do
 	it { should belong_to(:owner) }
-	
-	# context "versioning" do
-	# 	describe 'bump_minor_version' do
-	# 		doc = Fabricate :document
-	# 		it "can bump its minor version" do
-	# 			expect {
-	# 				doc.bump_minor_version
-	# 			}.to change{ doc.minor_version }.by(1)
-	# 		end
-	# 	end
-	# 
-	# 	describe 'bump_major_version' do
-	# 		doc = Fabricate :document
-	# 		it "can bump its major version" do
-	# 			expect {
-	# 				doc.bump_major_version
-	# 			}.to change{ doc.major_version }.by(1)
-	# 		end
-	# 	end
-	# end
-
 end
