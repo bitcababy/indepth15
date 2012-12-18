e988fdde38c4b03679cbab5d4414c687dd669e33
Meredith <bitcababy@gmail.com>
2012-12-18 10:26:42 -0500

    Cleaned up DepartmentController

--- spec/controllers/department_controller_spec.rb
+++ spec/controllers/department_controller_spec.rb
@@ -1,24 +1,14 @@
 require 'spec_helper'
 
 describe DepartmentController do
-  describe 'GET :home_page' do
-    it "sets up a bunch of objects to be displayed" do
-      docs = []
-      5.times do 
-        docs << mock('doc') do
-          stubs(:content).returns "Some content"
-        end
-      end
-      dept = mock('dept') do
-        stubs(:how_doc).returns docs[0]
-        stubs(:why_doc).returns docs[1]
-        stubs(:resources_doc).returns docs[2]
-        stubs(:news_doc).returns docs[3]
-        stubs(:puzzle_doc).returns docs[4]
-      end
+  include DepartmentHelper
+
+  describe 'GET :home' do
+    it "uses the first department" do
+      dept = mock_department
       Department.stubs(:first).returns dept
-      get :home_page
-      assigns(:panes).should == docs
+      get :home
+      assigns(:dept).should == dept
     end
   end  
 	
