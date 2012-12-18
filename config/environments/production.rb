362aa46e34eb4fa67fdd8e5f7f43d657d37d9048
Meredith <bitcababy@gmail.com>
2012-12-18 10:23:58 -0500

    Added ckeditor/init to precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,9 @@ InDepth::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-	config.assets.precompile += %w(application-all.css application-print.css application-screen.css application-ie.css application-shared.css)
+	config.assets.precompile += %w(application-all.css application-print.css)
+  config.assets.precompile += %w(application-screen.css application-ie.css application-shared.css)
+  config.assets.precompile += %w(ckeditor/init)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
