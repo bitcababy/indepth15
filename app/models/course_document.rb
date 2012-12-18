2b04060669e40d0100746616cfe36f166a2cd86e
Meredith <bitcababy@gmail.com>
2012-12-18 10:32:13 -0500

    Added CourseDocument class
    (tentative)

---
+++ app/models/course_document.rb
@@ -0,0 +1,9 @@
+class CourseDocument < TextDocument
+  include Mongoid::Document
+  
+
+  # field :kind, type: String
+  # 
+  # TYPES = [:assignment, :worksheet, :test, :quiz, :retake, :benchmark, :classwork, :homework]
+  
+end
