9efc11decbb4882eb8be44d2c6a54519bd359e93
Meredith <bitcababy@gmail.com>
2012-12-19 11:37:04 -0500

    Changed _course_link partial to use :home_course_path

--- spec/views/shared/_course_link.html.haml_spec.rb
+++ spec/views/shared/_course_link.html.haml_spec.rb
@@ -4,6 +4,6 @@ describe 'shared/_course_link' do
 	it "returns a link to a course" do
 		course = Fabricate(:course, full_name: 'Fractals')
 		render partial: 'shared/course_link', locals: {course: course}
-		rendered.should have_selector("a", content: course.full_name, href: course_path(course))
+		rendered.should have_selector("a", content: course.full_name, href: home_course_path(course))
 	end
 end
