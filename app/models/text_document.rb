f10e1d011788fa083ca037626b8e5ec92479d7e2
Meredith <bitcababy@gmail.com>
2012-12-19 11:32:57 -0500

    TextDocument: got rid of state machine stuff

--- app/models/text_document.rb
+++ app/models/text_document.rb
@@ -1,22 +1,3 @@
 class TextDocument < Document
 	field :content, type: String, default: ""
-	
-	# embeds_one :text_document_context
-	# @doc_machine = StateMachine.build do
-	# 	state :edit_requested do
-	# 		event :lock_document, :editing
-	# 		trans :unlocked, :lock_document, :editing
-	# 		trans :locked_by_user, :update_lock, :editing
-	# 		trans :locked_by_someone_else, :lock_timed_out, :editing
-	# 		trans :locked_by_someone_else, :lock_not_timed_out, :edit_error	
-	# 	end
-	# 		state :editing do
-	# 			event :save_requested, :saved, :successful_save
-	# 			event :save_requested, :save_error, :unsuccessful_save
-	# 			event :reset_requested, :editing, :reset
-	# 			event :cancel_requested, :, :cancel
-	# 			on_exit :unlock
-	# 		end
-	# 	
 
-end
