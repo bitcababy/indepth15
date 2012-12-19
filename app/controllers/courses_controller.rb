51884f20fac27b02f1439197e728542e594171be
Meredith <bitcababy@gmail.com>
2012-12-19 13:06:33 -0500

    Commented out pane methods
    Will remove them if performance is OK

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -58,35 +58,35 @@ class CoursesController < ApplicationController
 	##
 	## Panes
 	##
-	def resources_pane
-		respond_to do |format|
-			format.html { render :layout => !request.xhr? }
-		 end
-	end
-	
-	def information_pane
-		respond_to do |format|
-			format.html { render :layout => !request.xhr? }
-		end
-	end
-	
-	def news_pane
-		respond_to do |format|
-			format.html { render :layout => !request.xhr? }
-		end
-	end
-	
-	def policies_pane
-		respond_to do |format|
-			format.html { render :layout => !request.xhr? }
-		end
-	end
-
-	def sections_pane
-		respond_to do |format|
-			format.html { render :layout => !request.xhr? }
-		end
-	end
+  # def resources_pane
+  #   respond_to do |format|
+  #     format.html { render :layout => !request.xhr? }
+  #    end
+  # end
+  # 
+  # def information_pane
+  #   respond_to do |format|
+  #     format.html { render :layout => !request.xhr? }
+  #   end
+  # end
+  # 
+  # def news_pane
+  #   respond_to do |format|
+  #     format.html { render :layout => !request.xhr? }
+  #   end
+  # end
+  # 
+  # def policies_pane
+  #   respond_to do |format|
+  #     format.html { render :layout => !request.xhr? }
+  #   end
+  # end
+  # 
+  # def sections_pane
+  #   respond_to do |format|
+  #     format.html { render :layout => !request.xhr? }
+  #   end
+  # end
 	
 	protected
 	
