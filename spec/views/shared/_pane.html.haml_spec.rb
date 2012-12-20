90a59a71a96d578f72000ac1fb54ef1eb6addf54
Meredith <bitcababy@gmail.com>
2012-12-20 13:36:45 -0500

    shared/_pane: added test for edit button
    Also fix up a little this and that.

--- spec/views/shared/_pane.html.haml_spec.rb
+++ spec/views/shared/_pane.html.haml_spec.rb
@@ -2,24 +2,30 @@ require 'spec_helper'
 
 describe 'shared/_pane' do
   before :each do
-    @src = mock('src') do
+    @doc = mock('src') do
       stubs(:title).returns "This is a title"
       stubs(:div_id).returns 'demo'
       stubs(:content).returns "This is some content"
+      stubs(:to_param).returns "1"
     end
-    render partial: 'shared/pane', locals: {src_obj: @src}
   end
 
   it "displays a title" do
+    render partial: 'shared/pane', locals: {doc: @doc}
     rendered.should have_selector('h3') do |hdr|
-      hdr.should have_selector('a', href: '#', content: @src.title)
+      hdr.should have_selector('a', href: '#', content: @doc.title)
     end
   end
 
   it "should contain a div with the right name which displays the content of the doc" do
-    rendered.should have_selector("##{@src.div_id}") do |div|
-      div.should have_content(@src.content)
-    end
+    render partial: 'shared/pane', locals: {doc: @doc}
+    rendered.should have_content(@doc.content)
   end
   
+  it "should have an edit button if the pane is editable" do
+    assign(:editable, true)
+    render partial: 'shared/pane', locals: {doc: @doc}
+    rendered.should have_selector('button a', href: edit_text_document_path(@doc))
+  end
+   
 end
