b29958e9151d5ec139f89053560e75b5f96d61a5
Meredith <bitcababy@gmail.com>
2012-12-31 11:56:39 -0500

    DepartmentController now sorts its panes

--- app/controllers/department_controller.rb
+++ app/controllers/department_controller.rb
@@ -1,18 +1,18 @@
 class DepartmentController < ApplicationController
-	before_filter :find_department, except: []
 	before_filter :find_department, except: [:edit_doc]
 	
 	def home
+    @editable = true # Temporary
+    @panes = @dept.homepage_docs.sort
 		respond_to do |format|
 			format.html { render }
-			format.json { render json: @panes}
+			format.json { render json: @panes }
 		end
 		return
 	end
 
   def about
   end
-  
 
   def edit_doc
     @doc = TextDocument.find params[:doc_id]
