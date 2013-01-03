cfc5d7af8671909eacd0706c29336053ea117523
Meredith <bitcababy@gmail.com>
2013-01-03 13:25:04 -0500

    Commented out a bunch of helper methods
    Unclear that I need them, but not removing them for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,71 +3,72 @@ class ApplicationController < ActionController::Base
 	# For ajax requests
 	# skip_before_filter :verify_authenticity_token, :only => [:name_of_your_action] 
 	
-	unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, with: :render_500
-    rescue_from ActionController::RoutingError, with: :render_404
-    rescue_from ActionController::UnknownController, with: :render_404
-    rescue_from ActionController::UnknownAction, with: :render_404
-    rescue_from Mongoid::Errors::DocumentNotFound, with: :render_404
-	end	
-
+  unless Rails.application.config.consider_all_requests_local
+      rescue_from Exception, with: :render_500
+      rescue_from ActionController::RoutingError, with: :render_404
+      rescue_from ActionController::UnknownController, with: :render_404
+      rescue_from ::AbstractController::ActionNotFound, with: :render_404
+      rescue_from Mongoid::Errors::DocumentNotFound, with: :render_404
+  end  
+  # 
+  ## Note: Unclear why I need any of these
 	##
 	## Devise methods
 	##
-	helper_method :current_user
-
-	def current_user
-		return @current_user ||= warden.authenticate(:scope => :user)
-	end
-
-	helper_method :user_signed_in?
-
-	def user_signed_in?
-		return !!current_user
-	end
-	
-	def user_session
-		return current_user && warden.session(:user)
-	end
+  # helper_method :current_user
+  # 
+  # def current_user
+  #   return @current_user ||= warden.authenticate(:scope => :user)
+  # end
+  # 
+  # helper_method :user_signed_in?
+  # 
+  # def user_signed_in?
+  #   return !!current_user
+  # end
+  # 
+  # def user_session
+  #   return current_user && warden.session(:user)
+  # end
 
 	# if user is logged in, return current_user, else return guest_user
-	def current_or_guest_user
-		if current_user
-			if session[:guest_user_id]
-				logging_in
-				guest_user.destroy
-				session[:guest_user_id] = nil
-			end
-			return current_user
-		else
-			return guest_user
-		end
-	end
-
-	# find guest_user object associated with the current session,
-	# creating one as needed
-	def guest_user
-		return  User.find(session[:guest_user_id].nil? ? session[:guest_user_id] = create_guest_user.id : session[:guest_user_id])
-	end
+  # def current_or_guest_user
+  #   if current_user
+  #     if session[:guest_user_id]
+  #       logging_in
+  #       guest_user.destroy
+  #       session[:guest_user_id] = nil
+  #     end
+  #     return current_user
+  #   else
+  #     return guest_user
+  #   end
+  # end
+  # 
+  # # find guest_user object associated with the current session,
+  # # creating one as needed
+  # def guest_user
+  #   return  User.find(session[:guest_user_id].nil? ? session[:guest_user_id] = create_guest_user.id : session[:guest_user_id])
+  # end
 
 	private
 
 	# called (once) when the user logs in, insert any code your application needs
 	# to hand off from guest_user to current_user.
-	def logging_in
-		# For example:
-		# guest_comments = guest_user.comments.all
-		# guest_comments.each do |comment|
-			# comment.user_id = current_user.id
-			# comment.save
-		# end
-	end
-
-	def create_guest_user
-		u = User.new
-		u.save(:validate => false)
-		return u
-	end
+  # def logging_in
+  #   # For example:
+  #   # guest_comments = guest_user.comments.all
+  #   # guest_comments.each do |comment|
+  #     # comment.user_id = current_user.id
+  #     # comment.save
+  #   # end
+  # end
+  # 
+  # def create_guest_user
+  #   u = User.new
+  #   u.save(:validate => false)
+  #   return u
+  # end
 
 	# def devise_mapping
 	#		@devise_mapping ||= Devise.mappings[:user]
