99ffabd62bd4d233af7189282e5eec7fc76272b6
Meredith <bitcababy@gmail.com>
2013-01-03 13:37:51 -0500

    Added some specs to Department model

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Department do
-  it { should have_many :homepage_docs }
+  subject { Fabricate :department }
+  it { should have_and_belong_to_many :homepage_docs }
+  specify { subject.name.should_not be_nil }
+  specify { subject.name.should_not be_empty }
 end
