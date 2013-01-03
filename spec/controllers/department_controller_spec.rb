2992139acdf0dd201e7b35f74f6fb40dc68c3034
Meredith <bitcababy@gmail.com>
2013-01-03 13:36:28 -0500

    Added test for Department#edit_doc

--- spec/controllers/department_controller_spec.rb
+++ spec/controllers/department_controller_spec.rb
@@ -11,5 +11,25 @@ describe DepartmentController do
     end
   end
   
+  describe '#edit_doc' do
+    it "gets the doc referenced by params[:doc_id]" do
+      doc = Fabricate :text_document
+      get :edit_doc, doc_id: doc
+      assigns(:doc).should == doc
+    end
+    it "locks the document if it's unlocked and returns an edit form" do
+      doc = Fabricate :text_document
+      get :edit_doc, doc_id: doc
+      response.should render_template :edit_doc
+    end
+      
+    # SMELL: Not sure if this is the right way to handle it
+    it "returns :bad_request if the document is locked" do
+      doc = Fabricate :text_document
+      doc.lock
+      get :edit_doc, doc_id: doc
+      assert_response :bad_request
+    end
+  end
 	
 end
