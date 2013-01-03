e1b5f7f0c6e9acf9df3ebc1dcb49a092a6fabe52
Meredith <bitcababy@gmail.com>
2013-01-03 13:29:07 -0500

    Department: added validation to name field

--- app/models/department.rb
+++ app/models/department.rb
@@ -5,6 +5,7 @@ class Department
   include Mongoid::Timestamps if Rails.env == 'production'
   
 	field :n, as: :name, type: String
+  validates :name, presence: true, length: {minimum: 4}
   field :_id, type: String, default: -> { n }
 
   has_many :homepage_docs, class_name: 'TextDocument', inverse_of: :owner, dependent: :delete
