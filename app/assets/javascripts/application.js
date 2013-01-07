e2d8c93222cc23f6b3be9d9c8aadf9357e7d9f3e
Meredith <bitcababy@gmail.com>
2013-01-07 14:45:08 -0500

    Rearranged application.js slightly.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require rails.validations
 //= require ckeditor/init
+//= require rails.validations
+//= require rails.validations.simple_form
 //= require_tree .
