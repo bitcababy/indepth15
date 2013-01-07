94eff8b8dd8e8d165e025fc165aa3a4edf3a9261
Meredith <bitcababy@gmail.com>
2013-01-07 14:55:19 -0500

    Removed Mongoid::Locker from Document

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,14 +2,12 @@ class Document
 	include Mongoid::Document
 	include Mongoid::Paranoia
   include Mongoid::Timestamps
-  include Mongoid::Locker
   include Mongoid::TaggableWithContext
+  include Mongoid::History::Trackable
   include Comparable
   
   field :pos, as: :position, type: Integer, default: 0
-
-  attr_accessor :locked_by
-  
+    
   def <=>(b)
     self.position <=> b.position
   end      
