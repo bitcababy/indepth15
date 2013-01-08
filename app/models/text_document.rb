85edddae4aadefd2d461d0253be397a4544abdff
Meredith <bitcababy@gmail.com>
2013-01-08 10:19:14 -0500

    Added last_editor field to TextDocument

--- app/models/text_document.rb
+++ app/models/text_document.rb
@@ -1,16 +1,23 @@
 class TextDocument < Document
   include Mongoid::History::Trackable
-
   include Mongoid::Locker
+  
+  before_save do
+    if @locked_by && (@title.dirty? || @content.dirty?)
+      self.last_editor = @locked_by
+    end
+  end
+    
   field :ti, as: :title, type: String, default: ''
 	field :co, as: :content, type: String, default: ''
+  
+  attr_accessor :locked_by, :last_editor
 
-  track_history on: [:title, :content],
+  track_history on: [:title, :content, :last_editor],
                 modifier_field: :modifier, 
-                version_field: :version, 
+                version_field: :version,
                 track_create: true, 
                 track_update: true, 
                 track_destroy: true
-
   
 end
