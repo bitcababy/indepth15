901e3263fec922a98e2ee1e15b88a20eff360b59
Meredith <bitcababy@gmail.com>
2013-01-09 13:35:10 -0500

    DepartmentControllerSpec: fixed #edit_doc

--- spec/controllers/department_controller_spec.rb
+++ spec/controllers/department_controller_spec.rb
@@ -13,8 +13,9 @@ describe DepartmentController do
   
   describe '#edit_doc' do
     it "gets the doc referenced by params[:doc_id]" do
+      subject.current_user.should be_nil
       doc = Fabricate :text_document
-      get :edit_doc, doc_id: doc
+      get :edit_doc, doc_id: doc.to_param
       assigns(:doc).should == doc
     end
     it "locks the document if it's unlocked and returns an edit form" do
@@ -23,11 +24,12 @@ describe DepartmentController do
       response.should render_template :edit_doc
     end
       
-    # SMELL: Not sure if this is the right way to handle it
     it "returns :bad_request if the document is locked" do
-      doc = Fabricate :text_document
-      doc.lock
-      get :edit_doc, doc_id: doc
+      doc = Fabricate :locked_doc
+      doc.stubs(:locked?).returns(true)
+      doc.stubs(:can_relock?).returns(false)
+      get :edit_doc, doc_id: doc.to_param
+      response.should_not render_template :edit_doc
       assert_response :bad_request
     end
   end
