7c52a42c45fa1850c65603dcdefe35888f7ea395
Meredith <bitcababy@gmail.com>
2013-01-09 13:38:40 -0500

    Stubbed subview in courses/_section_table

--- spec/views/courses/_sections_table.html.haml_spec.rb
+++ spec/views/courses/_sections_table.html.haml_spec.rb
@@ -4,10 +4,9 @@ describe 'courses/_sections_table' do
 	include CourseMockHelpers
 
 	it "creates a table of a courses sections" do
-		course = mock_course_with_sections
-		[:full_name, :description, :credits, :duration, :number].each {|k| course.unstub(k)}
-
-		assign(:course, course)
+    course = mock_course_with_sections
+    stub_template 'courses/section_table_row' => "A row"
+    assign(:course, course)
 		render partial: 'courses/sections_table'
 		rendered.should have_selector('table#sections_table thead tr') do |row|
 			%W(block teacher room).each do |hdr|
