7618f8485ca1c740fa6799ff7da2a36eba7bc781
Meredith <bitcababy@gmail.com>
2013-01-09 10:07:12 -0500

    Added route name for department#edit_doc

--- config/routes.rb
+++ config/routes.rb
@@ -69,7 +69,7 @@ InDepth::Application.routes.draw do
   # match 'department/update_doc/:value/:id', as: :update_dept_doc, to: 'department#update_doc'
   
   resources :text_documents
-  get 'departments/edit_doc/:doc_id', to: 'department#edit_doc'
+  get 'departments/edit_doc/:doc_id', to: 'department#edit_doc', as: :edit_dept_doc
 
   unless Rails.application.config.consider_all_requests_local
     match '*not_found', to: 'errors#error_404'
