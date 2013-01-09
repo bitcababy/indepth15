7fcc392ed32dea146f1fa1b70ca565504ed2c64b
Meredith <bitcababy@gmail.com>
2013-01-09 13:30:18 -0500

    TextController: commented out all methods except for :update

--- app/controllers/text_documents_controller.rb
+++ app/controllers/text_documents_controller.rb
@@ -1,82 +1,97 @@
 class TextDocumentsController < ApplicationController
  	# before_filter :authenticate_user!
 	before_filter :find_text_document, only: [:show, :edit, :update, :destroy]
-
- # GET text_documents
-  # GET text_documents.json
-  def index
-    @text_documents = TextDocument.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @text_documents }
-    end
-  end
-
-  # GET text_documents/1
-  # GET text_documents/1.json
-  def show
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @text_document }
-    end
-  end
-
-  # GET text_documents/new
-  # GET text_documents/new.json
-  def new
-    @text_document = TextDocument.new(params[:text_document])
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @text_document }
-    end
-  end
-
-  # GET text_documents/1/edit
-  def edit
-  end
-
-  # POST text_documents
-  # POST text_documents.json
-  def create
-    @text_document = TextDocument.new(params[:text_document])
-
-    respond_to do |format|
-      if @text_document.save
-        format.html { redirect_to text_document_url(@text_document), notice: 'TextDocument was successfully created.' }
-        format.json { render json: @text_document, status: :created, location: @text_document }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @text_document.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
+  
+  # # # GET text_documents
+  # # GET text_documents.json
+  # def index
+  #   @text_documents = TextDocument.all
+  # 
+  #   respond_to do |format|
+  #     format.html # index.html.erb
+  #     format.json { render json: @text_documents }
+  #   end
+  # end
+  # 
+  # # GET text_documents/1
+  # # GET text_documents/1.json
+  # def show
+  #   respond_to do |format|
+  #     format.html # show.html.erb
+  #     format.json { render json: @text_document }
+  #   end
+  # end
+  # 
+  # # GET text_documents/new
+  # # GET text_documents/new.json
+  # def new
+  #   @text_document = TextDocument.new(params[:text_document])
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.json { render json: @text_document }
+  #   end
+  # end
+  # 
+  # # GET text_documents/1/edit
+  # def edit
+  #   respond_to do |format|
+  #     format.html { render :layout => !request.xhr? }
+  #   end
+  # end
+  # 
+  # # POST text_documents
+  # # POST text_documents.json
+  # def create
+  #   @text_document = TextDocument.new(params[:text_document])
+  # 
+  #   respond_to do |format|
+  #     if @text_document.save
+  #       format.html { redirect_to text_document_url(@text_document), notice: 'TextDocument was successfully created.' }
+  #       format.json { render json: @text_document, status: :created, location: @text_document }
+  #     else
+  #       format.html { render action: "new" }
+  #       format.json { render json: @text_document.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  
   # PUT text_documents/1
   # PUT text_documents/1.json
   def update
-		respond_to do |format|
-      if @text_document.update_attributes(params[:text_document])
-        format.html { redirect_to text_document_url(@text_document), notice: 'TextDocument was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @text_document.errors, status: :unprocessable_entity }
+    if request.xhr?
+      respond_to do |format|
+        if @text_document.update_attributes(params[:text_document])
+          format.html { redirect_to :back, notice: 'TextDocument was successfully updated.' }
+        else
+          format.json { render json: @text_document.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      respond_to do |format|
+        if @text_document.update_attributes(params[:text_document])
+          format.json { head :no_content }
+          format.html { redirect_to text_document_url(@text_document), notice: 'TextDocument was successfully updated.' }
+        else
+          format.json { render json: @text_document.errors, status: :unprocessable_entity }
+          format.html { render action: "edit" }
+        end
       end
     end
   end
 
-  # DELETE text_documents/1
-  # DELETE text_documents/1.json
-  def destroy
-    @text_document.destroy
+  # 
+  # # DELETE text_documents/1
+  # # DELETE text_documents/1.json
+  # def destroy
+  #   @text_document.destroy
+  # 
+  #   respond_to do |format|
+  #     format.html { redirect_to text_documents_url }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
-    respond_to do |format|
-      format.html { redirect_to text_documents_url }
-      format.json { head :no_content }
-    end
-  end
-
+  private
 	def find_text_document
    	@text_document = TextDocument.find(params[:id])
 	end
--- spec/controllers/text_documents_controller_spec.rb
+++ spec/controllers/text_documents_controller_spec.rb
@@ -11,73 +11,73 @@ describe TextDocumentsController do
 		}
   end
 
-   describe "GET index" do
-    it "assigns all text_documents as @text_documents" do
-      text_document = Fabricate :text_document, valid_attributes
-      get :index, {}
-      assigns(:text_documents).should eq([text_document])
-    end
-  end
-
-  describe "GET show" do
-    it "assigns the requested text_document as @text_document" do
-      text_document = Fabricate :text_document, valid_attributes
-      get :show, {:id => text_document.to_param}
-      assigns(:text_document).should eq(text_document)
-    end
-  end
-
-  describe "GET new" do
-    it "assigns a new text_document as @text_document" do
-      get :new, {}
-      assigns(:text_document).should be_a_new(TextDocument)
-    end
-  end
-
-  describe "GET edit" do
-    it "assigns the requested text_document as @text_document" do
-      text_document = Fabricate :text_document, valid_attributes
-      get :edit, {:id => text_document.to_param}
-      assigns(:text_document).should eq(text_document)
-    end
-  end
-
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new TextDocument" do
-        expect {
-          post :create, {:text_document => valid_attributes}
-        }.to change(TextDocument, :count).by(1)
-      end
-
-      it "assigns a newly created text_document as @text_document" do
-        post :create, {:text_document => valid_attributes}
-        assigns(:text_document).should be_a(TextDocument)
-        assigns(:text_document).should be_persisted
-      end
-
-      it "redirects to the created text_document" do
-        post :create, {:text_document => valid_attributes}
-        response.should redirect_to(text_document_url(TextDocument.last))
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved text_document as @text_document" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        TextDocument.any_instance.stubs(:save).returns(false)
-        post :create, {:text_document => {}}
-        assigns(:text_document).should be_a_new(TextDocument)
-      end
-
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        TextDocument.any_instance.stubs(:save).returns(false)
-        post :create, {:text_document => {}}
-        response.should render_template("new")
-      end
-    end
-  end
+  #  describe "GET index" do
+  #   it "assigns all text_documents as @text_documents" do
+  #     text_document = Fabricate :text_document, valid_attributes
+  #     get :index, {}
+  #     assigns(:text_documents).should eq([text_document])
+  #   end
+  # end
+  # 
+  # describe "GET show" do
+  #   it "assigns the requested text_document as @text_document" do
+  #     text_document = Fabricate :text_document, valid_attributes
+  #     get :show, {:id => text_document.to_param}
+  #     assigns(:text_document).should eq(text_document)
+  #   end
+  # end
+  # 
+  # describe "GET new" do
+  #   it "assigns a new text_document as @text_document" do
+  #     get :new, {}
+  #     assigns(:text_document).should be_a_new(TextDocument)
+  #   end
+  # end
+  # 
+  # describe "GET edit" do
+  #   it "assigns the requested text_document as @text_document" do
+  #     text_document = Fabricate :text_document, valid_attributes
+  #     get :edit, {:id => text_document.to_param}
+  #     assigns(:text_document).should eq(text_document)
+  #   end
+  # end
+  # 
+  # describe "POST create" do
+  #   describe "with valid params" do
+  #     it "creates a new TextDocument" do
+  #       expect {
+  #         post :create, {:text_document => valid_attributes}
+  #       }.to change(TextDocument, :count).by(1)
+  #     end
+  # 
+  #     it "assigns a newly created text_document as @text_document" do
+  #       post :create, {:text_document => valid_attributes}
+  #       assigns(:text_document).should be_a(TextDocument)
+  #       assigns(:text_document).should be_persisted
+  #     end
+  # 
+  #     it "redirects to the created text_document" do
+  #       post :create, {:text_document => valid_attributes}
+  #       response.should redirect_to(text_document_url(TextDocument.last))
+  #     end
+  #   end
+  # 
+  #   describe "with invalid params" do
+  #     it "assigns a newly created but unsaved text_document as @text_document" do
+  #       # Trigger the behavior that occurs when invalid params are submitted
+  #       TextDocument.any_instance.stubs(:save).returns(false)
+  #       post :create, {:text_document => {}}
+  #       assigns(:text_document).should be_a_new(TextDocument)
+  #     end
+  # 
+  #     it "re-renders the 'new' template" do
+  #       # Trigger the behavior that occurs when invalid params are submitted
+  #       TextDocument.any_instance.stubs(:save).returns(false)
+  #       post :create, {:text_document => {}}
+  #       response.should render_template("new")
+  #     end
+  #   end
+  # end
 
   describe "PUT update" do
     describe "with valid params" do
@@ -123,19 +123,19 @@ describe TextDocumentsController do
     end
   end
 
-  describe "DELETE destroy" do
-    it "destroys the requested text_document" do
-      text_document = Fabricate :text_document, valid_attributes
-      expect {
-        delete :destroy, {:id => text_document.to_param}
-      }.to change(TextDocument, :count).by(-1)
-    end
-
-    it "redirects to the text_documents list" do
-      text_document = Fabricate :text_document, valid_attributes
-      delete :destroy, {:id => text_document.to_param}
-      response.should redirect_to(text_documents_url)
-    end
-  end
+  # describe "DELETE destroy" do
+  #   it "destroys the requested text_document" do
+  #     text_document = Fabricate :text_document, valid_attributes
+  #     expect {
+  #       delete :destroy, {:id => text_document.to_param}
+  #     }.to change(TextDocument, :count).by(-1)
+  #   end
+  # 
+  #   it "redirects to the text_documents list" do
+  #     text_document = Fabricate :text_document, valid_attributes
+  #     delete :destroy, {:id => text_document.to_param}
+  #     response.should redirect_to(text_documents_url)
+  #   end
+  # end
 	
 end
