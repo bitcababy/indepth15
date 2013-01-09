c0c4fb0f51a5ee65837fd7dc476764e90b6f5eef
Meredith <bitcababy@gmail.com>
2013-01-09 10:02:17 -0500

    SectionController: renamed #assignments to #assignments_pane

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,11 +1,11 @@
 class SectionsController < ApplicationController
 	before_filter :find_section, except: [:assignments]
 
-	def assignments
+	def assignments_pane
 		@section = Section.find_by course_id: params[:course_id].to_i, academic_year: params[:year].to_i, teacher_id: params[:teacher_id], block: params[:block].upcase
     @course = @section.course
 		respond_to do |format|
-			format.html {render file: 'courses/home'}
+			format.html {render layout: nil}
 		end
 	end
 
