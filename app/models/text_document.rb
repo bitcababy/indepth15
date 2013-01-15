aaf1d57f39427b7ef910d3f16fd038b220a6f5db
Meredith <bitcababy@gmail.com>
2013-01-15 10:27:46 -0500

    Added locking code to TextDocument

--- app/models/text_document.rb
+++ app/models/text_document.rb
@@ -1,18 +1,21 @@
 class TextDocument < Document
   include Mongoid::History::Trackable
-  include Mongoid::Locker
   
   before_save do
-    if @locked_by && (@title.dirty? || @content.dirty?)
+    if @locked_by && (self.title_change || self.content_change)
       self.last_editor = @locked_by
+      self.locked_by = nil
     end
+    return true
   end
     
   field :ti, as: :title, type: String, default: ''
 	field :co, as: :content, type: String, default: ''
+  field :la, as: :locked_at, type: Time, default: nil
+  
   belongs_to :owner, polymorphic: true
   
-  attr_accessor :locked_by, :last_editor
+  attr_accessor :locked_by
 
   track_history on: [:title, :content, :last_editor],
                 modifier_field: :modifier, 
@@ -21,4 +24,28 @@ class TextDocument < Document
                 track_update: true, 
                 track_destroy: true
   
+  @@timeout = 5*60
+
+  def lock(lb=nil)
+    self.locked_at = Time.now
+    self.locked_by = lb
+    self.save
+   end
+  
+  def can_lock?(lb)
+    return self.locked_at.nil? || self.timed_out? || (lb && self.locked_by == lb)
+  end
+
+  def unlock
+    self.locked_at = nil
+    self.save
+  end
+  
+  def locked?
+    return !!self.locked_at
+  end
+
+  def timed_out?
+    return (Time.now - self.locked_at > @@timeout) 
+  end
 end
