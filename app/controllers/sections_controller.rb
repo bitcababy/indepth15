debddc783fb740b87351fa904c92c8060b868d06
Meredith <bitcababy@gmail.com>
2013-01-22 17:36:37 -0500

    Uncommented #assignments_pane in SectionsController

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,14 +1,14 @@
 class SectionsController < ApplicationController
 	before_filter :find_section, except: [:assignments]
 
-  # def assignments_pane
-  #   @section = Section.find_by course_id: params[:course_id].to_i, academic_year: params[:year].to_i, teacher_id: params[:teacher_id], block: params[:block].upcase
-  #     @course = @section.course
-  #   respond_to do |format|
-  #     format.html {render layout: nil}
-  #   end
-  # end
-
+  def assignments_pane
+     # @section = Section.find_by course_id: params[:course_id].to_i, academic_year: params[:year].to_i, teacher_id: params[:teacher_id], block: params[:block].upcase
+     @course = @section.course
+     respond_to do |format|
+       format.html {render layout: !request.xhr?}
+     end
+   end
+ 
 	# protected
 	def find_section
 		return @section = Section.find(params[:id])
