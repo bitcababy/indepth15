cc385408722af483b9e13ee5676603fa90c7b24f
Meredith <bitcababy@gmail.com>
2013-01-26 10:46:47 -0500

    Restored Branch creation

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -2,13 +2,28 @@ class Branch
   include Mongoid::Document
   include Mongoid::Timestamps if Rails.env == 'production'
 
-  # before_save :create_major_tags
-
-	validates :name, presence: true, length: { minimum: 3 }
 	field :na, as: :name, type: String
-	
+	validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
+	field :mt, as: :major_tags, type: Array, default: []
+  field :_id, default: -> {name}
 	index( {name: 1} )
-		
+
+	BRANCH_MAP = {
+		321 => 'Geometry',
+		322 => 'Geometry',
+		326 => 'Algebra',
+		331 => 'Algebra',
+		332 => 'Algebra',
+		333 => 'Algebra',
+		341 => 'Precalculus',
+		342 => ['Precalculus', 'Statistics', 'Algebra'],
+		343 => 'Discrete Math',
+		352 => 'Precalculus',
+		361 => 'Calculus',
+		371 => 'Calculus',
+		391 => 'Statistics',
+	}
+
 	MAJOR_TAGS = {
 		'Geometry' => [
 			'Transformations', 'Similarity', 'Congruence', 'Logic/Proof',
@@ -60,19 +75,19 @@ class Branch
 			],
 	}
 	
-  # def self.create_all
-  #   Branch.delete_all
-  #   for name in MAJOR_TAGS.keys do
-  #     branch = Branch.create! name: name
-  #     branch.create_major_tags
-  #   end
-  # end
-  # 
-  # def create_major_tags
-  #   return unless MAJOR_TAGS[self.name]
-  #   for tag in MAJOR_TAGS[self.name] do
-  #     self.major_tags << MajorTag.find_or_create_by(name: tag)
-  #   end
-  # end
+  def self.create_all
+    Branch.delete_all
+    for name in MAJOR_TAGS.keys do
+      branch = Branch.create! name: name
+      branch.create_major_tags
+    end
+  end
+
+  def create_major_tags
+    return unless MAJOR_TAGS[self.name]
+    for tag in MAJOR_TAGS[self.name] do
+      self.major_tags << tag
+    end
+  end
 
 end
