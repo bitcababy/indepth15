14f28d5c134cc5185a9bc7b1be7e5fbe1842928f
Meredith <bitcababy@gmail.com>
2013-01-26 10:34:17 -0500

    Removed some fossil code

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -60,40 +60,6 @@ class CoursesController < ApplicationController
 	#				end
 	#			end
 	#		end
- 
-	##
-	## Panes
-	##
-  # def resources_pane
-  #   respond_to do |format|
-  #     format.html { render :layout => !request.xhr? }
-  #    end
-  # end
-  # 
-  # def information_pane
-  #   respond_to do |format|
-  #     format.html { render :layout => !request.xhr? }
-  #   end
-  # end
-  # 
-  # def news_pane
-  #   respond_to do |format|
-  #     format.html { render :layout => !request.xhr? }
-  #   end
-  # end
-  # 
-  # def policies_pane
-  #   respond_to do |format|
-  #     format.html { render :layout => !request.xhr? }
-  #   end
-  # end
-  # 
-  # def sections_pane
-  #   respond_to do |format|
-  #     format.html { render :layout => !request.xhr? }
-  #   end
-  # end
-	
 	protected
 	
 	def find_course
