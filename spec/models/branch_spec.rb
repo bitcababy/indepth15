53008ffeee1030027adc969993cdc5180c03870f
Meredith <bitcababy@gmail.com>
2013-01-28 16:13:38 -0500

    Removed fossil test from Branch spec

--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -3,17 +3,5 @@ require 'spec_helper'
 describe Branch do
 	it {should validate_presence_of :name}
 	it {should validate_length_of :name}
-	
-	describe '#create_major_tags' do
-		it "creates a major tag for each tag in the MAJOR_TAGS hash" do
-			branch_name = Branch::MAJOR_TAGS.keys.first
-			tags = Branch::MAJOR_TAGS[branch_name]
-			branch = Branch.new name: branch_name
-			branch.create_major_tags
-			for tag in tags do
-				MajorTag.where(name: tag).should exist
-			end
-		end
-	end
 
 end
