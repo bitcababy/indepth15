898c745693c72beca8aa7d52bcc0b00106dac6ad
Meredith <bitcababy@gmail.com>
2013-01-28 16:14:18 -0500

    Fixed Courses request spec

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'Courses' do
 	it "shows a course page" do
 		course = Fabricate(:course)
-		get course_path(course)
-		response.should render_template(:show)
+		get home_course_path(course)
+		response.should render_template(:home)
 	end
 end
