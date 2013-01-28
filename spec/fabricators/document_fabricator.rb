ce35f8b31199bddfde654a440cf28fc4ed0a6e35
Meredith <bitcababy@gmail.com>
2013-01-28 16:23:26 -0500

    Removed CourseDocument from fabricators

--- spec/fabricators/document_fabricator.rb
+++ spec/fabricators/document_fabricator.rb
@@ -21,8 +21,6 @@ Fabricator :assignment, from: :text_document, class_name: :assignment  do
 	assgt_id						{ sequence(1)}
 end
 
-Fabricator :course_document, from: :text_document, class_name: :course_document
-
 Fabricator :department_document, from: :text_document, class_name: :department_document do
   title     "foo"
   content   "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
