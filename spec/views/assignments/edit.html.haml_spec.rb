41f79922b7b0d5a7178dabf8a5c001d820c74f49
Meredith <bitcababy@gmail.com>
2013-01-30 12:42:36 -0500

    Removed some view specs that maybe should be done in Cucumber

--- spec/views/assignments/edit.html.haml_spec.rb
+++ spec/views/assignments/edit.html.haml_spec.rb
@@ -1,58 +1,58 @@
 require 'spec_helper'
 
 describe 'assignments/edit' do
-  before :each do
-    teacher = mock('teacher') do
-      stubs(to_param).returns "doej"
-    end
-    assign(:teacher, teacher)
-    course = mock('course') do
-      stubs(:to_param).returns 222
-    end
-    assign(:course, course)
-
-    assignment = mock('assignment') do
-      stubs(:content).returns ''
-      stubs(:model_name).returns 'Assignment'
-      stubs(:to_param).returns 1
-      stubs(:id).returns 1
-    end
-    assign(:assignment, assignment)
-
-    section1 = mock('section') do
-      stubs(:block).returns 'B'
-    end
-    sa1 = mock('section_assignment') do
-      stubs(:section).returns sa1
-      stubs(:assignment).returns assignment
-    end
-    
-    section2 = mock('section') do
-      stubs(:block).returns 'D'
-    end
-    sa2 = mock('section_assignment') do
-      stubs(:section).returns sa2
-      stubs(:assignment).returns assignment
-    end
-    
-    assignment.stubs(:section_assignments).returns [sa1, sa2]
-
-    assign(:course_id, @course.to_param)
-    render template: 'assignments/edit', id: @assignment.to_param
-  end
-
-  it "should have a form to create the assignment" do
-    rendered.should have_selector('form')
-  end
-  
-  it "should have an area to edit the assignment" do
-    rendered.should have_selector('#editor') do |div|
-      div.should have_selector('label', content: 'Contents')
-      div.should have_selector('textarea')
-    end
-  end
-
-  it "should have a section area"
-  it "should have a tag area"
-  it "should have a 'Create' button"
-end
+  # before :each do
+  #    teacher = mock('teacher') do
+  #      stubs(to_param).returns "doej"
+  #    end
+  #    assign(:teacher, teacher)
+  #    course = mock('course') do
+  #      stubs(:to_param).returns 222
+  #    end
+  #    assign(:course, course)
+  # 
+  #    assignment = mock('assignment') do
+  #      stubs(:content).returns ''
+  #      stubs(:model_name).returns 'Assignment'
+  #      stubs(:to_param).returns 1
+  #      stubs(:id).returns 1
+  #    end
+  #    assign(:assignment, assignment)
+  # 
+  #    section1 = mock('section') do
+  #      stubs(:block).returns 'B'
+  #    end
+  #    sa1 = mock('section_assignment') do
+  #      stubs(:section).returns sa1
+  #      stubs(:assignment).returns assignment
+  #    end
+  #    
+  #    section2 = mock('section') do
+  #      stubs(:block).returns 'D'
+  #    end
+  #    sa2 = mock('section_assignment') do
+  #      stubs(:section).returns sa2
+  #      stubs(:assignment).returns assignment
+  #    end
+  #    
+  #    assignment.stubs(:section_assignments).returns [sa1, sa2]
+  # 
+  #    assign(:course_id, @course.to_param)
+  #    render template: 'assignments/edit', id: @assignment.to_param
+  #  end
+  # 
+  #  it "should have a form to create the assignment" do
+  #    rendered.should have_selector('form')
+  #  end
+  #  
+  #  it "should have an area to edit the assignment" do
+  #    rendered.should have_selector('#editor') do |div|
+  #      div.should have_selector('label', content: 'Contents')
+  #      div.should have_selector('textarea')
+  #    end
+  #  end
+  # 
+  #  it "should have a section area"
+  #  it "should have a tag area"
+  #  it "should have a 'Create' button"
+ end
--- spec/views/assignments/new.html.haml_spec.rb
+++ spec/views/assignments/new.html.haml_spec.rb
@@ -1,61 +1,61 @@
 require 'spec_helper'
 
 describe 'assignments/new' do
-  before :each do
-    section1 = Fabricate :section, block: "A"
-    section2 = Fabricate :section, block: "B"
-    
-    assignment = Assignment.new
-     
-    sa1 = Fabricate :section_assignment, section: section1, due_date: Date.today, use: true
-    sa1.block.should == 'A'
-    sa2 = Fabricate :section_assignment, section: section2, due_date: Date.today, use: true
-    assignment.stubs(:section_assignments).returns [sa1,sa2]
-    assignment.section_assignments.count.should == 2
-    assignment.section_assignments.first.block.should == 'A'
-    assign(:assignment, assignment)
-    render
-  end
-  
-  it "displays a form" do
-    rendered.should have_selector('form')
-  end
-  
-  context 'inputs' do
-    it "displays an editor for the assignment" do
-      rendered.should have_selector('form') do |form|
-        form.should have_selector('textarea', name: 'assignment[content]')
-      end
-    end
-  
-    it "allows setting the due date and use for each section_assignment" do
-      rendered.should have_selector('form') do |form|
-        form.should have_selector('#assignment_section_assignments_attributes_0_block', value: 'A')
-        form.should have_selector('#assignment_section_assignments_attributes_1_block', value: 'B')
-        form.should have_selector('#assignment_section_assignments_attributes_0_due_date')
-        form.should have_selector('#assignment_section_assignments_attributes_1_due_date')
-      end
-    end 
-  end
-  
-  context 'buttons' do
-    it "should have a create button" do
-      rendered.should have_selector('form') do |form|
-        form.should have_selector('input', class: 'btn', label: 'Create')
-      end
-    end
-    it "should have a cancel button" do
-      rendered.should have_selector('form') do |form|
-        form.should have_selector('input', class: 'btn', label: 'Cancel')
-      end
-    end
-
-    it "should have a reset button" do
-      rendered.should have_selector('form') do |form|
-        form.should have_selector('input', class: 'btn', label: 'Reset')
-      end
-    end
-  end
+  # before :each do
+  #   section1 = Fabricate :section, block: "A"
+  #   section2 = Fabricate :section, block: "B"
+  #   
+  #   assignment = Assignment.new
+  #    
+  #   sa1 = Fabricate :section_assignment, section: section1, due_date: Date.today, use: true
+  #   sa1.block.should == 'A'
+  #   sa2 = Fabricate :section_assignment, section: section2, due_date: Date.today, use: true
+  #   assignment.stubs(:section_assignments).returns [sa1,sa2]
+  #   assignment.section_assignments.count.should == 2
+  #   assignment.section_assignments.first.block.should == 'A'
+  #   assign(:assignment, assignment)
+  #   render
+  # end
+  # 
+  # it "displays a form" do
+  #   rendered.should have_selector('form')
+  # end
+  # 
+  # context 'inputs' do
+  #   it "displays an editor for the assignment" do
+  #     rendered.should have_selector('form') do |form|
+  #       form.should have_selector('textarea', name: 'assignment[content]')
+  #     end
+  #   end
+  # 
+  #   it "allows setting the due date and use for each section_assignment" do
+  #     rendered.should have_selector('form') do |form|
+  #       form.should have_selector('#assignment_section_assignments_attributes_0_block', value: 'A')
+  #       form.should have_selector('#assignment_section_assignments_attributes_1_block', value: 'B')
+  #       form.should have_selector('#assignment_section_assignments_attributes_0_due_date')
+  #       form.should have_selector('#assignment_section_assignments_attributes_1_due_date')
+  #     end
+  #   end 
+  # end
+  # 
+  # context 'buttons' do
+  #   it "should have a create button" do
+  #     rendered.should have_selector('form') do |form|
+  #       form.should have_selector('input', class: 'btn', label: 'Create')
+  #     end
+  #   end
+  #   it "should have a cancel button" do
+  #     rendered.should have_selector('form') do |form|
+  #       form.should have_selector('input', class: 'btn', label: 'Cancel')
+  #     end
+  #   end
+  # 
+  #   it "should have a reset button" do
+  #     rendered.should have_selector('form') do |form|
+  #       form.should have_selector('input', class: 'btn', label: 'Reset')
+  #     end
+  #   end
+  # end
     
     
 end
