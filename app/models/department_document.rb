8626fa94cf27a05eea0049a0aec978254a0d3e7a
Meredith <bitcababy@gmail.com>
2013-01-30 12:37:48 -0500

    Changed DepartmentDocument's parent to TitledDocument

--- app/models/department_document.rb
+++ app/models/department_document.rb
@@ -1,12 +1,6 @@
-class DepartmentDocument < TextDocument
+class DepartmentDocument < TitledDocument
   include Mongoid::Ordered
-  # include Mongoid::Locker
-  # enable_locking_with :locked_at
 
-  field :title, type: String, default: ""
-  
-  validates :title, presence: true, length: {minimum: 3 }
-  
   ordered_on :pos
   belongs_to :department
 
