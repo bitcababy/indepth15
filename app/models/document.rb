b35dbda812b68f22f7be4f0da4791e2b909e28bd
Meredith <bitcababy@gmail.com>
2013-02-04 17:49:08 -0500

    Added :updated_by to Documents.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,13 +1,14 @@
 class Document
 	include Mongoid::Document
-	include Mongoid::Paranoia
+	include Mongoid::Paranoia unless Rails.env == 'test'
   include Mongoid::Timestamps
   
   field :lv, as: :lock_version, type: Integer, default: 0
+  belongs_to :updated_by, class_name: 'User'
   
-  # before_update :check_lock_version
+  before_update :check_lock_version
   
-  # This is a basic, no associated documents version
+  # This is for optimistic locking
   def check_lock_version
     self.lock_version += 1
     prev = self.class.find self.to_param
