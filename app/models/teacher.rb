9b310d922c8dafb07aac80d6daee495ad8a97d5a
Meredith <bitcababy@gmail.com>
2013-02-04 17:50:13 -0500

    Track teachers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,11 +1,14 @@
 class Teacher < Author
+  include Mongoid::History::Trackable
 
 	field :cu, as: :current, type: Boolean
 	field :dr, as: :default_room, type: String
 	field :hp, as: :home_page, type: String
 	field :gm, as: :generic_msg, type: String
-	field :cm, as: :current_msg, type:String
-	field :um, as: :upcoming_msg, type:String
+	field :cm, as: :current_msg, type: String
+	field :um, as: :upcoming_msg, type: String
+
+  track_history on: [:current, :default_room, :home_page, :generic_msg, :current_msg, :upcoming_msg], version_field: :version
 
 	has_many :sections
 	
