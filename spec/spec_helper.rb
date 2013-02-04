398f93d286b8cc884bc3d926d1b47aa14e213434
Meredith <bitcababy@gmail.com>
2013-02-04 18:00:53 -0500

    Added Capybara setup to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,6 +37,12 @@ Spork.prefork do
 
   include Utils
 
+  Capybara.configure do |c|
+  	c.default_driver = :rack_test
+  	c.javascript_driver = :webkit
+  	c.default_selector = :css
+  end
+
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("app/lib/extras/**/*.rb")].each {|f| require f}
@@ -56,14 +62,11 @@ Spork.prefork do
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
 
-    Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
   	config.include Mongoid::Matchers
     config.include(EmailSpec::Helpers)
     config.include(EmailSpec::Matchers)
     config.include Devise::TestHelpers, :type => :controller
     config.include Devise::TestHelpers, :type => :view
-    config.extend ControllerMacros, :type => :controller
-    config.extend ViewMacros, :type => :views
 
     DatabaseCleaner.orm = "mongoid"
 
@@ -85,7 +88,7 @@ Spork.each_run do
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("app/lib/extras/**/*.rb")].each {|f| require f}
-
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   # if( ENV['COVERAGE'] == 'on' )
     require 'simplecov'
@@ -108,37 +111,3 @@ Spork.each_run do
   # end
 end
 
-# --- Instructions ---
-# Sort the contents of this file into a Spork.prefork and a Spork.each_run
-# block.
-#
-# The Spork.prefork block is run only once when the spork server is started.
-# You typically want to place most of your (slow) initializer code in here, in
-# particular, require'ing any 3rd-party gems that you don't normally modify
-# during development.
-#
-# The Spork.each_run block is run each time you run your specs.  In case you
-# need to load files that tend to change during development, require them here.
-# With Rails, your application modules are loaded automatically, so sometimes
-# this block can remain empty.
-#
-# Note: You can modify files loaded *from* the Spork.each_run block without
-# restarting the spork server.  However, this file itself will not be reloaded,
-# so if you change any of the code inside the each_run block, you still need to
-# restart the server.  In general, if you have non-trivial code in this file,
-# it's advisable to move it into a separate file so you can easily edit it
-# without restarting spork.  (For example, with RSpec, you could move
-# non-trivial code into a file spec/support/my_helper.rb, making sure that the
-# spec/support/* files are require'd from inside the each_run block.)
-#
-# Any code that is left outside the two blocks will be run during preforking
-# *and* during each_run -- that's probably not what you want.
-#
-# These instructions should self-destruct in 10 seconds.  If they don't, feel
-# free to delete them.
-
-
-
-
-
-# This file is copied to spec/ when you run 'rails generate rspec:install'
