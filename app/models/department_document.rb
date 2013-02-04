bb5b88d38abfe57318f465773cbf8e93eab3542a
Meredith <bitcababy@gmail.com>
2013-02-04 17:48:30 -0500

    Track DepartmentDocuments

--- app/models/department_document.rb
+++ app/models/department_document.rb
@@ -1,7 +1,10 @@
 class DepartmentDocument < TitledDocument
   include Mongoid::Ordered
+  include Mongoid::History::Trackable
 
   ordered_on :pos
   belongs_to :department
+
+  track_history version_field: :version
 
 end
