f5ba783993100d4b59078e2ed8a4af47de35239a
Meredith <bitcababy@gmail.com>
2013-02-04 17:59:05 -0500

    Created fabricator for :department_with_docs

--- spec/fabricators/department_fabricator.rb
+++ spec/fabricators/department_fabricator.rb
@@ -1,3 +1,10 @@
 Fabricator(:department) do
-  name    { "Foobar" }
+  name              { sequence(:dept_name) {|i| "Department #{i}"} }
 end
+
+Fabricator :department_with_docs, from: :department do
+  after_create { |dept|
+    3.times { Fabricate :department_document, department: dept }
+  }
+end
+
