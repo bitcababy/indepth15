b8b21ff146291052fef75f7fcc858f10e792492f
Meredith <bitcababy@gmail.com>
2013-02-07 12:28:40 -0500

    Cosmetic

--- spec/views/courses/_sections_table.html.haml_spec.rb
+++ spec/views/courses/_sections_table.html.haml_spec.rb
@@ -3,16 +3,16 @@ require 'spec_helper'
 describe 'courses/_sections_table' do
 	include CourseMockHelpers
 
-	it "creates a table of a courses sections" do
+  it "creates a table of a courses sections" do
     course = mock_course_with_sections
     stub_template 'courses/section_table_row' => "A row"
-		render partial: 'courses/sections_table', locals: {course: course}
-		rendered.should have_selector('table#sections_table thead tr') do |row|
-			%W(block teacher room).each do |hdr|
-				row.should have_selector('th', class: hdr)
-			end
-		end
-	end
+    render partial: 'courses/sections_table', locals: {course: course}
+    rendered.should have_selector('table#sections_table thead tr') do |row|
+      %W(block teacher room).each do |hdr|
+        row.should have_selector('th', class: hdr)
+      end
+    end
+  end
 		
 end
 
