c8c630c1480d196dd6582c83dd7b97ae01878cd2
Meredith <bitcababy@gmail.com>
2013-02-07 12:07:21 -0500

    Changed Assignment parent to Document, adding content field

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,4 @@
-class Assignment < TextDocument
+class Assignment < Document
   include Mongoid::History::Trackable
   include Mongoid::TaggableWithContext
   include Mongoid::TaggableWithContext::AggregationStrategy::RealTime
@@ -7,6 +7,8 @@ class Assignment < TextDocument
   	field :assgt_id, type: Integer
   	validates :assgt_id, uniqueness: true
   end
+  
+  field :co, as: :content, type: String, default: ""
   
   # A section_assignment is meaningless if the assignment is deleted
   has_many :section_assignments, dependent: :delete, autosave: true
