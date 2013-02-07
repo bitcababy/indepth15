cabe39da93d5085bf73f86766450a61d24aca354
Meredith <bitcababy@gmail.com>
2013-02-07 12:28:19 -0500

    Changed courses/_sections_pane spec to reflect embedded documents

--- spec/views/courses/_sections_pane.html.haml_spec.rb
+++ spec/views/courses/_sections_pane.html.haml_spec.rb
@@ -5,22 +5,21 @@ require 'spec_helper'
 describe "courses/_sections_pane" do
 	include CourseMockHelpers
 
-	it "display a pane with the sections of a course" do
+  it "display a pane with the sections of a course" do
     view.stubs(:assignments_page_path).returns "foo"
-		course = mock_course_with_sections(6)
-		assign(:course, course)
-	
-		course.stubs(:number).returns 321
-		course.stubs(:duration).returns Course::FULL_YEAR
-		course.stubs(:credits).returns 5.0
-		t = mock_text_doc("Some description")
-		course.stubs(:description).returns t
-		render partial: "courses/sections_pane", locals: {course: course}
-		rendered.should have_selector('div#sections_pane') do |div|
-			div.should contain('Course 321 — Full Year ')
-			div.should contain("Some description")
-			div.should contain("In the #{academic_year_string(Settings.academic_year)} academic year there are 6 sections of Fractals 101.")
-		end
-	end
+    course = mock_course_with_sections(6)
+    assign(:course, course)
+  
+    course.stubs(:number).returns 321
+    course.stubs(:duration).returns Course::FULL_YEAR
+    course.stubs(:credits).returns 5.0
+    course.stubs(:doc_of_kind).returns mock_text_doc("Some description")
+    render partial: "courses/sections_pane", locals: {course: course}
+    rendered.should have_selector('div#sections_pane') do |div|
+      div.should contain('Course 321 — Full Year ')
+      div.should contain("Some description")
+      div.should contain("In the #{academic_year_string(Settings.academic_year)} academic year there are 6 sections of Fractals 101.")
+    end
+  end
 	
 end
