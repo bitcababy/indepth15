3dc177882f557ff5cf24ca548f532fa54cee5347
Meredith <bitcababy@gmail.com>
2013-02-07 12:13:54 -0500

    Removed :lock_version from Document, since :version will do

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,23 +1,18 @@
 class Document
 	include Mongoid::Document
 	include Mongoid::Paranoia unless Rails.env == 'test'
-  include Mongoid::Timestamps
-  
-  field :lv, as: :lock_version, type: Integer, default: 0
-  belongs_to :updated_by, class_name: 'User'
-  
+  include Mongoid::Timestamps if Rails.env == "production"
+  include Mongoid::History::Trackable 
+    
   before_update :check_lock_version
-  
+
   # This is for optimistic locking
   def check_lock_version
-    self.lock_version += 1
-    prev = self.class.find self.to_param
-    return true unless prev
-    if prev.lock_version >= self.lock_version
-      self.lock_version -= 1
+    prev = self.history_tracks.last
+    if prev.version > self.version
       # Someone's saved it while I was editing it
       raise StaleDocument
-     end
+    end
   end
 
 end
