bc63b9dfc92c53d79f75be9077311e17072e1d64
Meredith <bitcababy@gmail.com>
2013-02-07 12:24:41 -0500

    Added versioning tests to Document

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,12 +1,28 @@
 require 'spec_helper'
 
+class TestDocument < Document
+  field :content, type: String, default: ""
+  track_history on: :content, track_create: true
+end
+
 describe Document do
-   subject = Fabricate :document
-  
-  describe '<=>' do
-    subject.position = 1
-    doc = Fabricate :document, position: 2
-    subject.should < doc
-  end  
+  describe 'lock checking' do
+    it "starts out with a version of 1" do
+      doc = TestDocument.create
+      doc.version.should == 1
+    end
     
+    it "bumps the version when it's updated" do
+      doc = TestDocument.create
+      doc.update_attributes content: "Foo"
+      doc.version.should == 2
+    end
+    
+    it "raises an StaleDocument exception if someone else has updated it" do
+      doc = TestDocument.create
+      d1 = TestDocument.first
+      d1.update_attributes content: "Foo"
+      expect {doc.update_attributes(content: "Bar")}.to raise_error(StaleDocument)
+    end
+  end    
 end
