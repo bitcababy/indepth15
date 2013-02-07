50083c8ec8c328d562690cdfd2b4980a018cba12
Meredith <bitcababy@gmail.com>
2013-02-07 12:23:37 -0500

    Fixed DepartmentDocumentsController spec to use embedded documents
    updating spec isn't working for some reason.

--- spec/controllers/department_documents_controller_spec.rb
+++ spec/controllers/department_documents_controller_spec.rb
@@ -3,26 +3,27 @@ require 'spec_helper'
 describe DepartmentDocumentsController do
   include DeviseHelpers
   before :each do
-    @doc = Fabricate :department_document
+    @dept = Fabricate :department_with_docs
   end
 
   describe '#edit, logged in' do
     login_user
-
     it "gets the doc referenced by params[:doc_id]" do
-      get :edit, id: @doc.to_param
-      assigns(:doc).should == @doc
+      doc = @dept.homepage_docs.first
+      xhr :get, :edit, dept_id: @dept.to_param, id: doc.to_param
+      assigns(:doc).should == doc
     end
   end
   
   describe '#save, logged_in' do
     login_user
     it "updates the doc" do
-      session[:goto_url] = "/"
-      put :update, id: @doc.to_param, department_document: {title: "Foo" }
-      response.should redirect_to "/"
+      doc = @dept.homepage_docs.first
+      pending
+      # put :update, dept_id: @dept.to_param, id: doc.to_param, department_document: {title: "Foo" }
+      xhr :put, :update, dept_id: @dept.to_param, id: doc.to_param, department_document: {title: "Foo" }
+      assigns(:doc).should == doc
     end
   end
       
-
 end
