5e8c2b15c1103786e48b70568a767180538d8be9
Meredith <bitcababy@gmail.com>
2013-02-07 12:25:08 -0500

    Added updating testing to SectionAssignment

--- spec/models/section_assignment_spec.rb
+++ spec/models/section_assignment_spec.rb
@@ -40,39 +40,14 @@ describe SectionAssignment do
 		
 	end
 	
-	# context "incoming" do
-	# 	describe '::handle_incoming' do
-	# 		before :each do
-	# 			course = Fabricate :course, number: 321
-	# 			Fabricate :assignment, assgt_id: 614639
-	# 			teacher = Fabricate :teacher, login: 'davidsonl'
-	# 			@section = Fabricate :section, academic_year: 2013, block: "D", course: course, teacher: teacher
-	# 
-	# 			@hash = {:teacher_id=>"davidsonl", 
-	# 				:year=>"2013", 
-	# 				:course_num=>"321",
-	# 				:block=>"D", 
-	# 				:due_date=>"2012-09-06", 
-	# 				:name=>"1", 
-	# 				:use_assgt=>"Y", 
-	# 				:assgt_id =>"614639", 
-	# 				:ada=>"2012-08-19 18:34:40", 
-	# 				:aa=>"2012-08-19 18:37:16"
-	# 			}
-	# 		end
-	# 		it "creates a new section_assignment if the assgt_id is new" do
-	# 			expect {
-	# 				SectionAssignment.handle_incoming @hash
-	# 			}.to change(@section.section_assignments, :count).by(1)
-	# 		end
-	# 		it "updates an section_assignment if the assgt_id is old" do
-	# 			SectionAssignment.handle_incoming @hash
-	# 			expect {
-	# 				SectionAssignment.handle_incoming @hash
-	# 			}.to change(@section.section_assignments, :count).by(0)
-	# 		end
-	# 	end
-	# end
-	
+  describe "test updating" do
+    before :each do
+      @sa = Fabricate :section_assignment
+    end
+    
+    it "should update itself" do
+      @sa.update_attributes({name: "foo"}).should be_true
+    end
+  end
 
 end
