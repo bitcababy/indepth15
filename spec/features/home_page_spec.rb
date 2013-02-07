e511a823c86aa6d37033999e479c5d5c16cb3a00
Meredith <bitcababy@gmail.com>
2013-02-07 12:24:00 -0500

    Added scenarios for home page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,17 +2,12 @@ require 'spec_helper'
 
 feature "Home page" do
   before :each do
-    @dept = Fabricate :department
-    @docs = [
-      Fabricate(:department_document, department: @dept, title: 'Tab 2', content: 'Content 2', pos: 2),
-      Fabricate(:department_document, department: @dept, title: 'Tab 1', content: 'Content 1', pos: 1),
-      Fabricate(:department_document, department: @dept, title: 'Tab 3', content: 'Content 2', pos: 3)
-    ]
-    @docs.sort!
-  end
-
-  scenario "User visits the home page", js: true do
+    @dept = Fabricate :department_with_docs
+    @docs = @dept.homepage_docs
     visit home_path
+   end
+
+  scenario "should show an accordion", js: true do
     within '.ui-accordion' do
       titles = page.all('.pane-title')
       titles.count.should eq @docs.count
@@ -26,8 +21,7 @@ feature "Home page" do
     end
   end # User visits the home page
   
-  scenario "User click a closed pane", js: true do
-    visit home_path
+  scenario "should toggle panes", js: true do
     within '.ui-accordion' do
       titles = page.all('.pane-title')
       titles[1].click
@@ -35,5 +29,33 @@ feature "Home page" do
       titles[0].should_not have_class 'ui-state-active'
     end
   end
-  
+
+end
+
+feature "Home page" do
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+  before :each do
+    @dept = Fabricate :department_with_docs
+    @docs = @dept.homepage_docs
+    login_as Fabricate(:user), scope: :user
+    visit home_path
+   end
+
+  scenario "should show edit buttons for logged-in users", js: true do
+     within '.ui-accordion' do
+       for cont in page.all('.pane-content') do
+         cont.should have_selector('button.edit-button')
+       end
+     end
+   end
+
+  scenario "should open an editor in a dialog when an edit button is clicked", js: true do
+    within '.ui-accordion' do
+      page.first('button.edit-button').click
+    end
+    page.should have_selector('.ui-dialog')
+  end
+   
 end
