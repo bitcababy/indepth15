ddd3189999c2bbcfe37851fdd76eb4de3d7a5964
Meredith <bitcababy@gmail.com>
2013-02-10 10:52:49 -0500

    SectionAssignment: added indexes to relations.

--- app/models/section_assignment.rb
+++ app/models/section_assignment.rb
@@ -2,7 +2,6 @@ class SectionAssignment
 	include Mongoid::Document
   include Mongoid::Timestamps
   include Mongoid::History::Trackable
-	include Utils
 
 	field :dd, as: :due_date, type: Date
 	field :na, as: :name, type: String, default: ""
@@ -14,10 +13,10 @@ class SectionAssignment
 
 	index({due_date: -1, use: 1})
 	
-	belongs_to :section
-  delegate :block, :block=, :course, to: :section
-	belongs_to :assignment
+	belongs_to :section, index: true
+	belongs_to :assignment, index: true
   delegate :content, :content=, to: :assignment
+  delegate :block, to: :section
 
 	scope :due_after,	->(date) { gt(due_date: date) }
   scope :due_on, ->(date) { where(due_date: date) }
