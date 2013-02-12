4ac728d7ea303ca8b3e57f8cb7e98f6402d5165d
Meredith <bitcababy@gmail.com>
2013-02-12 10:41:07 -0500

    Created form for Assignments#new
    Not finished, but getting there

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -1,32 +1,55 @@
 require 'spec_helper'
 
 describe AssignmentsController do
-  # describe "GET 'new'" do
-  #   before :each do
-  #     course = Fabricate :course, number: 322
-  #     teacher = Fabricate :teacher, login: 'doej'
-  #     Fabricate :section, course: course, teacher: teacher
-  #     Fabricate :section, course: course, teacher: teacher
-  #     puts teacher.sections
-  #     teacher.sections.for_course(course).count.should == 2
-  #     get 'new', course_id: course.to_param, teacher_id: teacher.to_param
-  #   end
-  #   it "should succeed" do
-  #     response.should be_success
-  #   end
-  #   it "should create a new assignment" do
-  #       assigns[:assignment].should_not be_nil
-  #   end
-  #   it "should create two new section assignments" do
-  #     asst = assigns[:assignment]
-  #     asst.should_not be_nil
-  #     asst.section_assignments.should_not be_nil
-  #     asst.section_assignments.count.should == 2
-  #   end
-  #   it "should render the right template" do
-  #     response.should render_template('new')
-  #   end
-  #   
-  # end
+  describe "GET 'new'" do
+    before :each do
+      teacher = Fabricate :teacher, login: "xyzzy"
+      course = Fabricate :course
+      @cs = (0..2).collect {|i| 
+        Fabricate :section, teacher: teacher, course: course, academic_year: Settings.academic_year, block: ('A'..'H').to_a[i]
+      }
+      @os = (0..1).collect {|i| 
+        Fabricate :section, teacher: teacher, course: course, academic_year: Settings.academic_year-1, block: ('A'..'H').to_a[i]
+      }
+      
+      get :new, teacher_id: teacher.to_param, course_id: course.to_param
+      response.should be_success
+    end
+    it "should create a new assignment" do
+      assigns[:assignment].should be_kind_of Assignment
+    end
+    
+    it "should build section assignments for each section and the new assignment" do
+      assigns[:sas].should be_kind_of Array
+      assigns[:sas].count.should == @cs.size
+      assigns[:sas].each do |sa|
+         @cs.should include sa.section
+      end
+    end
+    
+    it "should render the 'new' template" do
+      response.should render_template :new
+    end
+      
+  end
   
+  # {"utf8"=>"✓",
+  #  "authenticity_token"=>"Dg2hSqMqJnqxMxJm8wAwc74Yqk57LYHviLUR8/5ea/s=",
+  #  "assignment"=>{"section_assignments_attributes"=>{"0"=>{"due_date"=>"2013-02-12",
+  #  "use"=>"1"},
+  #  "1"=>{"due_date"=>"2013-02-12",
+  #  "use"=>"1"}},
+  #  "content"=>""}}
+  
+  describe "PUT create" do
+    it "saves the assignment" do
+      put :create, assignment: {"section_assignments_attributes"=>{"0"=>{"due_date"=>"2013-02-12",
+             "use"=>"1"},
+             "1"=>{"due_date"=>"2013-02-12",
+             "use"=>"1"}},
+             "content"=>"Some content"}
+    end
+    it "saves the section assignments"
+  end
+      
 end
