777d40929deecd51313143c7e7fa8481a117f12e
Meredith <bitcababy@gmail.com>
2013-02-12 10:43:35 -0500

    Added validation of number to course spec

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,30 +2,13 @@ require 'spec_helper'
 
 describe Course do
 	it { should have_many(:sections) }
+	it { should validate_presence_of(:number)}
 	it { should validate_uniqueness_of(:number)}
 	it { should embed_many :documents }
 	
-	context "Fabricator" do
-		it "creates courses with unique numbers" do
-			course1 = Fabricate.build(:course)
-			course2 = Fabricate.build(:course)
-			course1.number.should_not == course2.number
-		end
-	end
-		
-	
-	context "changes" do
-		before :each do
-			@course = Fabricate(:course)
-		end
-
-		it "should have one more section when it adds a section" do
-			expect {
-				@course.sections << Fabricate(:section, course: @course)
-			}.to change {@course.sections.count}.by(1)
-		end
-
-	end
+  # expect {
+  #   sections << Fabricate(:section, course: @course)
+  # }.to change { subject.sections.count }.by(1)
 
 	# context '#teachers' do
 	# 	it "should return a list of the teachers teaching the course" do 
