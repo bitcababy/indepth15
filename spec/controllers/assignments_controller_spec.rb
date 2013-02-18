ae0c50521a9ce844080b24a7952ea2cc51115f66
Meredith <bitcababy@gmail.com>
2013-02-18 13:01:57 -0500

    Added a "before :each" @course variable to AssignmentsController spec

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -7,7 +7,7 @@ describe AssignmentsController do
   describe "GET 'new'" do
     before :each do
       teacher = Fabricate :teacher, login: "xyzzy"
-      course = Fabricate :course
+      @course = Fabricate :course
       @course.major_topics = [
         Fabricate(:none_topic),
         Fabricate(:major_topic, name: "Quadratics"), 
@@ -16,25 +16,34 @@ describe AssignmentsController do
         Fabricate(:major_topic, name: 'Systems of Equations'), 
       ]
       @cs = (0..2).collect {|i| 
-        Fabricate :section, teacher: teacher, course: course, academic_year: Settings.academic_year, block: ('A'..'H').to_a[i]
+        Fabricate :section, teacher: teacher, course: @course, academic_year: Settings.academic_year, block: ('A'..'H').to_a[i]
       }
       @os = (0..1).collect {|i| 
-        Fabricate :section, teacher: teacher, course: course, academic_year: Settings.academic_year-1, block: ('A'..'H').to_a[i]
+        Fabricate :section, teacher: teacher, course: @course, academic_year: Settings.academic_year-1, block: ('A'..'H').to_a[i]
       }
       
-      get :new, teacher_id: teacher.to_param, course_id: course.to_param
+      get :new, teacher_id: teacher.to_param, course_id: @course.to_param
       response.should be_success
     end
-    it "should create a new assignment" do
+
+    it "should build an assignment and section assignments for each current section" do
       assigns[:assignment].should be_kind_of Assignment
-    end
-    
-    it "should build section assignments for each section and the new assignment" do
-      assigns[:sas].should be_kind_of Array
       assigns[:sas].count.should == @cs.size
       assigns[:sas].each do |sa|
          @cs.should include sa.section
       end
+    end
+    
+    it "should not create section assignments for old sections" do
+      assigns[:sas].each do |sa|
+         @os.should_not include sa.section
+      end
+    end
+    
+    it "should assign all of topic names to @major_topics" do
+      assigns[:major_topics].should be_kind_of Array
+      names = MajorTopic.names_for_topics(assigns[:major_topics])
+      assigns[:major_topics].should == names
     end
     
     it "should render the 'new' template" do
@@ -50,7 +59,7 @@ describe AssignmentsController do
   #    "section_assignments_attributes"=>{
   #      "0"=>{"due_date"=>"2013-02-14", "use"=>"1", "section"=>"2013/321/davidsonl/B"},
   #      "1"=>{"due_date"=>"2013-02-14", "use"=>"1", "section"=>"2013/321/davidsonl/D"}},
-  #      "major_tags"=>["", "Similarity", "Measurement"],
+  #      "major_topics"=>["", "Similarity", "Measurement"],
   #      "name"=>"",
   #      "content"=>""
   #   }
@@ -95,7 +104,6 @@ describe AssignmentsController do
           "0"=>{"due_date"=>"2013-02-12", "use"=>"1", "block"=>"B", "section"=>s1.to_param},
           "1"=>{"due_date"=>"2013-02-12", "use"=>"1", "block"=>"D", "section"=>s2.to_param}
           },
-          "major_tags"=>["", "Similarity", "Measurement"],
           "content"=>"Some content",
           "name" => "foo",
          }
@@ -105,12 +113,12 @@ describe AssignmentsController do
     end
 
     it "creates the assignment" do
-      expect { put :create, @parms
-      }.to change{ Assignment.count }.by(1)
+      expect { xhr :put, :create, @parms }
+      .to change{ Assignment.count }.by(1)
     end
     it "creates the section_assignments" do
-      expect { put :create, @parms
-      }.to change{ SectionAssignment.count }.by(2)
+      expect { xhr :put, :create, @parms }
+      .to change{ SectionAssignment.count }.by(2)
     end
   end
       
