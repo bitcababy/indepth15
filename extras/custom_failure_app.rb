b1355a45ec1b94002830493a27797747f799d55f
Meredith <bitcababy@gmail.com>
2013-02-18 12:57:10 -0500

    Added bare minimum CustomFailureApp for devise

--- extras/custom_failure_app.rb
+++ extras/custom_failure_app.rb
@@ -1,11 +1,12 @@
-# # From: https://github.com/plataformatec/devise/wiki/How-To:-Do-not-redirect-to-login-page-after-session-timeout
-# class CustomFailureApp < Devise::FailureApp
-# 	def redirect
-# 		message = warden.message || warden_options[:message]
-# 		if message == :timeout
-# 			redirect_to attempted_path
-# 		else
-# 			super
-# 		end
-# 	end
-# end
+# From: https://github.com/plataformatec/devise/wiki/How-To:-Do-not-redirect-to-login-page-after-session-timeout
+class CustomFailureApp < Devise::FailureApp
+  def redirect
+    logger.warn "*** In CustomFailureApp ***"
+    message = warden.message || warden_options[:message]
+    if message == :timeout
+      redirect_to attempted_path
+    else
+      super
+    end
+  end
+end
