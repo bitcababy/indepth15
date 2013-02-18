59afd82be4e7a126c56aaa2f88a56405f559eaf6
Meredith <bitcababy@gmail.com>
2013-02-18 13:04:16 -0500

    Added a few tests to Assignment spec

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -3,6 +3,10 @@
 require 'spec_helper'
 
 describe Assignment do
+  it { should have_many :section_assignments }
+  it { should belong_to :teacher }
+  specify { subject.minor_topics.should be_kind_of SortedSet }
+
 	context "Fabricator" do
 		subject { Fabricate(:assignment) }
 		specify { subject.content.should_not be_nil }
