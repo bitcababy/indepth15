3f998bc23c52a295ca7e92f4d938570642f05a1c
Meredith <bitcababy@gmail.com>
2013-02-18 13:02:47 -0500

    Changed assignment fabricator to include major_topics

--- spec/fabricators/assignment_fabricator.rb
+++ spec/fabricators/assignment_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator :assignment, from: :document, class_name: :assignment  do
 	content							""
 	section_assignments []
-	oid						{ sequence(1) }
-  major_tags        []
+	oid						      { sequence(1) }
+  major_topics        SortedSet.new
 end
