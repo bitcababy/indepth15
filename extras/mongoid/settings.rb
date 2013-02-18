8f462983ede3ab27d21aebbe64367bd7536ef6e4
Meredith <bitcababy@gmail.com>
2013-02-18 13:06:00 -0500

    Started work on a Mongoid::Settings class

---
+++ extras/mongoid/settings.rb
@@ -0,0 +1,38 @@
+class Mongoid::Settings
+  field :name, kind: String
+  field :settings, kind: Hash, default: {}
+  field :_id, type: String, -> { name }
+    
+  ROOT_NAMESPACE = "__ROOT__"
+    
+  scope for_namespace  ->(n) { where(nameplace: n) }
+
+    
+  class << self do
+    attr_accessor :current_doc
+    
+    def in_namespace_set(ns, hash = {})
+      sdoc = self.find_or_create_by name: ns
+      sdoc.settings.merge! hash
+      sdoc.save!
+    end
+
+    alias []= set
+    def set(k, v)
+     return self.current_doc.settings[k] = v
+    end
+  
+    alias [] get
+    def get(k)
+      return self.current_doc.settings[k]
+    end
+  end
+ 
+  protected
+  
+  def current_doc
+    @current_doc ||= Mongoid::Settings.create name: ROOT_NAMESPACE
+  end
+  
+    
+end
