7e3dbb1c7fb136bdf8874c009ee943acdba8c61e
Meredith <bitcababy@gmail.com>
2013-02-18 12:49:41 -0500

    Added topic code
    Not being used in first release.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -10,15 +10,23 @@ class Assignment < Document
   
   field :co, as: :content, type: String, default: ""
   field :name, type: String, default: ""
+  field :mit, as: :minor_topics, type: SortedSet, default: SortedSet.new
   
   # A section_assignment is broken if the assignment is deleted
   has_many :section_assignments, dependent: :delete
   belongs_to :teacher
   
-  has_and_belongs_to_many :major_tags
+  has_and_belongs_to_many :major_topics
 
   accepts_nested_attributes_for :section_assignments
 
   track_history except: [:oid], track_create: true
+  
+  def add_major_topics(tags=[])
+    for tag in tags do
+      self.major_topics.find_or_create_by name: tag
+    end
+    self.save!
+  end
 
 end
