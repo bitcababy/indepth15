db3c7e46806ea109318cfd428fe568a593bfd75b
Meredith <bitcababy@gmail.com>
2013-02-18 13:03:51 -0500

    Cleaned up DepartmentDocumentsController spec a bit

--- spec/controllers/department_documents_controller_spec.rb
+++ spec/controllers/department_documents_controller_spec.rb
@@ -2,12 +2,13 @@ require 'spec_helper'
 
 describe DepartmentDocumentsController do
   include DeviseHelpers
+  login_user
+
   before :each do
     @dept = Fabricate :department_with_docs
   end
 
   describe '#edit, logged in' do
-    login_user
     it "gets the doc referenced by params[:doc_id]" do
       doc = @dept.homepage_docs.first
       xhr :get, :edit, dept_id: @dept.to_param, id: doc.to_param
@@ -16,13 +17,13 @@ describe DepartmentDocumentsController do
   end
   
   describe '#save, logged_in' do
-    login_user
     it "updates the doc" do
       doc = @dept.homepage_docs.first
       pending
       # put :update, dept_id: @dept.to_param, id: doc.to_param, department_document: {title: "Foo" }
       xhr :put, :update, dept_id: @dept.to_param, id: doc.to_param, department_document: {title: "Foo" }
       assigns(:doc).should == doc
+      response.should =~ /.*\/home$/
     end
   end
       
