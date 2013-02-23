5431f26e8bbb4138b37543db887e43f289bea7a7
Meredith <bitcababy@gmail.com>
2013-02-23 16:37:30 -0500

    Changed name validation in Department.

--- app/models/department.rb
+++ app/models/department.rb
@@ -5,15 +5,12 @@ class Department
   include Mongoid::Timestamps if Rails.env.production?
   
 	field :n, as: :name, type: String
-  validates :name, presence: true, length: { minimum: 3 }
   field :_id, type: String, default: -> { n }
 
+  validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
+ 
   embeds_many :homepage_docs, class_name: 'DepartmentDocument'
   
-  has_many :courses do
-    def in_catalog
-      @target.select {|c| c.in_catalog }
-    end
-  end
+  has_many :courses 
   
  end
