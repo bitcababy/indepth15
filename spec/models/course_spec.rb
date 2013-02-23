ce86df00e8d3c3b656bbeb2c74576e4bd0fd4e21
Meredith <bitcababy@gmail.com>
2013-02-23 16:53:39 -0500

    Added some method tests to Course spec

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,10 +1,48 @@
 require 'spec_helper'
 
 describe Course do
-	it { should have_many(:sections) }
-	it { should validate_presence_of(:number)}
+  [:number, :duration, :credits, :full_name].each do |field|
+	  it { should validate_presence_of(field)}
+  end
+
 	it { should validate_uniqueness_of(:number)}
+  
 	it { should embed_many :documents }
+	it { should have_many(:sections) }
+  it { should belong_to :department }
+  it { should have_and_belong_to_many :major_topics }
+  it { should have_many :browser_records }
+  # it { should have_and_belong_to_many :teachers }
+  # 
+  
+  describe '#branches' do
+    it "returns the branches that a course belongs to" do
+      course = Fabricate.build :course, number: Course::BRANCH_MAP.keys.sample
+      course.branches.should be_kind_of Array
+    end
+  end
+
+    
+  describe '#add_major_topics' do
+    it "adds the the major topics from the BRANCH_MAP" do
+      course = Fabricate.build :course, number: 321
+      course.add_major_topics
+      pending "unfinished test"
+    end
+  end
+
+  describe '#current_teachers' do
+    it "should return the teachers of the current sections" do
+      t1 = Fabricate :teacher
+      t2 = Fabricate :teacher
+      course = Fabricate :course
+      Fabricate :section, teacher: t1, course: course
+      Fabricate :section, teacher: t2, course: course
+      course.current_teachers.should contain t1
+      course.current_teachers.should contain t2
+    end
+  end
+  
 	
   # expect {
   #   sections << Fabricate(:section, course: @course)
