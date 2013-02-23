6bf195f6c1a4748effe8569980ec5ecd16fa361d
Meredith <bitcababy@gmail.com>
2013-02-23 16:56:44 -0500

    Set rspec treat_symbols_as_metadata_keys_with_true_values to true

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -63,6 +63,7 @@ Spork.prefork do
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
+    config.treat_symbols_as_metadata_keys_with_true_values = true
 
   	config.include Mongoid::Matchers
     config.include(EmailSpec::Helpers)
