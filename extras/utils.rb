e724e2a9d231c1ed54ab8b91e9b2ef84d13d3310
Meredith <bitcababy@gmail.com>
2013-02-23 16:44:16 -0500

    Cleaned up Utils by having instance methods call class methods

--- extras/utils.rb
+++ extras/utils.rb
@@ -3,6 +3,18 @@ module Utils
 	extend ActiveSupport::Concern
 
 	module ClassMethods
+		def next_school_day
+      day = Date.today
+			case
+			when day.friday?
+				day + 3
+			when day.saturday?
+				day + 2
+			else
+				day + 1
+			end
+		end
+
 		def before_cutoff?(time)
 			return !after_cutoff?(time)
 		end
@@ -14,52 +26,27 @@ module Utils
 		def future_due_date
 			time = Time.now.localtime
 			if after_cutoff?(time)
-				Date.tomorrow
+				next_school_day
 			else
 				Date.today
 			end
 		end
-
-		def next_school_day
-			case
-			when day.friday?
-				day + 3
-			when day.saturday?
-				day + 2
-			else
-				day + 1
-			end
-		end
-
 	end
 
 	def before_cutoff?(time)
-		return !after_cutoff?(time)
+		return self.class.before_cutoff?(time)
 	end
 
 	def after_cutoff?(time)
-		return time.hour > Settings.cutoff[:hour] || (time.hour == Settings.cutoff[:hour] && time.min >= Settings.cutoff[:minute])
+    return self.class.after_cutoff?(time)
 	end
 
 	def future_due_date
-		time = Time.now.localtime
-		if after_cutoff?(time)
-			Date.tomorrow
-		else
-			Date.today
-		end
+    return self.class.future_due_date
 	end
 
-	def next_school_day
-		day = Date.today
-		case
-		when day.friday?
-			day + 3
-		when day.saturday?
-			day + 2
-		else
-			day + 1
-		end
-	end
+  def next_school_day
+    return self.class.next_school_day
+  end
 
 end
