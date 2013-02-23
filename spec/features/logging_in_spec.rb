a948ddb3584fea816afa38e7459b93f42e6d040a
Meredith <bitcababy@gmail.com>
2013-02-23 16:53:17 -0500

    Added Warden::Test::Helpers to logging-in spec
    Lets it logout all users before each test

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 feature "Logging in" do
   include DeviseHelpers
+  include Warden::Test::Helpers
 
   before :each do
     @dept = Fabricate :department_with_docs
-    @user = Fabricate :user, login: 'doej', first_name: 'John', last_name: 'Doe', password: 'secret'
+    @user = Fabricate :user
+    logout
   end
 
   def open_login_dialog
@@ -16,6 +18,11 @@ feature "Logging in" do
     signin = menu.first '#signin'
     signin.should_not be_nil
     signin.click
+  end
+  
+  scenario "Guest logged in" do
+    visit home_path
+    page.should have_selector('#username', text: "Guest")
   end
   
   scenario "clicking 'Sign in' should bring up a sign-in dialog", js: true do
@@ -30,10 +37,10 @@ feature "Logging in" do
     dialog.should_not be_nil
     within '.ui-dialog form' do
       fill_in 'Login', with: @user.login
-      fill_in 'Password', with: 'secret'
+      fill_in 'Password', with: @user.password
     end
     click_button 'Sign in'
-    # current_user.should_not be_nil
-    pending "unfinished test"
+    page.should have_selector('#username')
+    page.should have_selector('#username', text: @user.full_name)
   end
 end
