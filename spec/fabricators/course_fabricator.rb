b61da5e76f38d8817bb92170df43b2fc8842ab91
Meredith <bitcababy@gmail.com>
2013-02-24 09:40:15 -0500

    Fixed a bug in Course fabricator

--- spec/fabricators/course_fabricator.rb
+++ spec/fabricators/course_fabricator.rb
@@ -3,13 +3,7 @@ Fabricator(:course) do
 	duration				{ [:full_year, :first_semester, :second_semester].sample }
 	credits					{ [ 5.0, 2.5].sample }
 	full_name				{ |attrs| "Course #{attrs[:number]}"}
-	in_catalog			true
-	description			nil
-	resources 			nil
-	policies				nil
-	news						nil
-  occurrences     [ (1..5).to_a ]
-  major_topics    []
+  occurrences     (1..5).to_a
 end
 
 Fabrication::Transform.define(:course, lambda { |number|
