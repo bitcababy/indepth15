73112d5a3f6aa7a20b15cdeea0ac6063bc611375
Meredith <bitcababy@gmail.com>
2013-02-24 09:40:42 -0500

    Tweaked the Department spec

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Department do
   subject { Fabricate :department }
   it { should embed_many :homepage_docs }
-  specify { subject.name.should_not be_nil }
-  specify { subject.name.should_not be_empty }
+  it { should validate_presence_of :name }
+  it { should validate_uniqueness_of :name }
+  it { should have_many :courses }
 end
