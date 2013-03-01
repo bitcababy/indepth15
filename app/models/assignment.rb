d0ccbcf17c9e203f935cb8bfe2ed3645dcbcf750
Meredith <bitcababy@gmail.com>
2013-03-01 13:30:59 -0500

    Assignment after_create: set course and teacher, update asst_number

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -13,9 +13,10 @@ class Assignment < Document
   field :mit, as: :minor_topics, type: SortedSet, default: SortedSet.new
   
   # A section_assignment is broken if the assignment is deleted
-  has_many :section_assignments, dependent: :delete
+  has_many :section_assignments, dependent: :delete, autosave: true
   has_many :browser_records
   belongs_to :teacher
+  belongs_to :course
   
   has_and_belongs_to_many :major_topics
 
@@ -25,11 +26,25 @@ class Assignment < Document
   
   index({name: -1})
   
+  after_create :set_course_and_teacher, :update_asst_number
+  
   def add_major_topics(tags=[])
     for tag in tags do
       self.major_topics.find_or_create_by name: tag
     end
     self.save!
+  end
+  
+  def update_asst_number
+    return unless self.teacher && self.name =~ /^\d+$/ 
+    self.teacher.update_number_for_course(self.course, self.name)
+  end
+  
+  def set_course_and_teacher
+    return if self.section_assignments.empty?
+    self.course = self.section_assignments.first.course
+    self.teacher = self.section_assignments.first.teacher
+    self.save
   end
 
 end
