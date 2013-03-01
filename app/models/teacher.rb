e845a85aab25e9df825418ab33165858f7cb3c3d
Meredith <bitcababy@gmail.com>
2013-03-01 13:31:50 -0500

    Added embedding and updating of asst_numbers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -7,7 +7,7 @@ class Teacher < User
 	field :gm, as: :generic_msg, type: String
 	field :cm, as: :current_msg, type: String
 	field :um, as: :upcoming_msg, type: String
-
+  
   track_history on: [:current, :default_room, :home_page, :generic_msg, :current_msg, :upcoming_msg], version_field: :version
 
 	index({current: -1})
@@ -20,6 +20,7 @@ class Teacher < User
 	scope :order_by_name, order_by(:last_name.asc, :first_name.asc)
   
   has_many :sections
+  embeds_many :asst_numbers
 
   def courses
     return self.sections.map(&:course).uniq
@@ -31,6 +32,12 @@ class Teacher < User
   
   def current_sections
     return self.sections.current
+  end
+  
+  def update_number_for_course(c, n)
+    rec = self.asst_numbers.find_or_initialize_by(course: c)
+    rec.number = [rec.number, n.to_i].max
+    self.save
   end
 
 end
