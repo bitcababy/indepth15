7dee9891562539ab88aef923c13d9177663f63c9
Meredith <bitcababy@gmail.com>
2013-03-03 13:47:15 -0500

    Removed BrowserRecord model
    SectionAssignments will be extended to serve the same purpose

--- spec/fabricators/browser_record_fabricator.rb
+++
@@ -1,8 +0,0 @@
-Fabricator(:browser_record) do
-  academic_year { Settings.academic_year }
-  course_name   { "Course #{sequence(:cn, 1)}"}
-  first_name    "John"
-  last_name     { "Doe#{sequence(:ln, 1)}"}
-  block         { ('A'..'H').to_a.sample }
-  due_date      { Date.today + (-5..5).to_a.sample}
-end
