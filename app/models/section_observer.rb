1722f2832d6615bad0b4555b8f227c44a5b2bfc6
Meredith <bitcababy@gmail.com>
2013-03-06 15:27:54 -0500

    After creating a section, add the section's course to the section's teacher

--- app/models/section_observer.rb
+++ app/models/section_observer.rb
@@ -1,10 +1,8 @@
 class SectionObserver < Mongoid::Observer
 
   def after_create(s)
-    unless s.teacher.courses.include? s.course
-      s.teacher.courses << s.course
-      s.save
-    end
+    s.teacher.courses << s.course
+    s.save
   end
  
 end
