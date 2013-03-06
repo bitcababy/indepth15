51a0a0cc3238ab75a8809a97a38dd6bb200e1bd3
Meredith <bitcababy@gmail.com>
2013-03-06 15:30:22 -0500

    Fixed Assignment fabricator to fix course and teacher assignments

--- spec/fabricators/assignment_fabricator.rb
+++ spec/fabricators/assignment_fabricator.rb
@@ -1,7 +1,18 @@
 Fabricator :assignment, from: :document, class_name: :assignment  do
   name                "The name"
 	content							"Some content"
-	section_assignments []
-	oid						      { sequence(:oid, 1) }
   major_topics        SortedSet.new
+	section_assignments []
+  teacher
+  course
+  after_create        {|obj|
+    if course
+      course.assignments << obj
+      course.save
+    end
+    if teacher
+      teacher.assignments << obj
+      teacher.save
+    end
+  }
 end
