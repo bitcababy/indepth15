68e180b17550197e01b901dc167628db5d8c4af5
Meredith <bitcababy@gmail.com>
2013-03-12 11:32:10 -0400

    Added #current? method to Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,8 +10,8 @@ class Course
 	FIRST_SEMESTER = :first_semester
 	SECOND_SEMESTER = :second_semester
 
-	DURATIONS = [FULL_YEAR, FIRST_SEMESTER, SECOND_SEMESTER, FULL_YEAR_HALF_TIME]
-	SEMESTERS = [FIRST_SEMESTER, SECOND_SEMESTER]
+	DURATIONS = [ FULL_YEAR, FIRST_SEMESTER, SECOND_SEMESTER, FULL_YEAR_HALF_TIME ]
+	SEMESTERS = [ FIRST_SEMESTER, SECOND_SEMESTER ]
 	MAJOR_TOPIC_MAP = {
 		'Geometry' => [
 			'Transformations', 'Similarity', 'Congruence', 'Logic/Proof',
@@ -77,7 +77,7 @@ class Course
 		371 => 'Calculus',
 		391 => 'Statistics',
 	}
-  
+
 	##
 	## Fields
 	##
@@ -97,38 +97,41 @@ class Course
 	field :sc, as: :schedule_name, type: String, default: ->{ full_name }
 
 	field :ha, as: :has_assignments, type: Boolean, default: true
-	field :ic, as: :in_catalog, type: Boolean, default: true
   field :oc, as: :occurrences, type: Array
-
-	field :_id, type: Integer, default: ->{ number }
   
+  field :ic, as: :in_catalog, type: Boolean, default: true
+
+	field :_id, type: String, default: ->{ number }
+
+  index( {number: -1} )
+  
+  scope :in_catalog, where(in_catalog: true)
+
  	##
 	## Associations
 	##
 	has_many :sections do
-    def for_year(y)
-      where(academic_year: y)
-    end
     def for_teacher(t)
       where(teacher: t)
     end
   end
 
   belongs_to :department, index: true
-  has_and_belongs_to_many :major_topics
   has_many :assignments
+  has_and_belongs_to_many :major_topics
   has_and_belongs_to_many :teachers
-    
+
   embeds_many :documents, class_name: 'CourseDocument'
+
 	##
 	## Scopes
 	##
-	scope :in_catalog, where(in_catalog: true).asc(:number)
-	default_scope where(in_catalog: true).asc(:number)
-  
   track_history except: [:number]
   
-  
+  def current?
+    self.sections.count > 0
+  end
+
   def branches
     BRANCH_MAP[self.number].to_a
   end
@@ -136,7 +139,7 @@ class Course
   def current_teachers
     return self.teachers.current
   end
- 
+
   def add_major_topics
     return unless self.branches
     topics = (self.branches.collect {|b| MAJOR_TOPIC_MAP[b]}).flatten
@@ -144,7 +147,7 @@ class Course
     mts = topics.collect { |topic| MajorTopic.find_or_create_by name: topic }
     self.major_topics = mts
   end
-    
+
   def doc_of_kind(k)
     return self.documents.where(kind: k).first
   end
@@ -171,7 +174,7 @@ class Course
 	def menu_label
 		self.full_name
 	end
-  
+
   def create_docs
     self.course_documents.create kind: :resources
     self.course_documents.create kind: :news
