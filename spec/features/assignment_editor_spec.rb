059cfa96e4f9b193834e81259d932876515787b1
Meredith <bitcababy@gmail.com>
2013-03-12 12:10:50 -0400

    Cleaned up AssignmentEditor spec

--- spec/features/assignment_editor_spec.rb
+++ spec/features/assignment_editor_spec.rb
@@ -1,34 +1,46 @@
 require 'spec_helper'
 
-feature "Creating an assignment" do
+feature "To create an assignment" do
   include Warden::Test::Helpers
-
-  before :each do
-    @dept = Fabricate :department
+  
+  background do
+    dept = Fabricate :department
     course = Fabricate :course
     teacher = Fabricate :teacher, login: 'davidsonl'
-    s1 = Fabricate :section, course: course, teacher: teacher, block: "B", academic_year: 2013
-    s2 = Fabricate :section, course: course, teacher: teacher, block: "D", academic_year: 2013
+    s1 = Fabricate :section, course: course, teacher: teacher, block: "B"
+    s2 = Fabricate :section, course: course, teacher: teacher, block: "D"
     login_as teacher
-    visit new_assignment_path(course, teacher)
+    visit new_course_teacher_assignment_path(course, teacher)
    end
    
-   context "the initial state" do
-     context " of the section assignemnts" do
-       scenario "initial state", :js do
-         div = page.find('#sas')
-      
-         use_fields = div.all('td.assigned div label input')
-         use_fields.count.should eq 2
-         use_fields.each do |uf|
-           uf.value.should == "1"
-         end
-         
-         due_dates = div.all('.due_date-alt')
-         due_dates.count.should eq 2
-         initial_date = Utils.next_school_day
-         due_dates.each {|dd| Date.parse(dd.value).should == initial_date}
-       end
-     end
+   scenario "fill in the form and click Create" do
+     expect(Assignment.count).to eq 0
+     expect(SectionAssignment.count).to eq 0
+     page.fill_in 'Name', with: "21"
+     expect(page).to have_selector('#save')
+     btn = page.find('#save')
+     btn.click
+     expect(SectionAssignment.count).to eq 2
+     expect(Assignment.count).to eq 1
    end
+   
+  #  
+  #  context "the initial state" do
+  #    context " of the section assignemnts" do
+  #      scenario "initial state", :js do
+  #        div = page.find('#sas')
+  #     
+  #        use_fields = div.all('td.assigned div label input')
+  #        expect(use_fields.count).to eq 2
+  #        use_fields.each do |uf|
+  #          expect(uf.value).to eq "1"
+  #        end
+  #        
+  #        due_dates = div.all('.due_date-alt')
+  #        expect(due_dates.count).to eq 2
+  #        initial_date = Utils.next_school_day
+  #        due_dates.each {|dd| expect(Date.parse(dd.value)).to eq initial_date}
+  #      end
+  #    end
+  #  end
 end
