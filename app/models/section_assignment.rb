75b6ced1414fedd2949f569113ff28d5b5d5d34e
Meredith <bitcababy@gmail.com>
2013-03-14 15:11:00 -0400

    Added future_due_date as due_date default

--- app/models/section_assignment.rb
+++ app/models/section_assignment.rb
@@ -3,10 +3,13 @@ class SectionAssignment
   include Mongoid::Timestamps::Short
   include Mongoid::History::Trackable
   
-	field :dd, as: :due_date, type: Date
+	field :dd, as: :due_date, type: Date, default: -> { Utils.future_due_date }
 	field :assigned, type: Boolean, default: false
-  field :bl, as: :block, type: String
-  field :ay, as: :academic_year, type: Integer
+  
+  attr_accessor :block
+  attr_accessor :year
+  
+  validates :due_date, presence: true
   
 	index({due_date: -1, assigned: 1})
 	
@@ -17,7 +20,11 @@ class SectionAssignment
   
   delegate :name, :content, :content=, to: :assignment
 
+  scope :for_section,       ->(s) { where(section: s) }
   scope :for_year,          -> (y) { where(year: y) }
+  scope :for_teacher,       -> (t) { where(teacher: t) }
+  scope :for_course,        -> (c) { where(course: c) }
+
   scope :due_after,         ->(date){ gt(due_date: date) }
   scope :due_on_or_after,   ->(date) { gte(due_date: date) }
   scope :due_on,            ->(date){ where(due_date: date) }
@@ -27,22 +34,31 @@ class SectionAssignment
   scope :future,            -> { due_on_or_after(future_due_date ).assigned }
   scope :next_assignment,   -> { due_on_or_after(future_due_date).assigned.asc(:due_date).limit(1) }
     
-  scope :for_section,       ->(s) { where(section: s) }
-  scope :for_teacher,       -> (t) { where(teacher: t) }
-  scope :for_course,        -> (c) { where(course: c) }
- 
   validate do |sa|
     errors.add(:base, 'SectionAssignment must have assignment') unless sa.assignment
   end
   
-  before_create do |sa|
-    sa.block = sa.section.block
-    sa.teacher = sa.section.teacher
-    sa.course = sa.section.course
-    sa.academic_year = sa.section.academic_year
+  def block
+    @block ||= self.section.block
   end
   
-  # validate do |sa|
+  def year
+    @year ||= self.section.year
+  end
+    
+  # after_build :transfer_props
+  # 
+  # def transfer_props
+  #   if sa.section
+  #     sa.block = sa.section.block
+  #     sa.teacher = sa.section.teacher
+  #     sa.course = sa.section.course
+  #     sa.year = sa.section.year
+  #     touch(sa)
+  #   end
+  # end
+        
+   # validate do |sa|
   #   errors.add(:base, 'Assignment must have name and content to be used') unless sa.assignment.name.size > 0 && sa.assignment.content.size > 0
   # end
 
