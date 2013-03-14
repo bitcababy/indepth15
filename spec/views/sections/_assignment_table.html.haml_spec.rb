a88f5daf28a19f5843b8e24e7573b3009074537a
Meredith <bitcababy@gmail.com>
2013-03-14 15:24:35 -0400

    Changed sections/_assignment_table to use shared examples

--- spec/views/sections/_assignment_table.html.haml_spec.rb
+++ spec/views/sections/_assignment_table.html.haml_spec.rb
@@ -1,14 +1,32 @@
 require 'spec_helper'
 
 describe 'sections/_assignment_table' do
-	context "guest mode" do
   before do
     stub_template 'sections/_assignment_row' => "Section row"
   end
+
+  shared_examples_for 'any assignment table' do
 		it "display a table with the provided assignments" do
+      view.stubs(:editable?).returns false
 			render partial: 'sections/assignment_table', locals: {sas: []}
 			expect(rendered).to have_selector('thead tr') do |row|
 				['#', 'Date due', 'Assignment'].each {|hdr| expect(row).to have_selector('th', content: hdr)}
+			end
+		end
+	end
+
+	context "guest mode" do
+    it_behaves_like "any assignment table"
+  end
+
+	context "user mode" do
+    it_behaves_like "any assignment table"
+
+		it "adds a couple of columns" do
+      view.stubs(:editable?).returns true
+			render partial: 'sections/assignment_table', locals: {sas: []}
+			expect(rendered).to have_selector('thead tr') do |row|
+				['Assign', 'Actions'].each {|hdr| expect(row).to have_selector('th', content: hdr)}
 			end
 		end
 	end
