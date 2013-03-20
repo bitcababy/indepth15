027e25626e1c7646914b638fc0432f4308dcf6f2
Meredith <bitcababy@gmail.com>
2013-03-20 11:37:56 -0400

    Changed %a to link_to in menus/_home

--- spec/views/menus/_home.html.haml_spec.rb
+++ spec/views/menus/_home.html.haml_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe 'menus/_home' do
+  it "goes to the home page if on another page" do
+    view.stub(:current_page?).and_return false
+    render
+    expect(page).to have_link 'Home', href: '/'
+  end
+    
+  it "does nothing if it's on the home page'" do
+    view.stub(:current_page?).and_return true
+    render
+    expect(page).to have_link 'Home', href: '#'
+  end
 		
 end
