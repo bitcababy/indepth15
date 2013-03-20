6bc7bda830a778a3f711203f980e49b34c9083b7
Meredith <bitcababy@gmail.com>
2013-03-20 11:44:47 -0400

    Fixed Assignment creation spec

--- spec/features/assignment_editor_spec.rb
+++ spec/features/assignment_editor_spec.rb
@@ -5,12 +5,12 @@ feature "To create an assignment" do
   
   background do
     dept = Fabricate :department
-    course = Fabricate :course
-    teacher = Fabricate :teacher, login: 'davidsonl'
+    course = Fabricate :course, department: dept
+    teacher = Fabricate :teacher, login: 'davidsonl', courses: [course]
     s1 = Fabricate :section, course: course, teacher: teacher, block: "B"
     s2 = Fabricate :section, course: course, teacher: teacher, block: "D"
     login_as teacher
-    visit new_course_teacher_assignment_path(course, teacher)
+    visit new_assignment_path(course, teacher)
    end
    
    scenario "fill in the form and click Create" do
@@ -24,23 +24,4 @@ feature "To create an assignment" do
      expect(Assignment.count).to eq 1
    end
    
-  #  
-  #  context "the initial state" do
-  #    context " of the section assignemnts" do
-  #      scenario "initial state", :js do
-  #        div = page.find('#sas')
-  #     
-  #        use_fields = div.all('td.assigned div label input')
-  #        expect(use_fields.count).to eq 2
-  #        use_fields.each do |uf|
-  #          expect(uf.value).to eq "1"
-  #        end
-  #        
-  #        due_dates = div.all('.due_date-alt')
-  #        expect(due_dates.count).to eq 2
-  #        initial_date = Utils.next_school_day
-  #        expect(due_dates.each {|dd| Date.parse(dd.value)).to eq initial_date
-  #      end
-  #    end
-  #  end
 end
--- spec/views/departments/home.html.haml_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe "departments/home.html.haml" do
-  pending
-
-end
