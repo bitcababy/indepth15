21fb2d2002eb6063a39f118f1482273a9777d4fc
Meredith <bitcababy@gmail.com>
2013-03-20 11:50:45 -0400

    Changed ViewMacros to allow the use of a block and to stub a couple of methods
    Stubbing :user_signed_in and :editable?

--- spec/support/view_macros.rb
+++ spec/support/view_macros.rb
@@ -1,9 +1,22 @@
 module ViewMacros
   def as_user
-    view.stubs(:editable?).returns true
+    view.stub(:user_signed_in?).and_return true
+    view.stub(:editable?).and_return true
+    yield if block_given?
   end
-
+  
   def as_guest
-    view.stubs(:editable?).returns false
+    view.stub(:user_signed_in?).and_return false
+    view.stub(:editable?).and_return false
+    yield if block_given?
   end
+  
+  def page
+    Capybara.string rendered
+  end
+  
+end
+
+RSpec.configure do |config|
+  config.include ViewMacros, :type => :view
 end
