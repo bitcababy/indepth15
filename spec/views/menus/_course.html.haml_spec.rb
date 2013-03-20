ee78788aacdff20bc8674560a64b8be507ff6647
Meredith <bitcababy@gmail.com>
2013-03-20 12:29:46 -0400

    Added specs for _course and _csection menus

--- spec/views/menus/_course.html.haml_spec.rb
+++ spec/views/menus/_course.html.haml_spec.rb
@@ -1,18 +1,23 @@
 require 'spec_helper'
 
 describe 'menus/_course' do
-	it "displays a course and its sections" do
-		sections = []
-		course = mock('course') do
-			stubs(:menu_label).returns "Fractals 101"
-			stubs(:sorted_sections).returns sections
-			stubs(:number).returns 321
-		end
+  before do
+    stub_template 'shared/_course_link' => "/course/1"
+    stub_template 'menus/_csection' => "a section"
+  end
+
+	it "displays 'Not taught this year' if there are no sections" do
+    course = stub_model Course
+    course.stub(:sorted_sections).and_return []
 		render partial: 'menus/course', locals: {course: course}
-		expect(rendered).to have_selector('li.course', text: 'Fractals 101') do |li|
-			pending "This needs a real model."
-			# expect(li).to have_selector('a', href: '/courses/321')
-		end
+    expect(rendered).to have_content('Not taught this year')
 	end
+  
+  it "Displays an unordered list if there are sections" do
+    course = stub_model Course
+    course.stub(:sorted_sections).and_return [1]
+		render partial: 'menus/course', locals: {course: course}
+  end
+    
 
 end
--- spec/views/menus/_csection.html.haml_spec.rb
+++ spec/views/menus/_csection.html.haml_spec.rb
@@ -1,30 +1,12 @@
 require 'spec_helper'
 
 describe 'menus/_csection' do
-	before do
-    view.stubs(:home_with_assignments_path).returns ""
+  it "renders the section's course name" do
+    view.stub :course_home_with_assts_path
+    section = stub_model Section, course: stub_model(Course)
+    section.stub(:label_for_course).and_return "Fractals 101"
+    # expect(section).to receive :label_for_course  # Not implemented yet
+    render partial: 'csection', locals: {section: section}
   end
-
-	it "display a line item with the section's teacher and block" do
-		course = mock do
-			stubs(:to_param).returns 321
-		end
-		teacher = mock do
-			stubs(:formal_name).returns "Mr. Ed"
-			stubs(:to_param).returns "edh"
-		end
-		section = mock do
-			stubs(:course).returns course
-			stubs(:block).returns block
-			stubs(:block).returns "B"
-			stubs(:teacher).returns teacher
-			stubs(:year).returns 2013
-			stubs(:label_for_course).returns "Mr. Ed, Block B"
-		end
-		render partial: 'menus/csection', locals: {section: section}
-		expect(rendered).to have_selector('li.section') do |li|
-			expect(li).to have_selector('a', href: course_home_with_assignments(section))
-		end
-			
-	end
+    
 end
