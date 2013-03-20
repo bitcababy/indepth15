e2cab31607614e45318432c39587261a1c79bc89
Meredith <bitcababy@gmail.com>
2013-03-20 11:36:39 -0400

    assignments/edit and assignments/new both render assignments/_form

--- spec/views/assignments/new.html.haml_spec.rb
+++ spec/views/assignments/new.html.haml_spec.rb
@@ -1,15 +1,10 @@
 require 'spec_helper'
 
 describe 'assignments/new' do
-  before do
-    stub_template 'assignments/_form' => "form"
-  end
-  
-  it "displays as if it were a widget" do
+  it "contains a form" do
+    stub_template 'assignments/_form' => "<form></form>"
     render
-    expect(rendered).to have_selector '#new_assignment.ui-widget.ui-corner-all'
-    expect(rendered).to have_selector '.ui-widget-header', text: "New assignment"
-    expect(rendered).to have_selector '.ui-widget-content'
+    expect(page).to have_selector 'form'
   end
   
 end
