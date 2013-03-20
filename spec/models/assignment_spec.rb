8926553dfcf2d778bc2aeb622a07256d836ca00c
Meredith <bitcababy@gmail.com>
2013-03-20 11:45:53 -0400

    Fixed Assignment spec

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -5,7 +5,6 @@ require 'spec_helper'
 describe Assignment do
   it { should have_many :section_assignments }
   it { should accept_nested_attributes_for :section_assignments }
-  it { should belong_to :teacher }
   it { should have_and_belong_to_many :major_topics }
   
 	context "Fabricator" do
@@ -14,22 +13,26 @@ describe Assignment do
       expect(asst).to be_valid
       expect(asst.content).to_not be_nil
     end
-    it "adds itself to the course assignments if there's a course" do
-      course = Fabricate :course
-      asst = Fabricate :assignment, course: course
-      expect(asst.course).to eq course
-      expect(course.assignments).to contain asst
-    end
-    it "adds itself to the teacher's assignments if there's a teacher" do
-      teacher = Fabricate :teacher
-      asst = Fabricate :assignment, teacher: teacher
-      expect(asst.teacher).to eq teacher
-      expect(teacher.assignments).to contain asst
-    end
-	end
+ 	end
   
-  subject { Fabricate :assignment }
-
-  # specify { subject.minor_topics.should be_kind_of SortedSet }
+   describe '#course' do
+    it "returns the course for this assignment" do
+      c = Fabricate :course
+      s = Fabricate :section, course: c
+      a = Fabricate :assignment
+      sa = Fabricate :section_assignment, section: s, assignment: a
+      expect(a.course).to eq c
+    end
+  end
+      
+  describe '#teacher' do
+    it "returns the course for this assignment" do
+      t = Fabricate :teacher
+      s = Fabricate :section, teacher: t
+      a = Fabricate :assignment
+      sa = Fabricate :section_assignment, section: s, assignment: a
+      expect(a.teacher).to eq t
+    end
+  end
   
 end
