678727ce93b9101a1b2155937ccb9b6c584dc901
Meredith <bitcababy@gmail.com>
2013-03-20 11:57:25 -0400

    Added UsersController
    Only has signin_form so far

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe UsersController do
+  # describe "get signin form" do
+  #   it "should return the signin form" do
+  #     get :signin_form
+  #     expect(response).to render_template('users/signin_form')
+  #     xhr :get, :signin_form
+  #     expect(response).to render_template('users/signin_form')
+  #   end
+  # end
+  
+end
