fdacff950e8f16cd8f20f9f4425383ccbfb51c27
Meredith <bitcababy@gmail.com>
2013-03-20 11:19:59 -0400

    Removed Assignment relation to teacher and course

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,24 +1,18 @@
-class Assignment < Document
 class Assignment < TextDocument
   include Mongoid::Timestamps::Short
   
   field :n, as: :name
-  field :co, as: :content, type: String, default: ""
   field :mit, as: :minor_topics, type: SortedSet, default: SortedSet.new
   
   # A section_assignment is broken if the assignment is deleted
   has_many :section_assignments, dependent: :delete, autosave: true
-  belongs_to :teacher
-  belongs_to :course, counter_cache: true
   has_and_belongs_to_many :major_topics
-
+  
   accepts_nested_attributes_for :section_assignments
 
   track_history track_create: true
   
   index({name: -1})
-  
-  after_create :set_course_and_teacher
   
   def add_major_topics(tags=[])
     for tag in tags do
@@ -27,11 +21,12 @@ class Assignment < TextDocument
     self.save!
   end
   
-  def set_course_and_teacher
-    return if self.section_assignments.empty?
-    self.course = self.section_assignments.first.course
-    self.teacher = self.section_assignments.first.teacher
-    self.save
+  def course
+    self.section_assignments.first.course
   end
-
+  
+  def teacher
+    self.section_assignments.first.teacher
+  end
+    
 end
