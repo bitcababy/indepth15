bc2c6d13ae260c64f5e032f229382ce63875ed01
Meredith <bitcababy@gmail.com>
2013-03-25 10:11:46 -0400

    Removed SectionsController#retrieve

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,21 +1,11 @@
 class SectionsController < ApplicationController
   before_filter :authenticate_user!, only: []
-	before_filter :find_section
+	before_filter :find_section, except: [:retrieve]
 
   def assignments_pane
-     @course = @section.course
-     respond_to do |format|
-       format.html {render layout: !request.xhr?}
-     end
-   end
-    if params[:year]
-      ss =  Section.retrieve(year: params[:year], teacher: params[:teacher], course: params[:course], block: params[:block], limit: params[:limit])
-    else
-      ss =  Section.retrieve(teacher: params[:teacher], course: params[:course], block: params[:block], limit: params[:limit)
-    end
-    
+    @course = @section.course
     respond_to do |format|
-      format.any { render json: ss }
+      format.html {render layout: !request.xhr?}
     end
   end
  
