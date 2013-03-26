8fc8a9179f3db62653f7ca7170943c33fa00490c
Meredith <bitcababy@gmail.com>
2013-03-26 15:10:32 -0400

    Set up CarrierWave cloudfiles

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-   config.cloud_files_user_name = 'xxxxxx'
-   config.cloud_files_api_key = 'xxxxxxxxxxxxxxxxxxxxx'
-   config.cloud_files_container = 'name_of_bucket'
+   config.cloud_files_user_name = 'mer'
+   config.cloud_files_api_key = '17b1fba2e589dd68a75040f7b963b343'
+   config.cloud_files_container = 'whs'
  end
 end
   
