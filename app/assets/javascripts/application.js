8bd29d6022625b9c1c0091b14bc8183853ca7cd1
Meredith <bitcababy@gmail.com>
2013-03-27 13:41:02 -0400

    Put the requires back in, since production seems to need it.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,8 @@
 *= require rails.validations
 *= require rails.validations.simple_form
 *= require ckeditor/init
+*= require jquery.dataTables
+*= require jquery.dialogForm
+*= require jquery.dcmegamenu.1.2.3
 *= require_tree .
 */
