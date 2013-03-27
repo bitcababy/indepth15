4b5c35e48a1b8097ed07e1c4ffe80d1dbd25a5ff
Meredith <bitcababy@gmail.com>
2013-03-27 10:23:23 -0400

    Added config.active_support.deprecation = :notify

--- config/application.rb
+++ config/application.rb
@@ -26,12 +26,15 @@ module InDepth
     config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
+    # config.mongoid.observers = [:course_observer, :section_observer, :section_assignment_observer]
+    # From http://bibwild.wordpress.com/2011/12/08/jquery-ui-css-and-images-and-rails-asset-pipeline/
+    initializer :after_append_asset_paths, group: :all, after: :append_assets_paths do
+      config.assets.paths.unshift Rails.root.join("app", "assets", "stylesheets", "screen", "images")
+    end
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -50,11 +53,6 @@ module InDepth
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
-    # Use SQL instead of Active Record's schema dumper when creating the database.
-    # This is necessary if your schema can't be completely dumped by the schema dumper,
-    # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
-
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
@@ -67,6 +65,8 @@ module InDepth
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.active_support.deprecation = :notify
 
 		config.generators do |g|
 			g.helper_spec 				false
@@ -74,16 +74,11 @@ module InDepth
 		  g.fixture_replacement :fabrication
 		end
 
-    # config.mongoid.observers = [:course_observer, :section_observer, :section_assignment_observer]
-    # From http://bibwild.wordpress.com/2011/12/08/jquery-ui-css-and-images-and-rails-asset-pipeline/
-    initializer :after_append_asset_paths, group: :all, after: :append_assets_paths do
-      config.assets.paths.unshift Rails.root.join("app", "assets", "stylesheets", "screen", "images")
-    end
   end
 end
 
-Mongoid.logger.level = Logger::WARN
-Moped.logger.level = Logger::WARN
+# Mongoid.logger.level = Logger::WARN
+# Moped.logger.level = Logger::WARN
 
 class Array
 	alias contains? include?
