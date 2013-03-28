261266ac672d9b43d9cb7e200d1fe9380fe53f9e
Meredith <bitcababy@gmail.com>
2013-03-28 10:05:34 -0400

    Moved extra javascript stuff into extras.js manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,13 +13,6 @@
 /*
 *= require jquery
 *= require jquery_ujs
-*= require jquery-ui-1.10.2.custom
-*= require rails.validations
-*= require rails.validations.simple_form
-*= require ckeditor/init
-*= require jquery.dataTables
-*= require jquery.dialogForm
-*= require jquery.dcmegamenu
-*= require dates
+*- require extras
 *= require_tree .
 */
---
+++ app/assets/javascripts/extras.js
@@ -0,0 +1,10 @@
+/*
+*= require jquery-ui-1.10.2.custom
+*= require rails.validations
+*= require rails.validations.simple_form
+*= require ckeditor/init
+*= require jquery.dataTables
+*= require jquery.dialogForm
+*= require jquery.dcmegamenu
+*= require dates
+*/
