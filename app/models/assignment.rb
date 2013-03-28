5bdcecf9dd3d465ff3ad51d6fbf4459cc6283a79
Meredith <bitcababy@gmail.com>
2013-03-28 15:22:48 -0400

    Added belongs_to :teacher

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -5,9 +5,17 @@ class Assignment < TextDocument
   field :mit, as: :minor_topics, type: SortedSet, default: SortedSet.new
   
   # A section_assignment is broken if the assignment is deleted
-  has_many :section_assignments, dependent: :delete, autosave: true
+  has_many :section_assignments, dependent: :delete, autosave: true do
+    def for_course(c)
+      @target.select {|sa| sa.course == c}
+    end
+  end
+
   has_and_belongs_to_many :major_topics
+  belongs_to :teacher
   
+  scope :with_numeric_name, ->{with_type(name: MongodbTypes::INTEGER32)}
+
   accepts_nested_attributes_for :section_assignments
 
   track_history track_create: true
