31f83a0397d77bcdb663b6a0e6836be024ac26a0
Meredith <bitcababy@gmail.com>
2013-03-31 12:56:22 -0400

    Set flash in CustomFailureApp

--- extras/custom_failure_app.rb
+++ extras/custom_failure_app.rb
@@ -1,8 +1,23 @@
 # From: https://github.com/plataformatec/devise/wiki/How-To:-Do-not-redirect-to-login-page-after-session-timeout
 class CustomFailureApp < Devise::FailureApp
+  def http_auth
+    logger.warn "***http_auth"
+    super
+  end
+
+  def recall
+    logger.warn "***recall"
+    super
+  end
+
+  def respond
+    logger.warn "***respond"
+    super
+  end
+
   def redirect
-    message = warden.message || warden_options[:message]
-    if message == :timeout
+    flash[:warden] = warden.message || warden_options[:message]
+    if flash[:warden] == :timeout
       redirect_to home_path
     else
       super
