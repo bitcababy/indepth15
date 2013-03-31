c2fe626a899a7300d80837bcca9ba52d5e351f95
Meredith <bitcababy@gmail.com>
2013-03-31 11:37:23 -0400

    Created spec for courses menu

--- spec/views/menus/courses.html.haml_spec.rb
+++ spec/views/menus/courses.html.haml_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe "menus/courses.html.haml" do
-  it "renders the courses menu"
+  before do
+    @dept = World.create_world
+    visit home_path
+  end
+  it "renders a courses menu", :js do
+    within '#menubar' do
+      expect(page).to have_link 'Courses'
+      Course.each do |course|
+        expect(page).to have_link course.menu_label
+        clink = page.find_link course.menu_label
+        course.sections.each do |section|
+          expect(clink).to have_link section.menu_label
+        end
+      end
+    end
+  end
 end
