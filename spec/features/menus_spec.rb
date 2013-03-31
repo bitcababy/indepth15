8a3efc911bfaebd863b352a8252a39bf9a0084d2
Meredith <bitcababy@gmail.com>
2013-03-31 11:36:02 -0400

    Started to add code to test menus

--- spec/features/menus_spec.rb
+++ spec/features/menus_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
 feature "User clicks on" do
-  before :each do
-    @dept = Fabricate :department_with_docs
+  before do
+    @dept = World.create_world
     visit home_path
   end
 
   scenario "Courses menu", :js do
     within '#menubar' do
+      expect(page).to have_link 'Courses'
       click_link 'Courses'
-      pending "unfinished test"
+      Course.each do |course|
+        expect(page).to have_link course.menu_label
+        clink = page.find_link course.menu_label
+        course.sections.each do |section|
+          expect(clink).to have_link section.menu_label
+        end
+      end
     end
   end
+
 end
 
