28ed3be5140e44322619ce9f03efc6382642d06f
Meredith <bitcababy@gmail.com>
2013-03-31 11:36:59 -0400

    Fixed a method name in _csection spec

--- spec/views/menus/_csection.html.haml_spec.rb
+++ spec/views/menus/_csection.html.haml_spec.rb
@@ -4,8 +4,8 @@ describe 'menus/_csection' do
   it "renders the section's course name" do
     view.stub :course_home_with_assts_path
     section = stub_model Section, course: stub_model(Course)
-    section.stub(:label_for_course).and_return "Fractals 101"
-    # expect(section).to receive :label_for_course  # Not implemented yet
+    section.stub(:menu_label).and_return "Fractals 101"
+    # expect(section).to receive :menu_label  # Not implemented yet
     render partial: 'csection', locals: {section: section}
   end
     
