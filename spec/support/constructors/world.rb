bf6a954a7760fb4a195b9c51d2ee55cbd3a77729
Meredith <bitcababy@gmail.com>
2013-03-31 11:36:22 -0400

    Fixed module problems in World

--- spec/support/constructors/world.rb
+++ spec/support/constructors/world.rb
@@ -1,28 +1,28 @@
 module World
-  def setup(tn: 3, cn: 2, sn: 2, an: 2)
-    Fabricate :department
-    tn.times { Fabricate :teacher }
-    cn.times { Fabricate :course }
-    Teacher.each do |teacher|
-      courses = Course.all.sample(3)
+  def self.create_world(tn: 3, cn: 2, sn: 2, an: 2)
+    dept = Fabricate :department
+    tn.times { Fabricate :teacher, dept: dept}
+    cn.times { Fabricate :course, department: dept }
+    ::Teacher.each do |teacher|
+      courses = ::Course.all.sample(3)
       courses.each do |course|
         sn.times { Fabricate :section, teacher: teacher, course: course }
       end
     end
     
-    Courses.each do |course|
+    ::Course.each do |course|
       course.teachers.each do |teacher|
         sections = teacher.sections.for_course(course)
         unless sections.empty?
-          assts = an.times { Fabricate :assignments}
+          an.times { Fabricate :assignment}
           for section in sections do
-            for asst in assts do
+            ::Assignment.each do |asst|
               section.section_assignments << Fabricate(:section_assignment, section: section, assignment: asst)
             end
           end
         end
       end
     end
-
-  end #setup
+    return dept
+  end #create_world
 end
