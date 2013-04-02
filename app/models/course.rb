b234b59c3c0b41b10faf0b9dba2ff6c097968408
Meredith <bitcababy@gmail.com>
2013-04-02 10:48:13 -0400

    Simplified course#sorted_sections, since sections know how to sort themselves

--- app/models/course.rb
+++ app/models/course.rb
@@ -168,18 +168,7 @@ class Course
   end
 
 	def sorted_sections
-		sections = self.sections.current
-		return sections.sort do |a, b|
-			teacher_a = a.teacher
-			teacher_b = b.teacher
-			if teacher_a == teacher_b 
-				 a.block <=> b.block
-			elsif teacher_a.last_name == teacher_b.last_name
-				return teacher_a.first_name <=> teacher_b.first_name
-			else
-				teacher_a.last_name <=> teacher_b.last_name
-			end
-		end
+		return self.sections.current.sort
 	end
 	
 	# def teachers
