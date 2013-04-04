b7c9d1e0e9ef4a87ae0b8ced6ffb171cfd7ea833
Meredith <bitcababy@gmail.com>
2013-04-04 13:01:40 -0400

    Moved require ckeditor/init to application.js

--- app/assets/javascripts/extras.js
+++ app/assets/javascripts/extras.js
@@ -2,7 +2,6 @@
 *= require jquery-ui-1.10.2.custom
 *= require rails.validations
 *= require rails.validations.simple_form
-*= require ckeditor/init
 *= require jquery.dataTables
 *= require jquery.dialogForm
 *= require jquery.dcmegamenu
