b096fe97e63b5a50cf3cabba42a1c9ca16442ca0
Meredith <bitcababy@gmail.com>
2013-08-15 10:23:39 -0400

    Went back to default CKEditor skin
    Need to do this  until I figure out how to get it to work in production.

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -8,7 +8,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// For the complete reference:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	config.skin = 'moonocolor';
+	// config.skin = 'moonocolor';
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
