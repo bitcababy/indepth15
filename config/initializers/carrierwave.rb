3d7131ae4d85bdfbd7e75399d8332af2b59da9e2
Meredith <bitcababy@gmail.com>
2013-08-15 10:22:14 -0400

    Set up CarrierWave configuration to use Rackspace.
    Untested at this poit.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,11 @@
 CarrierWave.configure do |config|
- #  if Rails.env.production?
- #   config.cloud_files_user_name = 'mer'
- #   config.cloud_files_api_key = '17b1fba2e589dd68a75040f7b963b343'
- #   config.cloud_files_container = 'whs'
- # end
+  config.fog_credentials = {
+    provider: 'Rackspace',
+    rackspace_user_name: 'mer',
+    rackspace_api_key: '17b1fba2e589dd68a75040f7b963b343',
+    cloud_files_container: 'whs'
+  }
+  config.fog_directory = 'whs'
+  config.asset_host = 'http://10e290356935c22c22a0-f9599cd593310f23e69703ca2d9a6168.r44.cf2.rackcdn.com/config.js'
 end
-  
+
