7d0e5ba56c20042d9cf9bea3f0a19a52a9ef7682
Meredith <bitcababy@gmail.com>
2013-08-28 11:10:53 -0400

    Set CarrierWave to use CDN in development or production but not test.

---
+++ app/uploaders/asset_uploader.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+class AssetUploader < CarrierWave::Uploader::Base
+  include Ckeditor::Backend::CarrierWave
+
+  if Rails.env.test? or Rails.env.cucumber?
+    CarrierWave.configure do |config|
+      config.storage = :file
+      config.enable_processing = false
+    end
+  else
+   CarrierWave.configure do |config|
+      config.storage = :fog
+      config.enable_processing = true
+    end
+  end
+   
+end
