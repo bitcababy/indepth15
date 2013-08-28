8b22e10a64598b7d704c82cf5502d1031277ed62
Meredith <bitcababy@gmail.com>
2013-08-28 10:17:45 -0400

    TextDocument: Added has_many relationships to images and attachments

--- app/models/text_document.rb
+++ app/models/text_document.rb
@@ -1,4 +1,9 @@
 class TextDocument < Document
   include Mongoid::Document
   field :co, as: :content, type: String, default: ""
+
+  has_many :images, class_name: 'Ckeditor::Picture'
+  has_many :attachments, class_name: 'Ckeditor::AttachmentFile'
+  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :attachments
 end
