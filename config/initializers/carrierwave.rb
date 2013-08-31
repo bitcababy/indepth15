d6aa85d78ad13e3c84b06b9880771e003a04d5aa
Meredith <bitcababy@gmail.com>
2013-08-31 11:31:22 -0400

    Fixed browsing.
    Now to making it work under Heroku.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,9 @@ CarrierWave.configure do |config|
     rackspace_api_key: '17b1fba2e589dd68a75040f7b963b343',
   }
   config.fog_directory = 'whsmd'
-  config.asset_host = 'https://dc2844559babc10ea3f6-5a9101a0fdfe9adb52a508fedafcfb04.ssl.cf1.rackcdn.com'
+  config.asset_host = 'http://files.westonmath.org'
+  # config.asset_host = 'https://bace7f7158181dd9d70a-5a9101a0fdfe9adb52a508fedafcfb04.r0.cf1.rackcdn.com'
   config.root = Rails.root.join('tmp')
-  config.cache_dir = 'carrierwave'
+  # config.cache_dir = Rails.root.join('tmp', "uploads")
+  config.cache_dir = 'carrierWave'
 end
-
