5c506d2727c51c66103b6e45a83ba409244d7e40
Meredith <bitcababy@gmail.com>
2013-12-23 15:19:59 -0500

    Got rid of extras, since everything's being done anyway
    Or so it appears.

--- app/assets/javascripts/extras.js
+++ app/assets/javascripts/extras.js
@@ -1,9 +1,2 @@
 /*
-*= require rails.validations
-*= require rails.validations.simple_form
-*= require rails.validations.customValidators
-*= require jquery.dataTables
-*= require jquery.dialogForm
-*= require jquery.dcmegamenu
-*= require jquery.setup
  */
