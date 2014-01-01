0e441d8958942abfb11bab0aec5854e143aacba4
Meredith <bitcababy@gmail.com>
2014-01-01 13:32:15 -0500

    Increase the production log level temporarily

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,19 +22,18 @@ InDepth::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.debug = false # Just to be safe
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
