4d6acfcbda7a354608f77ccc4637b8a76775b3e5
Meredith <bitcababy@gmail.com>
2014-02-09 08:58:17 -0500

    Turned digests back on

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ InDepth::Application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
