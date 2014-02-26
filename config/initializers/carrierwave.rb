ca71c50d4dd7d6670588bc0094cb9dbc542f0458
Meredith <bitcababy@gmail.com>
2014-02-26 12:20:58 -0500

    Added fog cache control

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,6 @@
 
 CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp')
-  config.cache_dir = '/carrierwave'
   # config.cache_dir = '/carrierwave'
   # This is needed for Heroku, maybe
   config.cache_dir = "uploads"
@@ -13,6 +12,7 @@ CarrierWave.configure do |config|
     rackspace_api_key: '17b1fba2e589dd68a75040f7b963b343',
   }
   config.fog_directory = 'whsmd'
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   config.asset_host = 'http://files.westonmath.org'
   # config.asset_host = 'https://bace7f7158181dd9d70a-5a9101a0fdfe9adb52a508fedafcfb04.r0.cf1.rackcdn.com'
 end
