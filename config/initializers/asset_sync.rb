04562dd7988e5ea856302bca1cb62ec7bd388ff9
Meredith <bitcababy@gmail.com>
2015-04-28 11:51:54 -0400

    Set fog and rackspace region

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -12,7 +12,7 @@ AssetSync.configure do |config|
   # config.invalidate = ['file1.js']
 
   # Increase upload performance by configuring your region
-  # config.fog_region = 'eu-west-1'
+  config.fog_region = :dfw
   #
   # Don't delete files from the store
   # config.existing_remote_files = "keep"
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,6 +10,7 @@ CarrierWave.configure do |config|
     provider: 'Rackspace',
     rackspace_username: 'mer',
     rackspace_api_key: '17b1fba2e589dd68a75040f7b963b343',
+    rackspace_region: :dfw
   }
   config.fog_directory = 'whsmd'
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
