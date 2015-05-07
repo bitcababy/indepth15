cfcf87d7558a0507c9ea085ef435f33c97f06544
Meredith <bitcababy@gmail.com>
2015-05-07 13:15:53 -0400

    Added Department.describe

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,23 +3,27 @@
 class Department
   include Mongoid::Document
   include Mongoid::Timestamps::Short
-  
+
 	field :n, as: :name, type: String
   field :fn, as: :full_name, type: String, default: -> { name }
-  field :_id, type: String, default: -> { name }
+  # field :_id, type: String, default: -> { name }
 
   validates :name, presence: true, length: { minimum: 3 }, uniqueness: true
- 
+
   embeds_many :homepage_docs, class_name: 'DepartmentDocument'
-  
+
   has_many :courses, autosave: true
   has_and_belongs_to_many :teachers, autosave: true do
     def current
       where(current: true)
     end
   end
-  
+
   def to_s
+    self.full_name
+  end
+
+  def describe
     self.full_name
   end
 
