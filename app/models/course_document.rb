4c9e4ac12e2406ebdf012170e5257e2aa8d37639
Meredith <bitcababy@gmail.com>
2015-05-07 13:15:11 -0400

    Added KINDS definition to CourseDocument

--- app/models/course_document.rb
+++ app/models/course_document.rb
@@ -1,7 +1,9 @@
 class CourseDocument < TextDocument
+  KINDS =  [:resources, :news, :policies, :information, :description]
+
   field :kind, type: Symbol
   embedded_in :course
-  
+
   def update_from_params(params)
     self.content = params[:content]
     self.course.save
